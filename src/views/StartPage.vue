507a62207bfe6eaf3128f86dc00acb33c54c7aac
qbclub <grachevrv@ya.ru>
2022-07-02 13:46:54 +0400

    layuot idea

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,9 +2,6 @@
 import SlideMenu from "../components/SlideMenu.vue";
 import { defineComponent, ref, reactive } from "vue";
 
-
-
-
 const direction = reactive([
   {
     menuItem: "Заграница",
@@ -33,14 +30,124 @@ const direction = reactive([
       background: `url(https://geo-1.ru/wp-content/uploads/2018/10/21-5.jpg)`,
       height: '100vh',
       'background-size': 'cover',
-      'background-repeat':   'no-repeat',
-      'background-position': 'center center', 
+      'background-repeat': 'no-repeat',
+      'background-position': 'center center',
     }"
   >
-    <h1>Хочу в поездку</h1>
+    <a-layout-header><h1>Хочу в поездку</h1></a-layout-header>
+    <a-row>
+      <a-col :xs="{ span: 24, order: 1 }" :sm="{ span: 12, order: 0 }">
+        <div class="selectors main_card">
+          <SlideMenu @change="console.log(event)" :direction="direction" />
+          <SlideMenu @change="console.log(event)" :direction="direction" />
+          <SlideMenu @change="console.log(event)" :direction="direction" />
+          <SlideMenu @change="console.log(event)" :direction="direction" />
+          <div class="variants">посмотреть варианты</div>
+        </div>
+      </a-col>
+      <a-col :xs="24" :sm="12">
+        <div class="budget_info">
+          <h3 class="main_card">
+            Бюджет
+            <span class="mdi mdi-24px mdi-approximately-equal"></span>
+            1500 - 3000 руб.
+          </h3>
+        </div>
+      </a-col>
+    </a-row>
+    <a-row style="height: 50%">
+      <a-col :xs="{ span: 24 }" :sm="{ span: 12 }" class="center_flex">
+        <button class="order">Заказать тур</button>
+      </a-col>
+      <a-col :xs="24" :sm="12"> </a-col>
+    </a-row>
 
-    <SlideMenu @change="console.log(event)" :direction="direction" />
+    <a-layout-footer
+      :style="{ position: 'fixed', bottom: 0, zIndex: 1, width: '100%' }"
+      >меню текст
+    </a-layout-footer>
   </div>
 </template>
-<style>
+<style lang="scss" scoped>
+.ant-layout-footer,
+.ant-layout-header {
+  background: rgba(0, 0, 0, 0.2);
+  color: white;
+  h1 {
+    color: white;
+  }
+}
+.budget_info {
+  display: flex;
+  justify-content: flex-end;
+  font-size: 1.3em;
+  h3 {
+    margin: 20px 20px 0 0;
+  }
+}
+.selectors {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  flex-direction: column;
+  margin: 20px;
+}
+.main_card {
+  color: white;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+
+.order {
+  animation: 3s ease infinite button_animation;
+  box-shadow: 1px 1px 30px white;
+  font-size: 1.3em;
+  &:hover {
+    font-size: 1.5em;
+    animation: none;
+  }
+  &:active {
+    box-shadow: 0px 0px 5px white;
+    font-size: 1.3em;
+  }
+}
+.variants{
+  position: absolute;
+  right: 40px;
+  bottom:20px;
+ 
+  font-size: 1.3em;
+  color:#e7e68b;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover{
+    font-size: 1.32em;
+  }
+}
+
+button {
+  color: white;
+  background: #3a5168;
+  transition: all 0.3s ease;
+  padding: 10px;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+   &:active {
+    font-size: 0.9em;
+  }
+}
+@keyframes button_animation {
+  0% {
+    font-size: 1.3em;
+  }
+  70% {
+    font-size: 1.4em;
+  }
+  100% {
+    font-size: 1.3em;
+  }
+}
 </style>
