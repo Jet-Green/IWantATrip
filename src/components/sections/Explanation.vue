e0af0caed029dfb4e57b89b1bd0164a4df5a96c1
Katya-com <katerina.yad.24@gmail.com>
2022-07-18 18:15:59 +0400

    Explanation

---
+++ src/components/sections/Explanation.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+
+
+</script>
+<template>
+  <a-row style="width: 100%; display: flex; justify-content: center">
+    <a-col style="display: flex; justify-content: center">
+      <a-typography style="font-size: 36px">
+        Удобная платформа для путешествий!
+      </a-typography>
+    </a-col>
+  </a-row>
+  <a-row style="width: 100%; display: flex; justify-content: center">
+    <a-col style="justify-content: center" v-for="i in 4">
+    {{i}}
+      <a-col style="justify-content: center; margin: 0 15px 0 15px">
+        <a-image
+          style="border-radius: 20px"
+          :width="200"
+          src="https://themiscochat.com/wp-content/uploads/2022/04/couple-relaxing-beach-vacation.webp "
+        />
+      </a-col>
+      <a-col style="justify-content: center; margin: 0 15px 0 15px">
+        <a-typography style="font-size: 16px"> Создайте свой тур </a-typography>
+      </a-col>
+    </a-col>
+    <!-- <a-col style="justify-content: center">
+      <a-col style="justify-content: center; margin: 0 15px 0 15px">
+        <a-image
+          style="border-radius: 20px"
+          :width="200"
+          src="https://s1.1zoom.ru/big0/234/Sea_Sky_Beach_Sunlounger_Sand_Rest_580068_1280x853.jpg "
+        />
+      </a-col>
+      <a-col style="justify-content: center; margin: 0 15px 0 15px">
+        <a-typography style="font-size: 16px"> Выберите готовый тур </a-typography>
+      </a-col>
+    </a-col> -->
+   
+  </a-row>
+</template>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,12 +3,13 @@ import CityGuide from "../components/sections/CityGuide.vue";
 import Companions from "../components/sections/Companions.vue";
 import CreateTrip from "../components/sections/CreateTrip.vue";
 import FindTrip from "../components/sections/FindTrip.vue";
+import Explanation from "../components/sections/Explanation.vue";
 </script>
 <template>
+  <Explanation />
   <CreateTrip />
   <FindTrip />
   <Companions />
   <CityGuide />
 </template>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
