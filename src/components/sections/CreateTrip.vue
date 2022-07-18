07027f9b059e56a5a67f7bc121e00468d11c175b
regtyit <titov54275@gmail.com>
2022-07-18 17:48:52 +0400

    CreateTrip updated

--- src/components/sections/CreateTrip.vue
+++ src/components/sections/CreateTrip.vue
@@ -1,12 +1,46 @@
-<script setup>
-    
-</script>
+<script setup></script>
 <template>
-    <a-row>
-        <a-col :xs="24" :sm="12" style="background-color:cyan">Image</a-col>
-        <a-col :xs="24" :sm="12">Text</a-col>
-    </a-row>
+  <a-row class="row">
+    <a-col :span="1" class="col">
+      <img
+        src="https://themiscochat.com/wp-content/uploads/2022/04/couple-relaxing-beach-vacation.webp"
+      />
+    </a-col>
+
+    <a-col :span="8" class="col">
+      <h1 style="font-weight: 900; font-size: 32px">Создайте свой тур</h1>
+      <p style="width: 50%; font-weight: 500">
+        Используя удобные фильтры создайте свой собственный тур
+      </p>
+      <a-button type="primary">Создать</a-button>
+    </a-col>
+  </a-row>
 </template>
 <style scoped>
-    
+.row {
+  display: flex;
+  justify-content: space-around;
+}
+.ant-btn-primary {
+  background: #02aff2;
+  border-color: #019edb;
+  width: 50%;
+  height: 17%;
+  border-radius: 100px;
+}
+.col {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 16px;
+}
+.col :nth-child(2n + 1) {
+  margin: 0px;
+}
+
+img {
+  width: 25vw;
+  height: 29vh;
+  border-radius: 25px;
+}
 </style>
