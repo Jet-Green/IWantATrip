2e7d2cb3e536d0c081194fc844f457075d841bda
Grisha Dzyin <grishadzyin@gmail.com>
2022-07-18 16:31:48 +0400

    footer

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,14 +2,16 @@
 </script>
 
 <template>
-  <a-layout>
+  <a-layout style="min-height: 100vh">
     <a-layout-header>
       <h1>Хочу в поездку</h1>
     </a-layout-header>
     <a-layout-content>
       <router-view></router-view>
     </a-layout-content>
-    <a-layout-footer>Это футер </a-layout-footer>
+    <a-affix>
+      <a-layout-footer>Это футер </a-layout-footer>
+    </a-affix>
   </a-layout>
 </template>
 <style lang="scss" scoped>
