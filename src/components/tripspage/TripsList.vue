e6859f38598b28d6c8fbb8dfc61f342b2de783ed
regtyit <titov54275@gmail.com>
2022-07-20 20:31:26 +0400

    cleaned TripsPage  added TripsList  updated FindTrip

--- src/components/tripspage/TripsList.vue
+++ src/components/tripspage/TripsList.vue
@@ -17,6 +17,34 @@ let tours = reactive([
     image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
     eventName: "БИ-2",
   },
+    {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
+
+  {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
+
+  {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
+    {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
+
+  {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
+
+  {
+    image: "https://страна2-0.рф/wp-content/uploads/2020/09/W50HIZer2wQ-1024x682.jpg",
+    eventName: "БИ-2",
+  },
 ]);
 </script>
 
@@ -24,7 +52,7 @@ let tours = reactive([
   <a-row display="flex" justify="center">
     <a-col :xs="22" :lg="16">
       <a-row>
-        <a-col :span="5" v-for="tour in tours">
+        <a-col :span="6" v-for="tour in tours">
           <TripCard
             :tour="tour"
           />
--- src/views/TripsPage.vue
+++ src/views/TripsPage.vue
@@ -1,6 +1,4 @@
 <script setup>
-    import TripsList from '../components/tripspage/TripsList.vue';
 </script>
 <template>
-    <TripsList />
 </template>
