27f60fe2ecd1110439c1f29bbd0ef8c016c31419
warcen11 <warcen2006@gmail.com>
2022-07-20 17:00:17 +0400

    forgetChangeRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StartPage from '../views/StartPage.vue'
 import Landing from '../views/Landing.vue'
-import TripsPage from '../components/TripsPage.vue'
+import TripsPage from '../views/TripsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/TripsPage.vue
+++ src/views/TripsPage.vue
@@ -1,4 +1,6 @@
 <script setup>
- 
+    
 </script>
-<template></template>
+<template>
+    
+</template>
