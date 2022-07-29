4f8f1d884848bf2ea93f5473b556a273446d84f6
qbclub <grachevrv@ya.ru>
2022-07-29 07:39:20 +0400

    Top section text

--- src/components/sections/Explanation.vue
+++ src/components/sections/Explanation.vue
@@ -9,7 +9,18 @@ const sm = breakpoints.smaller("md");
 <template>
   <a-row>
     <a-col :span="24" style="height: 60vh">
-      <div class="topImage"></div>
+      <div class="topImage">
+        <div class="titleContainer">
+          <h1>
+            Удобная платформа<br />
+            для путешествий!
+          </h1>
+        </div>
+
+        <a-button style="border-radius: 15px; color: #245159" size="large">
+          Поехали!
+        </a-button>
+      </div>
     </a-col>
   </a-row>
 </template>
@@ -20,10 +31,25 @@ h1 {
 }
 .topImage {
   background-image: url("../../assets/images/topImage.jpg");
-  height: 100%; 
+  height: 100%;
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .titleContainer {
+    padding: 5px 15px 0px 15px;
+    margin-bottom:15px;
+    border-radius: 15px;
+    background: rgba(0, 0, 0, 0.15);
+    box-shadow: 0 0 15px 15px rgba(0, 0, 0, 0.15);
+    h1 {
+      color: white;
+      line-height:36px;
+    }
+  }
 }
 </style>
