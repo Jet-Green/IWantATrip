a6efdea12dabcca3d0af4d03b91671addc508be4
qbclub <grachevrv@ya.ru>
2022-08-13 11:04:26 +0400

    history mode back arrow

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -7,28 +7,26 @@ let router = useRouter();
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const sm = breakpoints.smaller("md");
 
-
 function ToStartPage() {
-    router.push("/");
+  router.push("/");
 }
 </script>
 <template>
-    <a-row type="flex" justify="center" class="ma-16">
-        <a-col v-if="!sm" :xs="22" :lg="16">
-            <div @click="ToStartPage" style="font-size:18px;">
-                <span class="mdi mdi-18px mdi-arrow-left-thick" style="color: #245159"></span>
-                Назад
-            </div>
-        </a-col>
-        <a-col v-else :xs="22" :lg="16">
-            <div @click="ToStartPage">
-                <span class="mdi mdi-24px mdi-arrow-left-thick" style="color: #245159"></span>
-            </div>
-        </a-col>
-    </a-row>
+  <a-row type="flex" justify="center" class="ma-16">
+    <a-col :xs="22" :lg="16">
+      <div onclick="history.back()" style="font-size: 18px">
+        <span
+          class="mdi mdi-18px mdi-arrow-left-thick"
+          style="color: #245159"
+        ></span>
+
+        <span v-if="!sm">Назад</span>
+      </div>
+    </a-col>
+  </a-row>
 </template>
 <style scoped>
 div {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
