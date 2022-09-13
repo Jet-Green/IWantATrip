1dda014d06e5296be3faf26d80bc7ed4c7f9f184
regtyit <titov54275@gmail.com>
2022-09-13 23:00:35 +0400

    Made code lighter for AddCompanion

--- src/components/forms/AddCompanion.vue
+++ src/components/forms/AddCompanion.vue
@@ -20,126 +20,110 @@ const formState = reactive({
 
 <template>
   <BackButton />
-  <a-row type="flex" justify="center">
+
+
+  
+  <a-row class="mb-16" type="flex" justify="center">
     <a-col :xs="22" :lg="16">
-      <a-form name="basic" style="align-items: center">
-        <h1>Найти попутчика</h1>
-        <a-row type="flex" justify="space-between">
-          <a-col :span="12">
-            <a-form-item
-              label="Имя"
-              name="name"
-              :rules="[{ required: true, message: 'Введите имя!' }]"
-            >
-              <a-input v-model:value="formState.name" />
-            </a-form-item>
-          </a-col>
-          <a-col :span="11">
-            <a-form-item
-              label="Фамилия"
-              name="surname"
-              :rules="[{ required: true, message: 'Введите фамилию!' }]"
-            >
-              <a-input v-model:value="formState.surname" />
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row type="flex" justify="space-between">
-          <a-col :span="12">
-            <a-form-item
-              label="Электронная почта"
-              name="email"
-              :rules="[{ required: true, message: 'Введите электроннную почту!' }]"
-            >
-              <a-input v-model:value="formState.email" />
-            </a-form-item>
-          </a-col>
+      <h1>Найти попутчика</h1>
 
-          <a-col :span="11">
-            <a-form-item
-              label="Телефон"
-              name="phone"
-              :rules="[{ required: true, message: 'Введите телефон!' }]"
-            >
-              <a-input v-model:value="formState.phone" />
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row type="flex" justify="space-between">
-          <a-col :span="12">
-            <a-form-item
-              label="Возраст"
-              name="age"
-              :rules="[{ required: true, message: 'Введите свой возраст!' }]"
-            >
-              <a-input type="number" v-model:value="formState.age" />
-            </a-form-item>
-          </a-col>
+      <a-row class="mb-16" type="flex" justify="space-between">
+        <a-col :span="12" class="nowrap">
+          <label>Имя</label>
+          <a-input class="input" v-model:value="formState.name" />
+        </a-col>
 
-          <a-col :span="11">
-            <a-form-item
-              label="Пол"
-              name="gender"
-              :rules="[{ required: true, message: 'Укажите свой пол!' }]"
-            >
-              <a-radio-group
-                v-model:value="formState.gender"
-                name="radioGroup"
-                style="display: flex; justify-content: space-evenly"
-              >
-                <a-radio :value="'Male'">Мужчина</a-radio>
-                <a-radio :value="'Female'">Женщина</a-radio>
-              </a-radio-group>
-            </a-form-item>
-          </a-col>
-        </a-row>
+        <a-col :span="11" class="nowrap">
+          <label>Фамилия</label>
+          <a-input class="input" v-model:value="formState.surname" />
+        </a-col>
+      </a-row>
 
-        <a-form-item
-          label="Дата поездки"
-          name="date"
-          :rules="[{ required: true, message: 'Введите дату!' }]"
-          style="display: flex; justify-content: space-between; align-items: center"
-        >
-          <a-row>
-            <a-col :span="12">
-              Дата начала путешествия
-              <a-date-picker
-                v-model:value="formState.datestart"
-                placeholder="Начало"
-                :locale="ruLocale"
-                :format="dateFormatList"
-                style="width: 99%"
-              />
-            </a-col>
-            <a-col :span="12">
-              Дата окончания путешествия
-              <a-date-picker
-                v-model:value="formState.dateend"
-                placeholder="Конец"
-                :locale="ruLocale"
-                :format="dateFormatList"
-                style="width: 99%"
-              />
-            </a-col>
-          </a-row>
-        </a-form-item>
+      <a-row class="mb-16" type="flex" justify="space-between">
+        <a-col :span="12" class="nowrap">
+          <label>Электронная почта</label>
+          <a-input class="input" v-model:value="formState.email" />
+        </a-col>
 
-        <a-form-item
-          label="Описание"
-          name="description"
-          :rules="[{ required: true, message: 'Введите описание!' }]"
-        >
-          <a-textarea placeholder="Описание" :rows="4" />
-        </a-form-item>
-        <a-button
-          type="primary"
-          class="lets_go_btn"
-          size="large"
-          style="display: flex; justify-content: center"
-          >Отправить</a-button
-        >
-      </a-form>
+        <a-col :span="11" class="nowrap">
+          <label>Телефон</label>
+          <a-input class="input" v-model:value="formState.phone" />
+        </a-col>
+      </a-row>
+
+      <a-row class="mb-16" type="flex" justify="space-between">
+        <a-col :span="12" class="nowrap">
+          <label>Возраст</label>
+          <a-input class="input" type="number" v-model:value="formState.age" />
+        </a-col>
+
+        <a-col :span="11" class="nowrap">
+          <label>Пол</label>
+          <a-radio-group
+            v-model:value="formState.gender"
+            name="radioGroup"
+          >
+            <a-radio :value="'Male'">Мужчина</a-radio>
+            <a-radio :value="'Female'">Женщина</a-radio>
+          </a-radio-group>
+        </a-col>
+      </a-row>
+
+      <a-row class="mb-16 nowrap">
+        <label>Дата поездки</label>
+        <a-col :span="12">
+          Дата начала путешествия
+          <a-date-picker
+            v-model:value="formState.datestart"
+            placeholder="Начало"
+            :locale="ruLocale"
+            :format="dateFormatList"
+            class="input"
+            style="display:flex;flex-wrap: nowrap;"
+          />
+        </a-col>
+        <a-col :span="12">
+          Дата окончания путешествия
+          <a-date-picker
+            v-model:value="formState.dateend"
+            placeholder="Конец"
+            :locale="ruLocale"
+            :format="dateFormatList"
+            class="input"
+            style="display:flex;flex-wrap: nowrap;"
+          />
+        </a-col>
+      </a-row>
+
+      <a-row class="mb-16">
+        <a-col :span="24" class="nowrap">
+          <label>Пожелания</label>
+          <a-textarea :rows="4" v-model:value="formState.description"/>
+        </a-col>
+      </a-row>
+
+      <a-button
+        type="primary"
+        class="lets_go_btn"
+        size="large"
+        style="display: flex; justify-content: center"
+        >Отправить
+      </a-button>
+
     </a-col>
   </a-row>
+
+
+
 </template>
-<style scoped></style>
+<style scoped>
+.nowrap {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+}
+.input{
+  width: 75%;
+}
+</style>
