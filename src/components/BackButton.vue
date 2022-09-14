52092cf60cbdeebecf26d092b032ca9afeb8a8ba
Jet-Green <grishadzyin@gmail.com>
2022-09-14 17:43:27 +0400

    back to start page

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -6,15 +6,11 @@ let router = useRouter();
 
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const sm = breakpoints.smaller("md");
-
-function ToStartPage() {
-  router.push("/");
-}
 </script>
 <template>
   <a-row type="flex" justify="center" class="ma-16">
     <a-col :xs="22" :lg="16">
-      <div @click="router.back()" style="font-size: 18px">
+      <div @click="router.push('/')" style="font-size: 18px">
         <span class="mdi mdi-18px mdi-arrow-left-thick" style="color: #245159"></span>
 
         <span v-if="!sm">Назад</span>
