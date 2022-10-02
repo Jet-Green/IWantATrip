26aea51962f61afdc89120a4f42101db142bae01
Jet-Green <grishadzyin@gmail.com>
2022-10-02 12:22:59 +0400

    add root el

--- src/views/CompanyInfoPage.vue
+++ src/views/CompanyInfoPage.vue
@@ -1,53 +1,55 @@
 <script setup>
-  import { ref } from "vue";
-  import { useRoute } from "vue-router"
-  import axios from 'axios'
+import { ref } from "vue";
+import { useRoute } from "vue-router"
+import axios from 'axios'
 
-  import BackButton from "../components/BackButton.vue";
-  const route = useRoute()
-  
-  const _id = route.query._id;
-  
-  const backRoute = "/watch"
-  
-  let watch = ref({})
-  
-  axios.get(`http://localhost:3030/guide/get-by-id?_id=${_id}`)
-    .then((response) => {
-      watch.value = response.data;
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  </script>
+import BackButton from "../components/BackButton.vue";
+const route = useRoute()
+
+const _id = route.query._id;
+
+const backRoute = "/watch"
+
+let watch = ref({})
+
+axios.get(`http://localhost:3030/guide/get-by-id?_id=${_id}`)
+  .then((response) => {
+    watch.value = response.data;
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+</script>
 <template>
-  <BackButton :backRoute="backRoute" />
+  <div>
+    <BackButton :backRoute="backRoute" />
 
-  <a-row v-if="!watch._id">
+    <a-row v-if="!watch._id">
       <a-col :span="24" class="d-flex justify-center">
         <a-spin size="large"></a-spin>
       </a-col>
     </a-row>
 
-  <a-row v-else display="flex" justify="center" class="mb-16">
-    <a-col :xs="22" :lg="16">
-      <div class="cover mb-16">
-        <img :src="watch.image" alt="" srcset="" />
-      </div>
-       <div class="title mt-16">
-        <h1>{{ watch.name }}</h1>
-      </div>
+    <a-row v-else display="flex" justify="center" class="mb-16">
+      <a-col :xs="22" :lg="16">
+        <div class="cover mb-16">
+          <img :src="watch.image" alt="" srcset="" />
+        </div>
+        <div class="title mt-16">
+          <h1>{{ watch.name }}</h1>
+        </div>
 
-      <div class="ml-16">
-        <h3>{{ watch.description }}</h3>
-      </div>
-      <div style="display: flex; justify-content: flex-end; flex-direction: column">
-        <span>Адрес: {{ watch.address }}</span>
-        <span>Телефон: {{ watch.phone }}</span>
-        <span>Социальные сети: {{ watch.socialMedia }}</span>
-      </div>
-    </a-col>
-  </a-row>
+        <div class="ml-16">
+          <h3>{{ watch.description }}</h3>
+        </div>
+        <div style="display: flex; justify-content: flex-end; flex-direction: column">
+          <span>Адрес: {{ watch.address }}</span>
+          <span>Телефон: {{ watch.phone }}</span>
+          <span>Социальные сети: {{ watch.socialMedia }}</span>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 <style lang="scss" scoped>
 .custom-card {
@@ -56,12 +58,14 @@
   background: #ffffff;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
+
   .cover {
     object-fit: contain;
     display: flex;
     justify-content: center;
     background-color: #c4c4c4;
   }
+
   .prizes {
     .prize {
       font-size: 12px;
@@ -70,6 +74,7 @@
     }
   }
 }
+
 img {
   width: 50%;
   aspect-ratio: 1/1;
