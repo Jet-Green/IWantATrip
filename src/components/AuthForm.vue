c595f123f5bb8c0605f64bed3598c2a3611c4d1f
Katya-com <katerina.yad.24@gmail.com>
2022-10-12 19:32:33 +0400

    RegForm and AuthForm update

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,42 +1,47 @@
 <script setup>
-import { reactive } from 'vue';
-import { useAuth } from '../stores/auth';
-import users from '../fakeDB/users'
+import { reactive } from "vue";
+import { useAuth } from "../stores/auth";
+import users from "../fakeDB/users";
 
-let authStore = useAuth()
+let authStore = useAuth();
 
 let formState = reactive({
-  email: '',
-  password: '',
+  email: "",
+  password: "",
 });
 
-const emit = defineEmits([
-  'closeModal'
-])
+const emit = defineEmits(["closeModal"]);
 
 let currentUser = null;
 function login() {
   for (let user of users) {
-    if ((user.email == formState.email) && (user.password == formState.password)) {
-      console.log('login');
-      authStore.setUserStatus(user.userStatus)
-      authStore.login()
+    if (user.email == formState.email && user.password == formState.password) {
+      console.log("login");
+      authStore.setUserStatus(user.userStatus);
+      authStore.login();
     }
   }
 }
 </script>
 <template>
-  <a-form :model="formState" name="auth" :label-col="{ xs: 2, sm: 5 }" :wrapper-col="{ xs: 16, sm: 18 }"
-    @submit="login">
-    <a-form-item label="E-mail" name="e-mail" :rules="[{ required: true, message: 'Введите e-mail!' }]">
-      <a-input v-model:value="formState.email" />
-    </a-form-item>
+  <div
+    :model="formState"
+    name="auth"
+    :label-col="{ xs: 2, sm: 5 }"
+    :wrapper-col="{ xs: 16, sm: 18 }"
+    @submit="login"
+  >
+    <!-- <a-form-item label="E-mail" name="e-mail" :rules="[{ required: true, message: 'Введите e-mail!' }]"> -->
+    E-mail
+    <a-input v-model:value="formState.email" />
+    <!-- </a-form-item> -->
 
-    <a-form-item label="Пароль" name="password" :rules="[{ required: true, message: 'Введите пароль!' }]">
-      <a-input-password v-model:value="formState.password" />
-    </a-form-item>
-    <a-form-item>
-      <a-button key="submit" html-type="submit">Войти</a-button>
-    </a-form-item>
-  </a-form>
+    <!-- <a-form-item label="Пароль" name="password" :rules="[{ required: true, message: 'Введите пароль!' }]"> -->
+    Пароль
+    <a-input-password v-model:value="formState.password" />
+    <!-- </a-form-item> -->
+    <!-- <a-form-item> -->
+      <a-button key="submit" html-type="submit" class="lets_go_btn mt-8">Войти</a-button>
+    <!-- </a-form-item> -->
+  </div>
 </template>
--- src/components/RegForm.vue
+++ src/components/RegForm.vue
@@ -1,43 +1,58 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 const formState = reactive({
-  username: '',
-  email: '',
-  password: '',
+  username: "",
+  email: "",
+  password: "",
 });
 </script>
 <template>
-  <a-form
+  <div
     :model="formState"
     name="basic"
     :label-col="{ xs: 2, sm: 5 }"
     :wrapper-col="{ xs: 16, sm: 18 }"
   >
-    <a-form-item
+    <a-select v-model:value="value1" style="width: 100%">
+      <a-select-option value="jack"> Турист </a-select-option>
+      <a-select-option value="lucy"> Создатель тура </a-select-option>
+      <a-select-option value="disabled"> Владелец тур. объекта </a-select-option>
+    </a-select>
+
+    <!-- <a-form-item
       label="Ф.И.О"
       name="username"
       :rules="[{ required: true, message: 'Введите Ф.И.О!' }]"
-    >
-      <a-input v-model:value="formState.username" />
-    </a-form-item>
+    > -->
+    Ф. И. О.
+    <a-input v-model:value="formState.username" />
+    <!-- </a-form-item> -->
 
-    <a-form-item
+    <!-- <a-form-item
       label="E-mail"
       name="e-mail"
       :rules="[{ required: true, message: 'Введите e-mail!' }]"
-    >
-      <a-input v-model:value="formState.email" />
-    </a-form-item>
+    > -->
+    E-mail
+    <a-input v-model:value="formState.email" />
+    <!-- </a-form-item> -->
 
-    <a-form-item
+    <!-- <a-form-item
       label="Пароль"
       name="password"
       :rules="[{ required: true, message: 'Введите пароль!' }]"
-    >
-      <a-input-password v-model:value="formState.password" />
+    > -->
+    Пароль
+    <a-input-password v-model:value="formState.password" />
+    <!-- </a-form-item> -->
+    <a-form-item style="d.-flex">
+      <a-button
+        key="submit"
+        html-type="submit"
+        class="lets_go_btn mt-8"
+        style="justify-center"
+        >Зарегистрироваться</a-button
+      >
     </a-form-item>
-    <a-form-item>
-      <a-button key="submit" html-type="submit">Зарегистрироваться</a-button>
-    </a-form-item>
-  </a-form>
+  </div>
 </template>
