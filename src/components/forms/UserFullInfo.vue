c49699409297d187ab04ac23c267eea672bac8a1
qbclub <grachevrv@ya.ru>
2022-11-10 21:37:24 +0400

    Full creator info

--- src/components/forms/UserFullInfo.vue
+++ src/components/forms/UserFullInfo.vue
@@ -1,56 +1,91 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive} from "vue";
 const userTypes = [
   {
-    value: 'phys',
-    label: 'Физическое лицо'
+    value: "phys",
+    label: "Физическое лицо",
   },
   {
-    value: 'entrepreneur',
-    label: 'Индивидуальный предприниматель'
+    value: "entrepreneur",
+    label: "Индивидуальный предприниматель",
   },
   {
-    value: 'company',
-    label: 'Юридическое лицо'
-  }
-]
+    value: "company",
+    label: "Юридическое лицо",
+  },
+];
+
 
 let fullInfo = reactive({
-  type: null,
+  type: "phys",
+  creatorsType: "author",
   fullname: null,
   phone: null,
   govermentRegNumber: null,
-  companyName: null
-})
+  companyName: null,
+});
 </script>
 <template>
   <h2>О вас</h2>
   <a-row :gutter="[16, 16]">
-    <a-col :span="24">
+    <a-col :span="12">
       Вы
-      <a-select v-model:value="fullInfo.type" style="width: 100%" :options="userTypes">
+      <a-select
+        v-model:value="fullInfo.type"
+        style="width: 100%"
+        :options="userTypes"
+      >
       </a-select>
     </a-col>
-    <a-col v-if="fullInfo.type == 'phys'" :span="12">
-      ФИО
-      <a-input v-model:value="fullInfo.fullname" style="width: 100%" placeholder="Иванов Иван Иванович" />
+
+    <a-col :span="12" v-if="fullInfo.type">
+      Тип организатора
+      <div>
+         <a-radio-group v-model:value="fullInfo.creatorsType">
+        <a-radio-button value="author">Автор тура</a-radio-button>
+        <a-radio-button v-if="fullInfo.type != 'phys'" value="operator">Туроператор</a-radio-button>
+        <a-radio-button v-if="fullInfo.type != 'phys'&& fullInfo.type == 'company'" value="agency">Турагенство</a-radio-button>
+    </a-radio-group>
+      </div>
+     
     </a-col>
-    <a-col v-if="fullInfo.type == 'phys'" :span="12">
+    <a-col v-if="fullInfo.type == 'phys'|| fullInfo.type == 'entrepreneur' " :span="12">
+      Фaмилия Имя
+      <a-input
+        v-model:value="fullInfo.fullname"
+        style="width: 100%"
+        placeholder="Иванов Иван Иванович"
+      />
+    </a-col >
+    <a-col v-else :span="12">
+      Название
+      <a-input
+        v-model:value="fullInfo.companyName"
+        style="width: 100%"
+        placeholder="ООО Песня"
+      />
+    </a-col>
+    <a-col  :span="12">
       Телефон
-      <a-input v-model:value="fullInfo.phone" style="width: 100%" placeholder="79127528874" />
+      <a-input
+        v-model:value="fullInfo.phone"
+        style="width: 100%"
+        placeholder="79127528874"
+      />
     </a-col>
 
-    <a-col v-if="fullInfo.type == 'entrepreneur'" :span="12">
-      ФИО
-      <a-input v-model:value="fullInfo.fullname" style="width: 100%" placeholder="Иванов Иван Иванович" />
-    </a-col>
-    <a-col v-if="fullInfo.type == 'company'" :span="12">
-      Название
-      <a-input v-model:value="fullInfo.companyName" style="width: 100%" placeholder="Кубит" />
-    </a-col>
-    <a-col v-if="(fullInfo.type == 'company') || (fullInfo.type == 'entrepreneur')" :span="12">
-      ОГРН
-      <a-input v-model:value="fullInfo.govermentRegNumber" style="width: 100%" placeholder="1234567891011" />
+  
+  
+    <a-col
+      v-if="fullInfo.type == 'company' || fullInfo.type == 'entrepreneur'"
+      :span="12"
+    >
+      ИНН
+      <a-input
+        v-model:value="fullInfo.govermentRegNumber"
+        style="width: 100%"
+        placeholder="182900********"
+      />
     </a-col>
   </a-row>
 </template>
