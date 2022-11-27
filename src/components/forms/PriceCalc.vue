b8fd527efeb5c1f97f11711d3ba10ec26b07da25
regtyit <titov54275@gmail.com>
2022-11-27 16:57:25 +0400

    PriceCalc

--- src/components/forms/PriceCalc.vue
+++ src/components/forms/PriceCalc.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BackButton from "../BackButton.vue";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import { ref } from "vue";
@@ -9,30 +10,36 @@ const _id = route.query._id;
 const backRoute = "/trips";
 
 let trip = ref({});
-let mainprice = ref(0);
-let groupprice = ref(0);
+
 axios
   .get(`${import.meta.env.VITE_API_URL}/trips/get-by-id?_id=${_id}`)
   .then((response) => {
     trip.value = response.data;
     console.log(trip.value);
+    let humans=trip.value.fromAge;
+    if (humans>trip.value.maxPeople){
+      console.log('too many')
+      return humans
+    }
+    let mainprice = trip.value.cost[0].price;
+    let maxhumans=Math.ceil(humans/10)
+    let groupprice = humans*trip.value.cost[1].price/maxhumans;
+    trip.value.mainprice = mainprice;
+    trip.value.groupprice = groupprice;
   })
   .catch((error) => {
     console.log(error);
   });
 
-async function calculate(){
-    mainprice = x*trip.value.cost[0].price
-}
 </script>
 <template>
   <BackButton :backRoute="backRoute" />
-  <a-row>
-    <a-col :span="12">
-      <h1>{{ mainprice }}</h1>
+  <a-row type="flex" justify="center" align="top">
+    <a-col :span="6">
+      <h1>mainprice {{ trip.mainprice }}</h1>
     </a-col>
-    <a-col :span="12">
-      <h1>{{ groupprice }}</h1>
+    <a-col :span="6">
+      <h1>groupprice {{ trip.groupprice }}</h1>
     </a-col>
   </a-row>
 </template>
