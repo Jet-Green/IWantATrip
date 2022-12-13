a26b9500f8664931af66c175af554cc0bc42cce2
regtyit <titov54275@gmail.com>
2022-12-13 23:50:02 +0400

    PriceCalc(Very bad and unreadable)

--- src/components/forms/PriceCalc.vue
+++ src/components/forms/PriceCalc.vue
@@ -1,6 +1,6 @@
 <script setup>
 import BackButton from "../BackButton.vue";
-import { ref, reactive, onUpdated } from "vue";
+import { ref, reactive, onUpdated,createVNode,h  } from "vue";
 const backRoute = "/trips";
 let form = reactive({
   people: 5,
@@ -23,10 +23,11 @@ const groupAddCost = () => {
 };
 const tAddCost = () => {
   form.transport.push({
-    type: "",
+    type:"",
     peopleRange: 0,
     price: "",
   });
+  console.log(form.transport)
 };
 const indRemoveCost = (item) => {
   let index = form.indCost.indexOf(item);
@@ -46,16 +47,65 @@ const tRemoveCost = (item) => {
     form.transport.splice(index, 1);
   }
 };
-let marks = {}
+let marks = {};
 const onAfterChange = (value) => {
-      console.log('afterChange: ', value);
-    };
-const onAfterChange1 = (value) => {
-  form.maxPeople=value;
-  for(var i=1 ;i <= form.maxPeople; i++){ 
-    marks[i] = i
-  }
+  form.people=value;
 };
+const clearDict = (dict) => {
+  Object.keys(dict).map((key) => delete dict[key]);
+};
+const costParser1 = (value) => {
+  var x=0
+  form.indCost.forEach(function(item, index) {
+    x= x + item.price
+});
+  return x;
+};
+
+const costParser2 = (value) => {
+  var x=0
+  form.groupCost.forEach(function(item, index) {
+    x= x + item.price
+});
+  return x;
+};
+
+const costParser3 = (value) => {
+  var x=0
+  form.transport.forEach(function(item, index) {
+    x= x + item.price
+});
+  return x;
+};
+const findFinalPrice = (value) => {
+  var individual=costParser1(form.indCost)
+  var group=costParser2(form.groupCost)
+  var vehicle=costParser3(form.transport)
+  var finalPrice=individual+(group+vehicle)/form.people
+  console.log(individual,group,vehicle,finalPrice)
+  return finalPrice;
+}
+const transportRange = (value) => {
+  clearDict(marks);
+
+                         //[fruits.length - 1]
+    for (var i = 1; i <= form.transport[form.transport.length-1].peopleRange; i++) {
+      marks[i] = i;
+      form.maxPeople=i
+    }
+  
+    form.transport.forEach(function(item, index) {
+      marks[item.peopleRange] = {
+        style: {
+          color: '#000000',
+        },
+        label: createVNode('strong', {}, item.peopleRange),
+      }
+    });
+};
+
+const formatter = (value) => value=findFinalPrice()
+;
 </script>
 <template>
   <BackButton :backRoute="backRoute" />
@@ -131,13 +181,12 @@ const onAfterChange1 = (value) => {
         <a-col :span="12" class="centrify">
           <a-row
             v-for="item in form.transport"
-            :key="item.type"
             style="display: flex"
             class="ma-16"
           >
             <a-col :span="24">
               <a-input
-                v-model:value="item.first"
+                v-model:value="item.type"
                 style="width: 49%"
                 placeholder="Вид транспорта"
               />
@@ -150,29 +199,29 @@ const onAfterChange1 = (value) => {
                 :step="0.01"
               />
             </a-col>
-            <a-col :span="24">
-              <span>От</span>
+            <a-col :span="24" class="tprice">
+              <span>До</span>
               <a-input-number
                 :controls="false"
-                v-model:value="item.peopleRange"
+                v-model:value.lazy="item.peopleRange"
                 style="width: 25%"
                 placeholder="1"
                 :min="0"
+                @change="transportRange(value)"
               />
-              <span>человек до</span>
+              <!-- <span>человек до</span>
               <a-input-number
                 :controls="false"
                 v-model:value="item.peopleRange"
                 style="width: 25%"
                 placeholder="8"
                 :min="0"
-              />
+              /> -->
               <span>человек</span>
+              <a-button @click="tRemoveCost(item)" shape="circle">
+                <span class="mdi mdi-minus" style="cursor: pointer"></span>
+              </a-button>
             </a-col>
-
-            <a-button @click="tRemoveCost(item)" shape="circle">
-              <span class="mdi mdi-minus" style="cursor: pointer"></span>
-            </a-button>
           </a-row>
           <a-button type="dashed" block @click="tAddCost" class="ma-8">
             <span class="mdi mdi-12px mdi-plus"></span>
@@ -181,21 +230,8 @@ const onAfterChange1 = (value) => {
         </a-col>
 
         <a-col :span="24">
-
           <div class="slidecontainer">
-            <a-slider
-              :min="1"
-              :max="100"
-              :step="1"
-              class="slider"
-              :marks="marks"
-              v-model="form.maxPeople"
-              @afterChange="onAfterChange1"
-              :included="false"
-              />
-              
-              
-            <a-slider
+    <a-slider
               v-model="form.people"
               :min="1"
               :max="form.maxPeople"
@@ -204,15 +240,18 @@ const onAfterChange1 = (value) => {
               :marks="marks"
               @afterChange="onAfterChange"
               :included="false"
-            />
+              :tipFormatter="formatter"
+              :dots="true"
+            >
+            </a-slider>
+
+
             <!-- <datalist id="tickmarks">
               <option v-for="num in form.maxPeople" :value="num">{{num}}</option>
               list="tickmarks"
                             id="count"
               @click="count++"
             </datalist>               -->
-
-
           </div>
         </a-col>
       </a-row>
@@ -236,5 +275,8 @@ const onAfterChange1 = (value) => {
 datalist {
   display: flex;
   justify-content: space-between;
+}
+.tprice > * {
+  margin-left: 8px;
 }
 </style>
