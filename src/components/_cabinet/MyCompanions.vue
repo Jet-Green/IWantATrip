1582acdcf0c4cbbb28553b93d47cc71cc0a117d3
qbclub <17926057+qbclub@users.noreply.github.com>
2022-12-18 16:40:42 +0400

    companions tab in cabinet

---
+++ src/components/_cabinet/MyCompanions.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useAuth } from "../../stores/auth";
+import { useCompanions } from "../../stores/companions";
+
+const userStore = useAuth();
+const companionStore = useCompanions();
+
+let companions = ref([]);
+const companionRequests = userStore.user.companionRequests;
+const clearData = (dataString) => {
+  const dataFromString = new Date(dataString);
+  return dataFromString.toLocaleDateString();
+};
+onMounted(async () => {
+  if (companionRequests.length) {
+    for (let _id of companionRequests) {
+      const { data } = await companionStore.getById(_id);
+      companions.value.push(data);
+    }
+  }
+});
+</script>
+<template>
+ 
+  <a-row v-for="(companion, index) in companions" :key="index">
+    <a-col :xs="24" :md="12">
+      <b>Описание:</b> {{ companion.description }} <br />
+      <b>Время:</b> с {{ clearData(companion.start) }} по
+      {{ clearData(companion.end) }} <br />
+      <b>Тип: </b
+      ><span v-for="(type, index) in companion.type" :key="index"
+        >{{ type.toLowerCase() }},
+      </span>
+      <br />
+    </a-col>
+    <a-col :xs="24" :md="12">
+      <b>Отклики:</b> Имя, возраст, пол, телефон
+    </a-col>
+
+    <a-divider />
+  </a-row>
+</template>
