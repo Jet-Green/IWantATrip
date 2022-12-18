84b77a8dcc00007eb0a547a13c6ba77523898799
qbclub <17926057+qbclub@users.noreply.github.com>
2022-12-18 17:55:17 +0400

    start companions page rebuild

--- src/views/CompanionsPage.vue
+++ src/views/CompanionsPage.vue
@@ -1,158 +1,59 @@
 <script setup>
 import { useRouter } from "vue-router";
+import { useCompanions } from "../stores/companions";
 import BackButton from "../components/BackButton.vue";
+
+const companionStore = useCompanions();
 let router = useRouter();
-let companions = [
-  {
-    number: "1",
-    gender: "Ж",
-    age: 30,
-    way: "Египет",
-    date: "2022 Декабрь - 2023 Февраль",
-    time: "10 - 11 дней",
-  },
-  {
-    number: "2",
-    gender: "М",
-    age: 35,
-    way: "Япония",
-    date: "2022 Апрель - 2022 Май",
-    time: "20 - 25 дней",
-  },
-  {
-    number: "3",
-    gender: "Ж",
-    age: 25,
-    way: "Франция",
-    date: "2022 Сентябрь - 2022 Ноября",
-    time: "7 дней",
-  },
-  {
-    number: "4",
-    gender: "М",
-    age: 27,
-    way: "Египет",
-    date: "2022 Ноября - 2023 Январь",
-    time: "10 дней",
-  },
-  {
-    number: "5",
-    gender: "Ж",
-    age: 20,
-    way: "Америка",
-    date: "2023 Июнь - 2023 Августа",
-    time: "30 - 31 день",
-  },
-  {
-    number: "6",
-    gender: "М",
-    age: 35,
-    way: "Япония",
-    date: "2022 Апрель - 2022 Май",
-    time: "20 - 25 дней",
-  },
-  {
-    number: "7",
-    gender: "Ж",
-    age: 25,
-    way: "Франция",
-    date: "2022 Сентябрь - 2022 Ноября",
-    time: "7 дней",
-  },
-  {
-    number: "8",
-    gender: "М",
-    age: 27,
-    way: "Египет",
-    date: "2022 Ноября - 2023 Январь",
-    time: "10 дней",
-  },
-];
-let columnslg = [
-  {
-    title: "№",
-    dataIndex: "number",
-    key: "number",
-  },
-  {
-    title: "Пол",
-    dataIndex: "gender",
-    key: "gender",
-  },
-  {
-    title: "Возраст",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Направление",
-    dataIndex: "way",
-    key: "way",
-  },
-  {
-    title: "Период",
-    dataIndex: "date",
-    key: "date",
-  },
-  {
-    title: "Длительность",
-    dataIndex: "time",
-    key: "time",
-  },
-];
-let columnssm = [
-  {
-    title: "№",
-    dataIndex: "number",
-    key: "number",
-  },
-  {
-    title: "Пол",
-    dataIndex: "gender",
-    key: "gender",
-  },
-  {
-    title: "Возраст",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Направление",
-    dataIndex: "way",
-    key: "way",
-  },
-];
+
+const clearData = (dataString) => {
+  const dataFromString = new Date(dataString);
+  return dataFromString.toLocaleDateString();
+};
 </script>
 <template>
   <div>
     <BackButton></BackButton>
 
     <a-row type="flex" justify="center">
-      <a-col class="d-flex justify-center">
-        <a-button
-        class="lets_go_btn ma-16"
-            type="primary"
-            size="large"
-          @click="router.push('/add-companion')"
-          >Добавить себя</a-button
-        >
-      </a-col>
-    </a-row>
-    <a-row type="flex" justify="center">
       <a-col :xs="22" :lg="16">
         <h2>Ищу попутчика</h2>
-        <a-table
-          :columns="columnslg"
-          :data-source="companions"
-          :pagination="true"
+        <a-row type="flex" justify="center">
+          <a-col class="d-flex justify-center">
+            <a-button
+              class="lets_go_btn ma-16"
+              type="primary"
+              size="large"
+              @click="router.push('/add-companion')"
+              >Добавить себя</a-button
+            >
+          </a-col>
+        </a-row>
+        <a-row
+          v-for="(companion, i) in companionStore.companions"
+          :key="i"
+          :class="companion.gender == 'Male' ? 'men' : 'women'"
+          class="space-between align-center"
         >
-          <template #bodyCell="{ column, text }">
-            <template v-if="column.dataIndex === 'number'">
-              <a>{{ text }}</a>
-            </template>
-          </template>
-        </a-table>
+          <a-col>
+            {{ companion.name }}
+          </a-col>
+          <a-col> возраст {{ companion.age }} </a-col>
+          <a-col>
+            <a-button>+</a-button>
+          </a-col>
+          <a-divider class="ma-4" />
+        </a-row>
       </a-col>
     </a-row>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.men {
+  background: rgba(255, 102, 0, 0.1);
+}
+.women {
+  background: rgba(34, 176, 214, 0.1);
+}
+</style>
