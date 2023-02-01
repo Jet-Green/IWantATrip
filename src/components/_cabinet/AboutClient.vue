bbda65a7e0f976363a9bb11de84e86e6cff4930f
regtyit <titov54275@gmail.com>
2023-02-01 19:33:15 +0400

    some work on editing AboutClient

--- src/components/_cabinet/AboutClient.vue
+++ src/components/_cabinet/AboutClient.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { useAuth } from "../../stores/auth";
-
 const userStore = useAuth();
 const user = userStore.user;
 const info = user.fullinfo;
+info.companyName ? info.companyName : "**********"
+if(info.companyName){
+  console.log(1);
+} else{
+  info.companyName = "**********";
+}
 </script>
 <template>
   <div v-if="info">
-    <a-row>
+    <a-row style="padding-left:12px">
       <a-avatar
         style="margin-right: 8px; font-size: large; font-weight: bold"
         size="large"
@@ -16,29 +21,31 @@ const info = user.fullinfo;
       <h3 style="font-size: 28px; font-weight: bold">{{ user.fullname }}</h3>
     </a-row>
 
-    <a-row :xs="22" :md="18" :lg="16">
+    <a-row :xs="22" :md="18" :lg="16" style="padding-left:12px">
       <a-col :xs="11" :md="8" :lg="5">
         <a-typography-text type="secondary">E-mail</a-typography-text>
-        <h5 style="font-size: 16px">{{ user.email }}</h5>
+        <!-- <h5 style="font-size: 16px">{{ user.email }}</h5> -->
+        <a-typography-paragraph v-model:content="user.email" editable />
       </a-col>
 
       <a-col :xs="11" :md="8" :lg="5">
         <a-typography-text type="secondary">Телефон</a-typography-text>
-        <h5 style="font-size: 16px">{{ info.phone }}</h5>
+        <a-typography-paragraph v-model:content="info.phone" editable />
       </a-col>
 
       <a-col :xs="11" :md="8" :lg="5">
         <a-typography-text type="secondary">Отображаемое имя</a-typography-text>
-        <h5 style="font-size: 16px">{{ info.fullname }}</h5>
+        <a-typography-paragraph v-model:content="info.fullname" editable />
       </a-col>
 
       <a-col :xs="11" :md="8" :lg="5">
         <a-typography-text type="secondary">Название фирмы</a-typography-text>
-        <h5 style="font-size: 16px">{{ info.companyName ? info.companyName : "**********"}}</h5>
+        <!-- <h5 style="font-size: 16px">{{ info.companyName ? info.companyName : "**********"}}</h5> -->
+        <a-typography-paragraph v-model:content="info.companyName" editable ></a-typography-paragraph>
       </a-col>
     </a-row>
 
-    <a-row :xs="22" :md="18" :lg="16">
+    <a-row :xs="22" :md="18" :lg="16" style="padding-left:12px">
       <a-col :xs="11" :md="8" :lg="5">
         <a-typography-text type="secondary">Статус пользователя</a-typography-text>
         <h5 style="font-size: 16px">
@@ -72,3 +79,5 @@ const info = user.fullinfo;
     </a-row>
   </div>
 </template>
+<style>
+</style>
