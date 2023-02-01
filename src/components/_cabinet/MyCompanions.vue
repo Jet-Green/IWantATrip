130805197ef8adf352485d610341c60949455765
regtyit <titov54275@gmail.com>
2023-02-01 18:47:51 +0400

    date fix in MyCompanions

--- src/components/_cabinet/MyCompanions.vue
+++ src/components/_cabinet/MyCompanions.vue
@@ -2,16 +2,16 @@
 import { ref, onMounted } from "vue";
 import { useAuth } from "../../stores/auth";
 import { useCompanions } from "../../stores/companions";
-
+import dayjs from "dayjs";
 const userStore = useAuth();
 const companionStore = useCompanions();
 
 let companions = ref([]);
 const companionRequests = userStore.user.companionRequests;
-const clearData = (dataString) => {
-  const dataFromString = new Date(dataString);
-  return dataFromString.toLocaleDateString();
-};
+// const clearData = (dataString) => {
+//   const dataFromString = new Date(dataString);
+//   return dataFromString.toLocaleDateString();
+// };
 onMounted(async () => {
   if (companionRequests.length) {
     for (let _id of companionRequests) {
@@ -28,13 +28,12 @@ onMounted(async () => {
   <a-row v-for="(companion, index) in companions" :key="index">
     <a-col :xs="24" :md="12">
       <b>Описание:</b> {{ companion.description }} <br />
-      <b>Время:</b> с {{ clearData(companion.start) }} по
-      {{ clearData(companion.end) }} <br />
+      <b>Время:</b> с {{ dayjs.unix(companion.start / 1000).format("DD.MM.YYYY") }} по
+      {{ dayjs.unix(companion.end / 1000).format("DD.MM.YYYY") }} <br />
       <b>Направление: </b> {{ companion.direction }} <br />
     </a-col>
-    <a-col :xs="24" :md="12">
-      <b>Отклики:</b> Имя, возраст, пол, телефон
-    </a-col>
+    <a-col :xs="24" :md="12"> 
+    <b>Отклики:</b> Имя, возраст, пол, телефон </a-col>
 
     <a-divider />
   </a-row>
