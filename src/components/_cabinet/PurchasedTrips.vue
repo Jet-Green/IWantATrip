0e8a7f5fcf7e34ebea15de90f3511a7207f9a97a
qbclub <17926057+qbclub@users.noreply.github.com>
2023-02-05 09:04:03 +0400

    cabinet purhcasedTrips

--- src/components/_cabinet/PurchasedTrips.vue
+++ src/components/_cabinet/PurchasedTrips.vue
@@ -1,23 +1,25 @@
 <script setup>
-import { onMounted, reactive, computed } from "vue";
+import { onMounted, reactive, computed, ref } from "vue";
 import { useAuth } from "../../stores/auth";
 import { useTrips } from "../../stores/trips";
 
 const userStore = useAuth();
 const tripStore = useTrips();
+let boughtTrips = reactive([]);
 
-let myTrips = reactive([]);
-
-const myTrip = (id) => {
-  if (id) {
-    return tripStore.getById(id).then((result) => {
-      return result.data.name;
-    });
+const myTrip = async () => {
+  if (userStore.user.boughtTrips) {
+    for (let i = 0; i < userStore.user.boughtTrips.length; i++) {
+      await tripStore
+        .getById(userStore.user.boughtTrips[i].tripId)
+        .then((data) => {
+          boughtTrips.push(data.data);
+        });
+    }
   }
 };
-
-onMounted(async () => {
- 
+onMounted(() => {
+  myTrip();
 });
 </script>
 <template>
@@ -25,26 +27,29 @@ onMounted(async () => {
     <a-row>
       <a-col :span="24">
         <a-row class="table_header">
-          <a-col :md="12" :xs="4">тур</a-col>
+          <a-col :md="12" :xs="24">тур</a-col>
           <a-col :md="8" :xs="12">заказан</a-col>
-          <a-col :md="4">оплачен</a-col>
+          <a-col :md="4" :xs="12">оплачен</a-col>
         </a-row>
-        {{ myTrips }}
         <a-row
           v-for="(trip, index) of userStore.user.boughtTrips"
           :key="index"
           class="pt-8"
           :class="[trip.isBoughtNow ? 'even' : 'odd']"
         >
-          <a-col :md="12" :xs="4"> {{ }} </a-col>
+          <a-col :md="12" :xs="24">
+            {{ boughtTrips[index] ? boughtTrips[index].name : "нет названия" }}
+            с
+            {{ boughtTrips[index] ? boughtTrips[index].start : "нет даты" }}
+          </a-col>
           <a-col :md="8" :xs="12">
             <div v-for="(item, index) of trip.cart" :key="index">
               <b>{{ item.costType }}:</b> {{ item.count }} шт. на
               {{ item.count * item.cost }}руб.
             </div>
           </a-col>
-          <a-col :md="4" v-if="trip.isBoughtNow">да</a-col>
-          <a-col :md="4" v-else><a-button>Оплатить</a-button></a-col>
+          <a-col :md="4" :xs="12" v-if="trip.isBoughtNow">да</a-col>
+          <a-col :md="4" :xs="12" v-else><a-button>Оплатить</a-button></a-col>
           <a-divider class="ma-0"></a-divider>
         </a-row>
       </a-col>
--- src/stores/trips.js
+++ src/stores/trips.js
@@ -40,7 +40,7 @@ export const useTrips = defineStore('trips', {
             }
         },
         getById(_id) {
-            return axios.get(`${import.meta.env.VITE_API_URL}/trips/get-by-id?_id=${_id}`)
+            return TripService.getById( _id )
         },
         deleteById(_id) {
             return TripService.deleteTrip({ _id: _id });
