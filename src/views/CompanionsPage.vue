9a397418dd43eccdeef979117d5feffcb586381c
qbclub <17926057+qbclub@users.noreply.github.com>
2023-02-18 11:55:48 +0400

    companions page

--- src/views/CompanionsPage.vue
+++ src/views/CompanionsPage.vue
@@ -9,7 +9,13 @@ let router = useRouter();
 let breakpoints = useBreakpoints(breakpointsTailwind);
 let sm = breakpoints.smaller("md");
 const clearData = (dataString) => {
-  return new Date(Number(dataString)).toLocaleDateString("ru-Ru", { year: '2-digit', month: '2-digit', day: '2-digit' }).replaceAll("/", ".")
+  return new Date(Number(dataString))
+    .toLocaleDateString("ru-Ru", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replaceAll("/", ".");
 };
 const ageString = (age) => {
   if (age >= 10 && age <= 20) {
@@ -28,20 +34,78 @@ const ageString = (age) => {
 </script>
 <template>
   <div>
-    <BackButton></BackButton>
+    <BackButton />
 
-    <a-row type="flex" justify="center">
+    <a-row class="d-flex justify-center">
       <a-col :xs="22" :lg="12">
         <h2>Ищу попутчика</h2>
-        <a-row type="flex" justify="center">
-          <a-col class="d-flex justify-center">
-            <a-button class="lets_go_btn ma-16" type="primary" size="large"
-              @click="router.push('/add-companion')">Добавить себя</a-button>
+      </a-col>
+    </a-row>
+    <a-row type="flex" justify="center">
+      <a-col class="d-flex justify-center">
+        <a-button
+          class="lets_go_btn ma-16"
+          type="primary"
+          size="large"
+          @click="router.push('/add-companion')"
+          >Добавить себя</a-button
+        >
+      </a-col>
+    </a-row>
+    <a-row class="d-flex justify-center">
+      <a-col :lg="16" :xs="22">
+        <a-row :gutter="[8, 8]">
+          <a-col
+            :lg="6"
+            :md="8"
+            :sm="12"
+            :xs="12"
+            v-for="(companion, i) in companionStore.companions"
+            :key="i"
+          >
+            <a-card class="card">
+              <div>
+                <span class="mdi mdi-human-male-female"></span
+                >{{ companion.name }}
+              </div>
+              <div>
+                <span class="mdi mdi-human-cane"></span
+                >{{ ageString(companion.age) }}
+              </div>
+              <div>
+                <span class="mdi mdi-compass-outline"></span
+                >{{ companion.direction }}
+              </div>
+              <div :class="[companion.gender == 'Male' ? 'male' : 'female']">
+                <span class="mdi mdi-gender-male-female"></span
+                >{{ companion.gender == "Male" ? "Мужчину" : "Женщину" }}
+              </div>
+              <div>
+                <span class="mdi mdi-calendar-arrow-right"></span>
+                {{ `c ${clearData(companion.start)}` }}
+              </div>
+              <div>
+                <span class="mdi mdi-calendar-arrow-left"></span
+                >{{ `по ${clearData(companion.end)}` }}
+              </div>
+              <div>
+                <span class="mdi mdi-list-status"></span
+                >{{ companion.description }}
+              </div>
+              <a-tooltip placement="bottom">
+                <template #title>
+                  <span>отклик</span>
+                </template>
+                <a-button shape="circle" class="accept">
+                  <span class="mdi mdi-thumb-up-outline"></span>
+                </a-button>
+              </a-tooltip>
+            </a-card>
           </a-col>
         </a-row>
       </a-col>
 
-      <a-col :xs="22" :lg="16">
+      <!-- <a-col :xs="22" :lg="16">
         <a-row class="table_header">
           <a-col :xs="4" :md="3">имя</a-col>
           <a-col :xs="4" :md="2">возраст</a-col>
@@ -67,7 +131,7 @@ const ageString = (age) => {
               <span class="mdi mdi-thumb-up-outline"></span>
             </a-button></a-col>
         </a-row>
-      </a-col>
+      </a-col> -->
     </a-row>
   </div>
 </template>
@@ -84,6 +148,29 @@ const ageString = (age) => {
 .table_header {
   text-align: center;
   // background: rgba(34, 176, 214, 0.05) ;
+}
+.card {
+  width: 100%;
+  background: #f6f6f6;
+  padding: 8px;
+  position: relative;
+  .mdi {
+    margin: 4px;
+  }
+  .accept {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .male {
+    color: rgba(34, 176, 214);
+  }
+  .female {
+    color: rgb(255, 102, 0);
+  }
 }
 </style>
 
