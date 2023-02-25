27f9d3e112354eb8f653ad826d13f1a0cecd6cad
qbclub <17926057+qbclub@users.noreply.github.com>
2023-02-25 11:57:42 +0400

    companion not matter

--- src/views/CompanionsPage.vue
+++ src/views/CompanionsPage.vue
@@ -43,50 +43,36 @@ const ageString = (age) => {
     </a-row>
     <a-row type="flex" justify="center">
       <a-col class="d-flex justify-center">
-        <a-button
-          class="lets_go_btn mb-4"
-          type="primary"
-          size="large"
-          @click="router.push('/add-companion')"
-          >Добавить себя</a-button
-        >
+        <a-button class="lets_go_btn mb-4" type="primary" size="large" @click="router.push('/add-companion')">Добавить
+          себя</a-button>
       </a-col>
     </a-row>
     <a-row class="d-flex justify-center">
       <a-col :lg="16" :xs="22">
         <a-row :gutter="[8, 8]">
-          <a-col
-            :lg="8"
-         
-            :sm="12"
-            :xs="24"
-            v-for="(companion, i) in companionStore.companions"
-            :key="i"
-          >
+          <a-col :lg="8" :sm="12" :xs="24" v-for="(companion, i) in companionStore.companions" :key="i">
             <a-card class="card">
               <div>
-                <span class="mdi mdi-human-male-female"></span
-                >{{ companion.name }} <span class="mdi mdi-human-cane"></span
-                >{{ ageString(companion.age) }}
+                <span class="mdi mdi-human-male-female"></span>{{ companion.name }} <span
+                  class="mdi mdi-human-cane"></span>{{ ageString(companion.age) }}
               </div>
-          
+
               <div>
-                <span class="mdi mdi-compass-outline"></span
-                >{{ companion.direction }}
+                <span class="mdi mdi-compass-outline"></span>{{ companion.direction }}
               </div>
-              <div :class="[companion.gender == 'Male' ? 'male' : 'female']">
-                <span class="mdi mdi-gender-male-female"></span
-                >{{ companion.gender == "Male" ? "Мужчину" : "Женщину" }}
+              <div
+                :class="[companion.companionGender == 'Male' ? 'male' : companion.companionGender == 'Female' ? 'female' : 'not-matter']">
+                <span class="mdi mdi-gender-male-female"></span>{{ companion.companionGender == "Male" ? "Мужчину"
+                  : companion.companionGender == "Female" ? "Женщину" : "Не важно" }}
               </div>
               <div>
                 <span class="mdi mdi-calendar-arrow-right"></span>
-                {{ `c ${clearData(companion.start)}` }} <span class="mdi mdi-calendar-arrow-left"></span
-                >{{ `по ${clearData(companion.end)}` }}
+                {{ `c ${clearData(companion.start)}` }} <span class="mdi mdi-calendar-arrow-left"></span>{{ `по
+                                ${clearData(companion.end)}` }}
               </div>
-           
+
               <div>
-                <span class="mdi mdi-list-status"></span
-                >{{ companion.description }}
+                <span class="mdi mdi-list-status"></span>{{ companion.description }}
               </div>
               <a-tooltip placement="bottom">
                 <template #title>
@@ -101,33 +87,7 @@ const ageString = (age) => {
         </a-row>
       </a-col>
 
-      <!-- <a-col :xs="22" :lg="16">
-        <a-row class="table_header">
-          <a-col :xs="4" :md="3">имя</a-col>
-          <a-col :xs="4" :md="2">возраст</a-col>
-          <a-col :xs="12" :md="5">направление</a-col>
-          <a-col :xs="4" :md="3">попутчик</a-col>
-          <a-divider v-if="sm" class="ma-0"></a-divider>
-          <a-col :xs="4" :md="2">начало</a-col>
-          <a-col :xs="4" :md="2">конец</a-col>
-          <a-col :xs="12" :md="5">пожелания</a-col>
-          <a-col :xs="4" :md="2">отклик</a-col>
-        </a-row>
-        <a-row v-for="(companion, i) in companionStore.companions" :key="i"
-          :class="companion.gender == 'Male' ? 'men' : 'women'" class="mt-4 pa-8" style="text-align:center">
-          <a-col :xs="4" :md="3">{{ companion.name }}</a-col>
-          <a-col :xs="4" :md="2">{{ ageString(companion.age) }}</a-col>
-          <a-col :xs="12" :md="5">{{ companion.direction }}</a-col>
-          <a-col :xs="4" :md="3">{{ companion.gender == "Male" ? "муж" : "жен" }}</a-col>
-          <a-divider v-if="sm" class="ma-0"></a-divider>
-          <a-col :xs="4" :md="2">{{ clearData(companion.start) }}</a-col>
-          <a-col :xs="4" :md="2">{{ clearData(companion.end) }}</a-col>
-          <a-col :xs="12" :md="5">{{ companion.description }}</a-col>
-          <a-col :xs="4" :md="2"> <a-button shape="circle">
-              <span class="mdi mdi-thumb-up-outline"></span>
-            </a-button></a-col>
-        </a-row>
-      </a-col> -->
+
     </a-row>
   </div>
 </template>
@@ -145,14 +105,17 @@ const ageString = (age) => {
   text-align: center;
   // background: rgba(34, 176, 214, 0.05) ;
 }
+
 .card {
   width: 100%;
   background: #f6f6f6;
   padding: 8px;
   position: relative;
+
   .mdi {
     margin: 4px;
   }
+
   .accept {
     position: absolute;
     top: -6px;
@@ -161,11 +124,17 @@ const ageString = (age) => {
     justify-content: center;
     align-items: center;
   }
+
   .male {
     color: rgba(34, 176, 214);
   }
+
   .female {
     color: rgb(255, 102, 0);
+  }
+
+  .not-matter {
+    color: rgb(111, 133, 43);
   }
 }
 </style>
