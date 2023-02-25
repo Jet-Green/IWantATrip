8a26f44f83c9163cb4214bfe0f597e6408fcf0cb
qbclub <17926057+qbclub@users.noreply.github.com>
2023-02-25 14:40:52 +0400

    design companionFilter

--- src/components/CompanionFilter.vue
+++ src/components/CompanionFilter.vue
@@ -3,9 +3,7 @@ import { ref, onMounted, watch, computed } from "vue";
 import { useRouter } from "vue-router";
 import { useTrips } from "../stores/trips";
 
-let props = defineProps({
-  search: String,
-});
+
 
 const tripsStore = useTrips();
 
@@ -13,18 +11,7 @@ let router = useRouter();
 
 let where = ref("");
 let time = ref(null);
-let locations = computed(() => {
-  let result = [];
-  // проблема, надо получить направления и посчитать на сервере, мы не будем загружать все на клиента
-  for (let t of tripsStore.trips) {
-    if (t.location == "") {
-      continue;
-    }
-    result.push(t.location.toLowerCase());
-  }
-  // получаем уникальные значения
-  return [...new Set(result)];
-});
+
 
 let visible = ref(false);
 
@@ -47,64 +34,52 @@ watch(where, (newPlace) => {
 });
 
 onMounted(() => {
-  if (props.search) {
-    query.value = props.search;
-  }
-  tripsStore.searchTrips(query.value, where.value, {
-    start: time.value ? time.value[0].$d.getTime() : null,
-    end: time.value ? time.value[1].$d.getTime() : null,
-  });
+
 });
 </script>
 <template>
-  <a-row type="flex" justify="center" align="middle" class="section_bg pa-16">
+  <a-row>
     <a-col :xs="24">
-      <a-row class="ma-8" type="flex" justify="center">
+      <a-row type="flex" justify="center">
         <a-col :xs="24" class="d-flex">
-          <a-input-search
-            v-model:value="query"
-            placeholder="поиск"
-            enter-button
-            style="z-index: 0"
-            @search="find()"
-          />
-          <span
-            class="mdi mdi-24px mdi-filter-outline ml-16"
-            :class="{ active_filter: visible, filter: !visible }"
-            @click="visible = !visible"
-          ></span>
+          <a-input-search v-model:value="query" placeholder="поиск" enter-button style="z-index: 0" @search="find()" />
+          <span class="mdi mdi-24px mdi-filter-outline ml-16" :class="{ active_filter: visible, filter: !visible }"
+            @click="visible = !visible"></span>
+
         </a-col>
+     
       </a-row>
       <Transition name="fade">
         <div v-if="visible">
           <a-row type="flex" justify="center">
             <a-col :xs="24">
-              <a-select
-                style="width: 100%"
-                placeholder="Куда едем"
-                v-model:value="where"
-                :bordered="true"
-                size="large"
-                class="selector"
-              >
-                <a-select-option value="" placeholder="Куда едем"> </a-select-option>
-                <a-select-option
-                  v-for="(l, index) of locations"
-                  :value="l"
-                  :key="index"
-                >
+              <label for="gender">пол</label>
+              <a-select name="gender" style="width: 100%" v-model:value="where" :bordered="true"
+               class="selector">
+                <a-select-option value=""> </a-select-option>
+                <a-select-option v-for="(l, index) of ['Мужчину', 'Женщину', 'Не важно']" :value="l" :key="index">
                   {{ l }}
                 </a-select-option>
               </a-select>
             </a-col>
           </a-row>
-          <a-row type="flex" justify="center" class="mt-16">
-            <a-col :xs="12">
-              <a-range-picker style="width: 100%" v-model:value="time" />
+          <a-row type="flex" justify="center" >
+            <a-col :xs="24">
+              <label for="age">возраст</label>
+              <div name="age" class="d-flex align-en">
+                от <a-input type="number" :min="0"  />
+                до <a-input type="number" :min="0" />
+              </div>
+            </a-col>
+          </a-row>
+          <a-row type="flex" justify="center" >
+            <a-col :xs="24">
+              <label for="data">дата</label>
+              <a-range-picker name="data" style="width: 100%" v-model:value="time" />
             </a-col>
           </a-row>
           <a-row type="flex" justify="center" class="mt-8">
-            <a-button @click="find" type="primary">поиск</a-button>
+            <a-button @click="find" class="lets_go_btn" type="primary">поиск</a-button>
           </a-row>
         </div>
       </Transition>
@@ -113,17 +88,16 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
-.section_bg {
-  background: rgba(67, 65, 79);
-}
-
 .active_filter {
   color: #ff6600;
   cursor: pointer;
 }
 
 .filter {
-  color: white;
+  color: #227597;
   cursor: pointer;
+}
+label{
+  font-size:8px;
 }
 </style>
