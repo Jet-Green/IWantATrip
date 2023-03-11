140b61b045a2e7784a8d4b389c8964c35c014f38
Jet-Green <grishadzyin@gmail.com>
2023-03-11 18:07:56 +0400

    change auth form

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from "vue";
 import { useAuth } from "../stores/auth";
 import { useRouter } from "vue-router";
 import BackButton from "../components/BackButton.vue";
@@ -10,14 +9,10 @@ import * as yup from 'yup';
 
 const user = useAuth();
 const router = useRouter();
-let email = ref(null);
-let password = ref(null);
 
-async function logIn() {
-  let result = await user.login(email.value, password.value);
+async function logIn(values) {
+  let result = await user.login(values.email, values.password);
   if (result.success) {
-    email.value = "";
-    password.value = "";
     message.config({ duration: 1.5, top: "70vh" });
     message.success({
       content: "Успешно!",
@@ -42,18 +37,17 @@ const formSchema = yup.object({
         <a-row>
           <a-col :span="24">
             <h2>Вход</h2>
-            <Form :validation-schema="formSchema" v-slot="{ meta }" @submit="logIn()">
-              <Field name="email" type="email" v-slot="{ field, handleChange }">
-                <a-input @change="handleChange" :value="field.value" placeholder="email@email.com" size="large"
-                  v-model:value="email"></a-input>
+            <Form :validation-schema="formSchema" v-slot="{ meta }" @submit="logIn" :validateOnMount="true">
+              <Field name="email" type="email" v-slot="{ value, handleChange }">
+                <a-input @update:value="handleChange" :value="value" placeholder="email@email.com" size="large"></a-input>
               </Field>
               <Transition name="fade">
                 <ErrorMessage name="email" class="error-message" />
               </Transition>
 
-              <Field name="password" type="password" v-slot="{ field, handleChange }">
-                <a-input @change="handleChange" :value="field.value" placeholder="Введите пароль" size="large"
-                  v-model:value="password" type="password" class="mt-8"></a-input>
+              <Field name="password" type="password" v-slot="{ value, handleChange }">
+                <a-input @update:value="handleChange" :value="value" placeholder="Введите пароль" size="large"
+                  type="password" class="mt-8"></a-input>
               </Field>
               <Transition name="fade">
                 <ErrorMessage name="password" class="error-message" />
