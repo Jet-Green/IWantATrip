97babc194339f2398cf660260f8cd4ee602dd7c7
Jet-Green <grishadzyin@gmail.com>
2023-03-23 17:18:38 +0400

    cabinet changes

--- src/components/_cabinet/AboutClient.vue
+++ src/components/_cabinet/AboutClient.vue
@@ -6,7 +6,6 @@ const userStore = useAuth();
 const user = computed(() => userStore.user);
 
 const info = computed(() => userStore.user?.fullinfo);
-
 </script>
 <template>
   <div v-if="info && user">
@@ -17,60 +16,58 @@ const info = computed(() => userStore.user?.fullinfo);
       <h3 style="font-size: 28px; font-weight: bold">{{ user.fullname }}</h3>
     </a-row>
 
-    <a-row :xs="22" :md="18" :lg="16">
-      <a-col :xs="11" :md="8" :lg="5">
+    <a-row>
+      <a-col :xs="12" :md="8">
         <a-typography-text type="secondary">E-mail</a-typography-text>
-        <h5 style="font-size: 16px" :ref="(el)=>{
-          el.style.fontSize=`${256/el.textContent.length}px`
-        }">{{ user.email }}</h5>
+        <h5 style="font-size: 16px">{{ user.email }}</h5>
       </a-col>
 
-      <a-col :xs="11" :md="8" :lg="5">
+      <a-col :xs="12" :md="8">
         <a-typography-text type="secondary">Телефон</a-typography-text>
         <h5 style="font-size: 16px">{{ info.phone }}</h5>
       </a-col>
 
-      <a-col :xs="11" :md="8" :lg="5">
+      <a-col :xs="12" :md="8">
         <a-typography-text type="secondary">Отображаемое имя</a-typography-text>
         <h5 style="font-size: 16px">{{ info.fullname }}</h5>
       </a-col>
 
-      <a-col :xs="11" :md="8" :lg="5" v-if="info.companyName">
+      <a-col :xs="12" :md="8" v-if="info.companyName">
         <a-typography-text type="secondary">Название фирмы</a-typography-text>
-        <h5 style="font-size: 16px">{{ info.companyName  }}</h5>
+        <h5 style="font-size: 16px">{{ info.companyName }}</h5>
       </a-col>
     </a-row>
 
-    <a-row :xs="22" :md="18" :lg="16">
-      <a-col :xs="11" :md="8" :lg="5"  v-if="info.creatorsType">
+    <a-row>
+      <a-col :xs="11" :md="8" v-if="info.creatorsType">
         <a-typography-text type="secondary">Статус пользователя</a-typography-text>
         <h5 style="font-size: 16px">
           {{
             info.creatorsType == "author"
-              ? "Автор тура"
-              : info.creatorsType == "operator"
-                ? "Туроператор"
-                : "Турагенство"
+            ? "Автор тура"
+            : info.creatorsType == "operator"
+              ? "Туроператор"
+              : "Турагенство"
           }}
         </h5>
       </a-col>
 
-      <a-col :xs="11" :md="8" :lg="5" v-if="info.type">
+      <a-col :xs="11" :md="8" v-if="info.type">
         <a-typography-text type="secondary">Юр. статус</a-typography-text>
         <h5 style="font-size: 16px">
           {{
             info.type == "phys"
-              ? "Физическое лицо"
-              : info.type == "company"
-                ? "Юридическое лицо"
-                : "Индивидуальный предприниматель"
+            ? "Физическое лицо"
+            : info.type == "company"
+              ? "Юридическое лицо"
+              : "Индивидуальный предприниматель"
           }}
         </h5>
       </a-col>
 
-      <a-col :xs="11" :md="8" :lg="5" v-if="info.govermentRegNumber">
+      <a-col :xs="11" :md="8" v-if="info.govermentRegNumber">
         <a-typography-text type="secondary">ИНН</a-typography-text>
-        <h5 style="font-size: 16px">{{ info.govermentRegNumber}}</h5>
+        <h5 style="font-size: 16px">{{ info.govermentRegNumber }}</h5>
       </a-col>
     </a-row>
   </div>
