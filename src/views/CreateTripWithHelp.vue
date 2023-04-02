1bd64c2b8cf687b12c81b5a0eeb106d2c476a366
qbclub <17926057+qbclub@users.noreply.github.com>
2023-04-02 12:51:27 +0400

    boking form clearing

--- src/views/CreateTripWithHelp.vue
+++ src/views/CreateTripWithHelp.vue
@@ -1,10 +1,10 @@
 <script setup>
 import BackButton from "../components/BackButton.vue";
 import UserFullInfo from "../components/forms/UserFullInfo.vue";
-
+import { useRouter } from "vue-router";
 import { reactive, ref, onMounted } from "vue";
 import locale from "ant-design-vue/es/date-picker/locale/ru_RU";
-
+import { message } from "ant-design-vue";
 import BookingService from "../service/BookingService";
 import { useAuth } from "../stores/auth";
 import { useAppState } from "../stores/appState";
@@ -15,19 +15,18 @@ const ruLocale = locale;
 
 const userStore = useAuth();
 const appStore = useAppState();
-
+const router = useRouter();
 let form = reactive({
-  duration: "",
-  name: "",
-  delivery: false,
   type: [],
   start: "",
   end: "",
-  resource: "",
-  desc: "",
+  location: "",
+  duration: "",
+  adults: "",
+  children: "",
   fromAge: "",
-  phoneNumber: "",
-  tripType: "Любой",
+  wishes: "",
+
 });
 let userInfo = reactive({
   fullname: "",
@@ -39,6 +38,21 @@ let formState = reactive({
   password: "",
   username: "",
 });
+function close() {
+  router.push("/trips");
+}
+function clearForm() {
+  form.type = []
+  form.start = ""
+  form.end = ""
+  form.location = ""
+  form.duration = ""
+  form.adults = ""
+  form.children = ""
+  form.fromAge = ""
+  form.wishes = ""
+
+}
 
 async function submit() {
   let toSend = Object.assign(form);
@@ -57,7 +71,17 @@ async function submit() {
       console.log(err);
     });
 
-  return TripService.bookingTrip(toSend);
+  TripService.bookingTrip(toSend).then(() => {
+    message.config({ duration: 1.5, top: "70vh" });
+    message.success({
+      content: "Успешно!",
+      onClose: () => {
+        close()
+        clearForm()
+      },
+    });
+  });
+
   // очистить форму, сделать редирект на главную, вывести уведомление снизу об успехе
 }
 
@@ -73,140 +97,83 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div style="position: relative">
+  <div>
     <BackButton />
-    
-    <img src="../assets/images/бокл.png" style="position: absolute; left: 0px; bottom: 0px; font-size: large"/>
-      
-    <img src="../assets/images/бокп.png" style="position: absolute; right: 0px; bottom: 0px; font-size: large"/>
 
-    <form
-      action="POST"
-      @submit.prevent="submit"
-      enctype="multipart/form-data"
-      ref="newGuideElementForm"
-    >
-      
+    <img src="../assets/images/бокл.png" style="position: fixed; left: 0px; bottom: 0px;  width: 20%;" />
+
+    <img src="../assets/images/бокп.png" style="position: fixed; right: 0px; bottom: 0px; width: 20% " />
+
+    <form action="POST" @submit.prevent="submit" enctype="multipart/form-data">
+
       <a-row type="flex" justify="center">
         <a-col :xs="22" :lg="12">
           <h2>О вас</h2>
           <a-row :gutter="[16, 16]">
             <a-col :span="24" :md="12">
               Фaмилия Имя
-              <a-input
-                style="width: 100%"
-                v-model:value="userInfo.fullname"
-                placeholder="Иванов Иван Иванович"
-              />
+              <a-input style="width: 100%" v-model:value="userInfo.fullname" placeholder="Иванов Иван Иванович" />
             </a-col>
             <a-col :span="24" :md="12">
               Телефон
-              <a-input
-                style="width: 100%"
-                v-model:value="userInfo.phone"
-                placeholder="79127528874"
-              />
+              <a-input style="width: 100%" v-model:value="userInfo.phone" placeholder="79127528874" />
             </a-col>
-
             <a-col :span="24">
               <h2>Заказать тур</h2>
               <div>
                 Тип тура
                 <a-select v-model:value="form.type" style="width: 100%" mode="multiple">
-                  <a-select-option
-                    v-for="value in appStore.appState[0].tripType"
-                    :value="value"
-                    >{{ value }}</a-select-option
-                  >
+                  <a-select-option v-for="value in appStore.appState[0].tripType" :value="value">{{ value
+                  }}</a-select-option>
                 </a-select>
               </div>
             </a-col>
 
             <a-col :span="12">
               Дата начала
-              <a-date-picker
-                v-model:value="form.start"
-                style="width: 100%"
-                placeholder="Начало"
-                :locale="ruLocale"
-                :format="dateFormatList"
-              />
+              <a-date-picker v-model:value="form.start" style="width: 100%" placeholder="Начало" :locale="ruLocale"
+                :format="dateFormatList" />
             </a-col>
             <a-col :span="12">
               Дата конца
-              <a-date-picker
-                v-model:value="form.end"
-                style="width: 100%"
-                placeholder="Конец"
-                :locale="ruLocale"
-                :format="dateFormatList"
-              />
+              <a-date-picker v-model:value="form.end" style="width: 100%" placeholder="Конец" :locale="ruLocale"
+                :format="dateFormatList" />
             </a-col>
 
-            <a-col :xs="24" :md="12"
-              >Направление
-              <a-input
-                placeholder="Байкал"
-                size="large"
-                v-model:value="form.location"
-              ></a-input>
+            <a-col :xs="24" :md="12">Направление
+              <a-input placeholder="Байкал" size="large" v-model:value="form.location"></a-input>
             </a-col>
             <a-col :xs="24" :md="12">
               Продолжительность, дн.
-              <a-input-number
-                id="inputNumber"
-                v-model:value="form.duration"
-                style="width: 100%"
-                placeholder="5"
-                :min="1"
-              />
+              <a-input-number id="inputNumber" v-model:value="form.duration" style="width: 100%" placeholder="5"
+                :min="1" />
             </a-col>
 
             <a-col :xs="24" :md="8">
               Взрослые
-              <a-input-number :min="0" style="width: 100%" placeholder="2" />
+              <a-input-number :min="0" style="width: 100%" placeholder="2" v-model:value="form.adults" />
             </a-col>
             <a-col :xs="24" :md="8">
               Дети
-              <a-input-number :min="0" style="width: 100%" placeholder="1" />
+              <a-input-number :min="0" style="width: 100%" placeholder="1" v-model:value="form.children" />
             </a-col>
-            <a-col :xs="24" :md="8"
-              >Мин. возраст, лет
-              <a-input-number
-                id="inputNumber"
-                v-model:value="form.fromAge"
-                style="width: 100%"
-                placeholder="10"
-                :min="0"
-                :max="100"
-              />
+            <a-col :xs="24" :md="8">Мин. возраст, лет
+              <a-input-number id="inputNumber" v-model:value="form.fromAge" style="width: 100%" placeholder="10" :min="0"
+                :max="100" />
             </a-col>
 
             <a-col :xs="24">
               Пожелания
 
-              <a-textarea autoSize />
-            </a-col>
-            <a-col :xs="24" :md="12">
-              Телефон
-              <a-input />
+              <a-textarea autoSize v-model:value="form.wishes" />
             </a-col>
 
-            <a-col :xs="24" :md="12">
-              E-mail
-              <a-input />
-            </a-col>
+
           </a-row>
 
           <a-row type="flex" justify="center">
             <a-col :xs="24" :md="16" :lg="10" class="d-flex justify-center">
-              <a-button
-                type="primary"
-                html-type="submit"
-                class="lets_go_btn mt-8"
-                size="large"
-                >Отправить</a-button
-              >
+              <a-button type="primary" html-type="submit" class="lets_go_btn mt-8" size="large">Отправить</a-button>
             </a-col>
           </a-row>
         </a-col>
