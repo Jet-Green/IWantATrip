af501e1730b386ea4b681f170cfe87864833153b
tooortttt <savelijsutov@gmail.com>
2023-05-01 18:46:49 +0400

    <assc

--- src/components/_cabinet/CustomersTrip.vue
+++ src/components/_cabinet/CustomersTrip.vue
@@ -1,5 +1,25 @@
 <script setup>
-import { useRouter } from "vue-router";
+import { ref, onMounted, computed } from "vue";
+import { useRoute } from "vue-router";
+import { useTrips } from "../../stores/trips";
+
+const tripStore = useTrips();
+let route = useRoute();
+
+const _id = route.query.id;
+let trip = ref({});
+
+tripStore
+  .getById(_id)
+  .then((response) => {
+    trip.value = response.data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+console.log(trip.value)
+
 </script>
 <template>
 
