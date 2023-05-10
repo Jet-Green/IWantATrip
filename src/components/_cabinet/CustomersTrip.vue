9615de2069b59ddf661003e248a10f979e65f2f9
tooortttt <savelijsutov@gmail.com>
2023-05-10 17:43:32 +0400

    azcasc

--- src/components/_cabinet/CustomersTrip.vue
+++ src/components/_cabinet/CustomersTrip.vue
@@ -34,49 +34,49 @@ function getPhoneNumber(number) {
 </script>
 
 <template>
-    <a-row :gutter="[16, 16]" :span="24" class="mb-8">
+    <a-col :span="24" class="mb-8">
         <a-breadcrumb>
-            <a-breadcrumb-item @click="router.back()">scszcszc</a-breadcrumb-item>
-            <a-breadcrumb-item>Отклики</a-breadcrumb-item>
+            <a-breadcrumb-item @click="router.back()">{{ trip.name }}</a-breadcrumb-item>
+            <a-breadcrumb-item>Покупатели</a-breadcrumb-item>
         </a-breadcrumb>
-        <a-col :xs="24" :sm="12" :xl="6" v-for="(BILL, bill_index) of trip.billsList">
+    </a-col>
+    <a-col :xs="24" :sm="12" :xl="6" v-for="(BILL, bill_index) of trip.billsList">
 
-            <a-card hoverable v-if="trip.customers[bill_index]" class="pa-8" style="width: 100%;">
-                <div>
-                    <span class="mdi mdi-account-outline" style=""></span> {{ trip.customers[bill_index].fullname }}
-                </div>
-                <div>
-                    <span class="mdi mdi-phone-outline" style=""></span>
-                    <a :href='getPhoneNumber(trip.customers[bill_index].phone)'> {{ trip.customers[bill_index].phone
-                    }}</a>
+        <a-card hoverable v-if="trip.customers[bill_index]" class="pa-8">
+            <div>
+                <span class="mdi mdi-account-outline" style=""></span> {{ trip.customers[bill_index].fullname }}
+            </div>
+            <div>
+                <span class="mdi mdi-phone-outline" style=""></span>
+                <a :href='getPhoneNumber(trip.customers[bill_index].phone)'> {{ trip.customers[bill_index].phone
+                }}</a>
 
-                </div>
-                <div v-for="cartItem of BILL.cart">
-                    {{ cartItem.costType }} {{ cartItem.count }} x {{ cartItem.cost }} руб.
-                </div>
+            </div>
+            <div v-for="cartItem of BILL.cart">
+                {{ cartItem.costType }} {{ cartItem.count }} x {{ cartItem.cost }} руб.
+            </div>
 
-                <div class="d-flex justify-end"> <span>Итого: </span> {{ BILL.cart.reduce((accumulator, object) => {
-                    return accumulator + object.cost * object.count;
-                }, 0) }} руб.
-                </div>
+            <div class="d-flex justify-end"> <span>Итого: </span> {{ BILL.cart.reduce((accumulator, object) => {
+                return accumulator + object.cost * object.count;
+            }, 0) }} руб.
+            </div>
 
-                <div class="d-flex justify-end">
-                    <b>
-                        <span v-if="BILL.isBoughtNow" style="color: #BCC662">
-                            <span class="mdi mdi-check-all" style="font-size: 20px;"></span>
-                            оплачен
-                        </span>
-                        <span v-else style="display: flex; align-items: center;">
-                            <span class="mdi mdi-close" style="font-size: 20px;"></span>
-                            не оплачен
-                        </span>
-                    </b>
-                </div>
+            <div class="d-flex justify-end">
+                <b>
+                    <span v-if="BILL.isBoughtNow" style="color: #BCC662">
+                        <span class="mdi mdi-check-all" style="font-size: 20px;"></span>
+                        оплачен
+                    </span>
+                    <span v-else style="display: flex; align-items: center;">
+                        <span class="mdi mdi-close" style="font-size: 20px;"></span>
+                        не оплачен
+                    </span>
+                </b>
+            </div>
 
-            </a-card>
+        </a-card>
 
-        </a-col>
-    </a-row>
+    </a-col>
 </template>
 
 <style scoped></style>
