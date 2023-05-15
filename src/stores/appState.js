264f2e677a80d4429ae2a5a6635b72338067964f
JetGreen <grishadzyin@gmail.com>
2023-05-15 10:48:56 +0400

    add and delete triptype

--- src/stores/appState.js
+++ src/stores/appState.js
@@ -16,6 +16,31 @@ export const useAppState = defineStore('app-state', {
             } catch (err) {
                 console.log(err);
             }
+        },
+        async addTripType(name) {
+            try {
+                let res = await AppService.setTripType(name)
+                return res
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async deleteTripType(name) {
+            try {
+                if (name) {
+                    let res = await AppService.deleteTripType(name)
+                    if (res.status == 200) {
+                        for (let i = 0; i < this.appState[0].tripType.length; i++) {
+                            if (this.appState[0].tripType[i] == name) {
+                                this.appState[0].tripType.splice(i, 1);
+                            }
+                        }
+                    }
+                    return res
+                }
+            } catch (error) {
+                console.log(error);
+            }
         }
     },
 })
