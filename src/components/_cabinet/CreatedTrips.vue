44b61af181043b766a1e3f046200ac66bdfa16d2
JetGreen <grishadzyin@gmail.com>
2023-05-22 09:20:12 +0400

    add collapse panel

--- src/components/_cabinet/CreatedTrips.vue
+++ src/components/_cabinet/CreatedTrips.vue
@@ -103,135 +103,147 @@ onMounted(async () => {
   }
 
 });
+let activeKey = ref(2)
 </script>
 <template>
   <a-row>
     <a-col :span="24">
-      <h3 class="mt-16" v-if="tripsOnModeration.length">На модерации</h3>
+      <a-collapse v-model:activeKey="activeKey" ghost>
+        <a-collapse-panel v-if="tripsOnModeration.length" key="1" header="На модерации">
+          <a-row :gutter="[8, 8]" class="mt-8" v-if="tripsOnModeration.length > 0">
+            <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of tripsOnModeration" :key="index">
+              <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
+                <div style="text-align:center">
+                  {{ trip.name }}
+                </div>
+                <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
+                <div>
+                  <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
+                </div>
+                <div>
+                  <span class="mdi mdi-calendar-arrow-right"></span>
+                  {{ `c ${clearData(trip.start)}` }}
+                  <span class="mdi mdi-calendar-arrow-left"></span>
+                  {{ `по ${clearData(trip.end)}` }}
+                </div>
 
-      <a-row :gutter="[8, 8]" class="mt-8" v-if="tripsOnModeration.length > 0">
-        <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of tripsOnModeration" :key="index">
-          <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
-            <div style="text-align:center">
-              {{ trip.name }}
-            </div>
-            <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
-            <div>
-              <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
-            </div>
-            <div>
-              <span class="mdi mdi-calendar-arrow-right"></span>
-              {{ `c ${clearData(trip.start)}` }}
-              <span class="mdi mdi-calendar-arrow-left"></span>
-              {{ `по ${clearData(trip.end)}` }}
-            </div>
+                <div class="actions d-flex justify-center">
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
+                    v-if="!trip.billsList.length > 0">
+                    <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
+                    <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
 
-            <div class="actions d-flex justify-center">
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
-                v-if="!trip.billsList.length > 0">
-                <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
-                <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-
-              <span class="mdi mdi-information-outline"
-                @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
-                v-if="trip.billsList.length"></span>
-              <span class="mdi mdi-email-outline" v-if="trip.moderationMessage"
-                @click="showMessage = !showMessage"></span>
-            </div>
-            <div v-if="showMessage">
-              Замечания: {{ trip.moderationMessage }}
-            </div>
-          </a-card>
-        </a-col>
-      </a-row>
+                  <span class="mdi mdi-information-outline"
+                    @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
+                    v-if="trip.billsList.length"></span>
+                  <span class="mdi mdi-email-outline" v-if="trip.moderationMessage"
+                    @click="showMessage = !showMessage"></span>
+                </div>
+                <div v-if="showMessage">
+                  Замечания: {{ trip.moderationMessage }}
+                </div>
+              </a-card>
+            </a-col>
+          </a-row>
+        </a-collapse-panel>
 
 
+        <a-collapse-panel key="2" header="Действующие туры">
+          <a-row :gutter="[8, 8]" class="mt-8" v-if="trips.length > 0">
+            <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of trips" :key="index">
+              <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
+                <div style="text-align:center">
+                  {{ trip.name }}
+                </div>
+                <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
+                <div>
+                  <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
+                </div>
+                <div>
+                  <span class="mdi mdi-calendar-arrow-right"></span>
+                  {{ `c ${clearData(trip.start)}` }}
+                  <span class="mdi mdi-calendar-arrow-left"></span>
+                  {{ `по ${clearData(trip.end)}` }}
+                </div>
 
-      <h3>Действующие туры</h3>
-      <a-row :gutter="[8, 8]" class="mt-8" v-if="trips.length > 0">
-        <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of trips" :key="index">
-          <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
-            <div style="text-align:center">
-              {{ trip.name }}
-            </div>
-            <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
-            <div>
-              <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
-            </div>
-            <div>
-              <span class="mdi mdi-calendar-arrow-right"></span>
-              {{ `c ${clearData(trip.start)}` }}
-              <span class="mdi mdi-calendar-arrow-left"></span>
-              {{ `по ${clearData(trip.end)}` }}
-            </div>
+                <div class="actions d-flex justify-center">
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
+                    v-if="!trip.billsList.length > 0">
+                    <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
+                    <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="hideTrip(trip._id)">
+                    <span v-if="!trip.isHidden" class="mdi mdi-eye" style="color: #245159; cursor: pointer"></span>
+                    <span v-else class="mdi mdi-eye-off" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="copyTrip(trip._id)">
+                    <span class="mdi mdi-content-copy" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <span class="mdi mdi-information-outline"
+                    @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
+                    v-if="trip.billsList.length"></span>
+                </div>
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row :lg="8" :sm="12" :xs="24" v-else>
+            Нет туров
+          </a-row>
+        </a-collapse-panel>
 
-            <div class="actions d-flex justify-center">
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
-                v-if="!trip.billsList.length > 0">
-                <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
-                <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="hideTrip(trip._id)">
-                <span v-if="!trip.isHidden" class="mdi mdi-eye" style="color: #245159; cursor: pointer"></span>
-                <span v-else class="mdi mdi-eye-off" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="copyTrip(trip._id)">
-                <span class="mdi mdi-content-copy" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-              <span class="mdi mdi-information-outline"
-                @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
-                v-if="trip.billsList.length"></span>
-            </div>
-          </a-card>
-        </a-col>
-      </a-row>
-      <a-row :lg="8" :sm="12" :xs="24" v-else>
-        Нет туров
-      </a-row>
-      <h3>Архивные туры</h3>
-      <a-row :gutter="[8, 8]" class="mt-8" v-if="archiveTrips.length > 0">
-        <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of archiveTrips" :key="index">
-          <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
-            <div style="text-align:center">
-              {{ trip.name }}
-            </div>
-            <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
-            <div>
-              <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
-            </div>
-            <div>
-              <span class="mdi mdi-calendar-arrow-right"></span>
-              {{ `c ${clearData(trip.start)}` }}
-              <span class="mdi mdi-calendar-arrow-left"></span>
-              {{ `по ${clearData(trip.end)}` }}
-            </div>
 
-            <div class="actions d-flex justify-center">
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
-                v-if="!trip.billsList.length > 0">
-                <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
-                <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-              <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="copyTrip(trip._id)">
-                <span class="mdi mdi-content-copy" style="color: #245159; cursor: pointer"></span>
-              </a-popconfirm>
-              <span class="mdi mdi-information-outline"
-                @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
-                v-if="trip.billsList.length"></span>
-            </div>
-          </a-card>
-        </a-col>
-      </a-row>
-      <a-row :lg="8" :sm="12" :xs="24" v-else>
-        Нет туров
-      </a-row>
+        <a-collapse-panel key="3" header="Архивные туры">
+          <a-row :gutter="[8, 8]" class="mt-8" v-if="archiveTrips.length > 0">
+            <a-col :lg="8" :sm="12" :xs="24" v-for="(trip, index) of archiveTrips" :key="index">
+              <a-card class="card " hoverable :class="[trip.isHidden ? 'overlay' : '']">
+                <div style="text-align:center">
+                  {{ trip.name }}
+                </div>
+                <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
+                <div>
+                  <span class="mdi mdi-compass-outline"></span>{{ trip.location }}
+                </div>
+                <div>
+                  <span class="mdi mdi-calendar-arrow-right"></span>
+                  {{ `c ${clearData(trip.start)}` }}
+                  <span class="mdi mdi-calendar-arrow-left"></span>
+                  {{ `по ${clearData(trip.end)}` }}
+                </div>
+
+                <div class="actions d-flex justify-center">
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="tripToDelete(trip._id)"
+                    v-if="!trip.billsList.length > 0">
+                    <span class="mdi mdi-delete" style="color: #ff6600; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="editTrip(trip._id)">
+                    <span class="mdi mdi-pen" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <a-popconfirm title="Вы уверены?" ok-text="Да" cancel-text="Нет" @confirm="copyTrip(trip._id)">
+                    <span class="mdi mdi-content-copy" style="color: #245159; cursor: pointer"></span>
+                  </a-popconfirm>
+                  <span class="mdi mdi-information-outline"
+                    @click="router.push({ path: 'customers-trip', query: { id: trip._id } })"
+                    v-if="trip.billsList.length"></span>
+                </div>
+              </a-card>
+            </a-col>
+          </a-row>
+          <a-row :lg="8" :sm="12" :xs="24" v-else>
+            Нет туров
+          </a-row>
+        </a-collapse-panel>
+      </a-collapse>
+
+
+
+
+
+
     </a-col>
   </a-row>
 </template>
