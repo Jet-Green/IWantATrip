b31a2716ff865cc41ea72d3744b0dc458a091a61
regtyit <titov54275@gmail.com>
2023-05-24 08:42:26 +0400

    validation CreateTripWithHelp

--- src/views/CreateTripWithHelp.vue
+++ src/views/CreateTripWithHelp.vue
@@ -8,7 +8,8 @@ import BookingService from "../service/BookingService";
 import { useAuth } from "../stores/auth";
 import { useAppState } from "../stores/appState";
 import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
-
+import * as yup from "yup";
+import { Form, Field, ErrorMessage } from "vee-validate";
 let breakpoints = useBreakpoints(breakpointsTailwind);
 let sm = breakpoints.smaller("md");
 
@@ -94,6 +95,20 @@ onMounted(() => {
       : (userInfo.phone = "");
   }
 });
+
+let formSchema = yup.object({
+  start: yup.object().required("заполните поле"),
+  end: yup.object().required("заполните поле"),
+  type: yup.string().required("заполните поле"),
+  location: yup.string().required("заполните поле"),
+  adults: yup.string().required("заполните поле"),
+  children: yup.string().required("заполните поле"),
+  fromAge: yup.string().required("заполните поле"),
+  wishes: yup.string().required("заполните поле"),
+  // distance: yup.string().required("заполните поле"),
+  // cost: yup.string().required("заполните поле"),
+  // https://vee-validate.logaretm.com/v4/examples/array-fields/
+});
 </script>
 <template>
   <div>
@@ -103,77 +118,156 @@ onMounted(() => {
 
     <img v-if="!sm" src="../assets/images/booking_right.png" style="position: fixed; right: 0px; bottom: 0px; width: 20% " />
 
-    <form action="POST" @submit.prevent="submit" enctype="multipart/form-data">
+    <Form :validation-schema="formSchema" v-slot="{ meta }" @submit.prevent="submit">
       <a-row type="flex" justify="center">
         <a-col :xs="22" :lg="12">
           <h2>О вас</h2>
           <a-row :gutter="[16, 16]">
             <a-col :span="24" :md="12">
-              Фaмилия Имя
-              <a-input style="width: 100%" v-model:value="userInfo.fullname" placeholder="Иванов Иван Иванович" />
+              <Field name="fullname" v-slot="{ value, handleChange }" v-model="userInfo.fullname">
+                Фaмилия Имя
+
+                  <a-input style="width: 100%" @update:value="handleChange" :value="value" placeholder="Иванов Иван Иванович" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="fullname" class="error-message" />
+              </Transition>
             </a-col>
             <a-col :span="24" :md="12">
-              Телефон
-              <a-input style="width: 100%" v-model:value="userInfo.phone" placeholder="79127528874" />
+              <Field name="phone" v-slot="{ value, handleChange }" v-model="userInfo.phone">
+                Телефон
+
+                  <a-input style="width: 100%" @update:value="handleChange" :value="value" placeholder="79127528874" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="phone" class="error-message" />
+              </Transition>
             </a-col>
             <a-col :span="24">
               <h2>Заказать тур</h2>
               <div>
-                Тип тура
-                <a-select v-model:value="form.type" style="width: 100%" mode="multiple">
-                  <a-select-option v-for="(value, index) in appStore.appState[0].tripType" :key="index" :value="value">{{
-                    value
-                  }}</a-select-option>
-                </a-select>
+                <Field name="type" v-slot="{ value, handleChange }" v-model="form.type">
+                Tип тура
+
+                <a-select @update:value="handleChange" :value="value" style="width: 100%">
+                    <a-select-option v-for=" tripType  in  appStore.appState[0].tripType " :value="tripType">{{ tripType
+                    }}</a-select-option>
+                  </a-select>
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="tripType" class="error-message" />
+              </Transition>
               </div>
             </a-col>
 
             <a-col :span="12">
-              Дата начала
-              <a-date-picker v-model:value="form.start" style="width: 100%" placeholder="Начало"
-                :format="dateFormatList" />
+              <Field name="start" v-slot="{ value, handleChange }" v-model="form.start">
+                Дата начала
+                <a-date-picker @update:value="handleChange" :value="value" style="width: 100%" placeholder="Начало"
+                  :locale="ruLocale" :format="dateFormatList" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="start" class="error-message" />
+              </Transition>
             </a-col>
+
             <a-col :span="12">
-              Дата конца
-              <a-date-picker v-model:value="form.end" style="width: 100%" placeholder="Конец" :format="dateFormatList" />
+              <Field name="end" v-slot="{ value, handleChange }" v-model="form.end">
+                Дата конца
+                <a-date-picker @update:value="handleChange" :value="value" style="width: 100%" placeholder="Конец"
+                  :locale="ruLocale" :format="dateFormatList" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="end" class="error-message" />
+              </Transition>
             </a-col>
 
-            <a-col :xs="24" :md="12">Направление
-              <a-input placeholder="Байкал" size="large" v-model:value="form.location"></a-input>
-            </a-col>
             <a-col :xs="24" :md="12">
-              Продолжительность, дн.
-              <a-input-number id="inputNumber" v-model:value="form.duration" style="width: 100%" placeholder="5"
-                :min="1" />
+              <Field name="location" v-slot="{ value, handleChange }" v-model:value="form.location">
+                Направление
+
+                  <a-input style="width: 100%" @update:value="handleChange" :value="value" placeholder="Байкал" size="large" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="location" class="error-message" />
+              </Transition>
             </a-col>
 
+            <a-col :xs="24" :md="12">
+              <Field name="duration" v-slot="{ value, handleChange }" v-model:value="form.duration">
+                Продолжительность, дн.
+
+                  <a-input-number  id="inputNumber" style="width: 100%" @update:value="handleChange" :value="value" placeholder="5"
+                :min="1" size="large" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="duration" class="error-message" />
+              </Transition>
+
+            </a-col>
+
+
             <a-col :xs="24" :md="8">
-              Взрослые
-              <a-input-number :min="0" style="width: 100%" placeholder="2" v-model:value="form.adults" />
+              <a-col :span="12">
+              <Field name="adults" v-slot="{ value, handleChange }" v-model="form.adults">
+                Взрослые
+                <a-input-number :min="0" @update:value="handleChange" :value="value"  placeholder="2"
+                  />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="adults" class="error-message" />
+              </Transition>
+            </a-col>
+              
+
             </a-col>
             <a-col :xs="24" :md="8">
-              Дети
-              <a-input-number :min="0" style="width: 100%" placeholder="1" v-model:value="form.children" />
+              <a-col :span="12">
+              <Field name="children" v-slot="{ value, handleChange }" v-model="form.children">
+                Дети
+                <a-input-number @update:value="handleChange" :value="value"  placeholder="1"
+                  :min="0" />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="children" class="error-message" />
+              </Transition>
             </a-col>
-            <a-col :xs="24" :md="8">Мин. возраст, лет
-              <a-input-number id="inputNumber" v-model:value="form.fromAge" style="width: 100%" placeholder="10" :min="0"
-                :max="100" />
+              
+
+            </a-col>
+            <a-col :xs="24" :md="8">
+              <a-col :span="12">
+              <Field name="fromAge" v-slot="{ value, handleChange }" v-model="form.fromAge">
+                Мин.возраст,лет
+                <a-input-number id="inputNumber" @update:value="handleChange" :value="value"  placeholder="10"
+                  :min="0" :max="100"/>
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="fromAge" class="error-message" />
+              </Transition>
+            </a-col>
             </a-col>
 
             <a-col :xs="24">
-              Пожелания
+              <Field name="wishes" v-slot="{ value, handleChange }" v-model="form.wishes">
+                Пожелания
+                <a-textarea  @update:value="handleChange" :value="value" autoSize
+                  />
+              </Field>
+              <Transition name="fade">
+                <ErrorMessage name="wishes" class="error-message" />
+              </Transition>
 
-              <a-textarea autoSize v-model:value="form.wishes" />
             </a-col>
           </a-row>
 
           <a-row type="flex" justify="center">
             <a-col :xs="24" :md="16" :lg="10" class="d-flex justify-center">
-              <a-button type="primary" html-type="submit" class="lets_go_btn mt-8" size="large">Отправить</a-button>
+              <a-button type="primary" :disabled="!meta.valid" html-type="submit" class="lets_go_btn mt-8" size="large">Отправить</a-button>
             </a-col>
           </a-row>
         </a-col>
       </a-row>
-    </form>
+    </Form>
   </div>
 </template>
