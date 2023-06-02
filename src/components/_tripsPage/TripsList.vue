e20ebaf64af3e484fd3811b8f39aa8d78cf7486a
Jet-Green <grishadzyin@gmail.com>
2023-06-02 15:36:34 +0400

    fix scrolling

--- src/components/_tripsPage/TripsList.vue
+++ src/components/_tripsPage/TripsList.vue
@@ -29,9 +29,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <a-row class="d-flex justify-center">
-    <a-col :xs="22" :lg="16">
-      <div class="wrapper" ref="wrapper">
+  <div class="wrapper" ref="wrapper">
+    <a-row class="d-flex justify-center">
+      <a-col :xs="22" :lg="16">
         <a-row :gutter="[16, 16]" class="d-flex justify-center mt-8 ">
           <a-col v-if="tripStore.filteredTrips.length == 0" :xs="24" :sm="12" :md="8" :lg="6"
             v-for="trip in tripStore.trips" :key="trip.index">
@@ -44,9 +44,9 @@ onMounted(() => {
           </a-col>
 
         </a-row>
-      </div>
-    </a-col>
-  </a-row>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 <style lang="scss" scoped>
 .wrapper {
