070c7735b13a076afabd0c31e5acfe27433a5847
Jet-Green <grishadzyin@gmail.com>
2023-06-06 16:22:04 +0400

    add customer

--- src/components/_cabinet/CustomersTrip.vue
+++ src/components/_cabinet/CustomersTrip.vue
@@ -1,12 +1,17 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
-import { useTrips } from "../../stores/trips";
 import { useRouter } from "vue-router";
+
+import { message } from "ant-design-vue";
+
+import { useAuth } from "../../stores/auth";
+import { useTrips } from "../../stores/trips";
 
 const router = useRouter();
 const route = useRoute();
 
+const userStore = useAuth()
 const tripStore = useTrips();
 
 let customers = ref([]);
@@ -15,7 +20,34 @@ const payedBooks = ref({});
 const pricesSet = new Set();
 const _id = route.query.id;
 let trip = ref({});
+let userInfo = ref({
+    fullname: '',
+    phone: ''
+})
+let selectedByUser = ref([])
 
+let addCustomerDialog = ref(false)
+
+let tripsCount = computed(() => {
+    let sum = 0;
+    for (let i = 0; i < trip.value.billsList.length; i++) {
+        if (trip.value.billsList[i]) {
+            for (let j = 0; j < trip.value.billsList[i].cart?.length; j++) {
+                sum += trip.value.billsList[i].cart[j].count;
+            }
+        }
+    }
+    return sum;
+});
+let finalCost = computed(() => {
+    let sum = 0;
+    if (selectedByUser.value.length) {
+        for (let c of selectedByUser.value) {
+            sum += c.cost * c.count;
+        }
+    }
+    return sum;
+});
 async function setPayment(bill) {
     let res = await tripStore.setPayment(bill._id)
     if (res.status == 200) {
@@ -36,12 +68,45 @@ async function deletePayment(bill) {
         }
     }
 }
+async function buyTrip(isBoughtNow) {
+    if (userInfo.value.phone) {
+        let bill = {
+            isBoughtNow,
+            cart: [...selectedByUser.value],
+            tripId: trip.value._id,
+            userInfo: {
+                _id: null,
+                fullname: userInfo.value.fullname,
+                phone: userInfo.value.phone,
+            }
+        };
+
+        for (let i = 0; i < bill.cart.length; i++) {
+            if (bill.cart[i].count == 0) {
+                bill.cart.splice(i, 1);
+            }
+        }
+
+        await userStore
+            .buyTrip(trip.value._id, bill)
+            .then(() => {
+                message.config({ duration: 3, top: "90vh" });
+                message.success({ content: "Тур заказан!" });
+                addCustomerDialog.value = false;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        message.config({ duration: 3, top: "90vh" });
+        message.error({ content: "Нужен телефон" });
+    }
+}
 onMounted(async () => {
     let { data } = await tripStore.getFullTripById(route.query._id)
     trip.value = data;
 
     for (let book of trip.value.billsList) {
-        // if (book.isBoughtNow == true)
         for (let cart of book.cart) {
             let obj = {};
             if (!pricesSet.has(cart.costType)) {
@@ -54,7 +119,7 @@ onMounted(async () => {
                 allBooks.value[cart.costType].push(obj);
                 payedBooks.value[cart.costType].push(obj);
             } else {
-                allBooks.value[cart.costType].push(obj);;
+                allBooks.value[cart.costType].push(obj);
             }
         }
     }
@@ -75,7 +140,13 @@ onMounted(async () => {
             ;
         allBooks.value[pr] = y
     }
-
+    for (let cost of trip.value.cost) {
+        selectedByUser.value.push({
+            cost: cost.price,
+            count: 0,
+            costType: cost.first,
+        });
+    }
 });
 
 function getPhoneNumber(number) {
@@ -93,6 +164,11 @@ function getPhoneNumber(number) {
             <a-breadcrumb-item>Покупатели</a-breadcrumb-item>
         </a-breadcrumb>
     </a-col>
+    <a-row class="mb-8">
+        <a-col :span="24">
+            <a-button size="large" class="lets_go_btn" @click="addCustomerDialog = true">добавить покупателя</a-button>
+        </a-col>
+    </a-row>
 
     <a-row :gutter="[8, 8]">
         <a-col :lg="8" :sm="12" :xs="24">
@@ -156,6 +232,41 @@ function getPhoneNumber(number) {
             </div>
         </a-col>
     </a-row>
+    <a-modal v-model:visible="addCustomerDialog" :footer="null">
+        <a-row :gutter="[4, 4]">
+            <a-col :span="24" :md="12">
+                Фaмилия Имя
+                <a-input style="width: 100%" v-model:value="userInfo.fullname" placeholder="Иванов Иван Иванович" />
+            </a-col>
+            <a-col :span="24" :md="12">
+                Телефон
+                <a-input style="width: 100%" v-model:value="userInfo.phone" placeholder="79127528874" />
+            </a-col>
+
+            <a-col :span="24">
+                <div>Цена</div>
+                <div class="d-flex space-around align-center" v-for="(cost, index) of trip.cost" :key="index">
+                    {{ cost.first }}<span>{{ cost.price }} руб. </span>
+                    <div class="d-flex direction-column">
+                        <span style="font-size: 8px">кол-во</span>
+                        <a-input-number v-model:value="selectedByUser[index].count" :min="0"
+                            :max="trip.maxPeople - tripsCount" placeholder="чел"></a-input-number>
+                    </div>
+                </div>
+            </a-col>
+            <a-col :span="24">
+                <b>Итого: {{ finalCost }} руб.</b>
+            </a-col>
+
+            <a-col :span="24">
+                <div>Оплатить</div>
+                <div class="d-flex space-around">
+                    <a-button type="primary" @click="buyTrip(true)"> сейчас </a-button>
+                    <a-button @click="buyTrip(false)"> потом </a-button>
+                </div>
+            </a-col>
+        </a-row>
+    </a-modal>
 </template>
 
 <style scoped></style>
