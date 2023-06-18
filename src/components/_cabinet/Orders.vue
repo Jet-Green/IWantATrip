7d758ad7944592e1443979d3d73637dfbdef4dac
qbclub <17926057+qbclub@users.noreply.github.com>
2023-06-18 13:02:11 +0400

    orders card and methods

--- src/components/_cabinet/Orders.vue
+++ src/components/_cabinet/Orders.vue
@@ -9,15 +9,18 @@ let inWork = ref([])
 let closed = ref([])
 
 const clearData = (dateNumber) => {
-  let date = new Date(dateNumber).toLocaleDateString("ru-Ru", {
-    year: "2-digit",
-    month: "2-digit",
-    day: "2-digit",
-  })
-  if (date !== 'Invalid Date' && date) {
-    return date
-  }
-  return ''
+    let date = new Date(dateNumber).toLocaleDateString("ru-Ru", {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+    })
+    if (date !== 'Invalid Date' && date) {
+        return date
+    }
+    return ''
+}
+function getPhoneNumber(number) {
+  return `tel:${number}`;
 }
 onMounted(async () => {
     open.value = await tripStore.getBookingTrips('open')
@@ -29,42 +32,46 @@ onMounted(async () => {
     <h1>Новые</h1>
     <a-row :gutter="[8, 8]" class="mt-8">
         <a-col :lg="8" :sm="12" :xs="24" v-for="booking in open" v-if="open.length">
-          <a-card class="card " hoverable>
-            <div>
-              {{ booking.creatorId }}
-            </div>
-            <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
-            <div>
-              Тур: {{ booking.type.toString().toLowerCase() }}
-            </div>
-            <div>
-              C {{ clearData(booking.start) }} по {{ clearData(booking.end) }}
-            </div>
+            <a-card class="card " hoverable>
+                <div>
+                   Имя:  <b> {{ booking.creatorId.fullinfo.fullname }}</b>
+                </div>
+                <div>
+                     Телефон: <b> <a :href="getPhoneNumber(booking.creatorId.fullinfo.phone )"> {{ booking.creatorId.fullinfo.phone }}</a> </b>
 
-            <div>
-              На {{ booking.duration }} дн.
-            </div>
-            <div v-if="booking.adults">
-              Взрослых: {{ booking.adults }} чел.
-            </div>
-            <div v-if="booking.children">
-              Детей: {{ booking.children }} чел. от {{ booking.fromAge }} лет
-            </div>
-            <div>
-              Пожелания: {{ booking.wishes }}
-            </div>
-            <div>
-              № заказа: {{ booking._id }}
-            </div>
+                </div>
+                <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
+                <div>
+                    Тур: {{ booking.type.toString().toLowerCase() }}
+                </div>
+                <div>
+                    C {{ clearData(booking.start) }} по {{ clearData(booking.end) }}
+                </div>
 
-          </a-card>
+                <div>
+                    На {{ booking.duration }} дн.
+                </div>
+                <div v-if="booking.adults">
+                    Взрослых: {{ booking.adults }} чел.
+                </div>
+                <div v-if="booking.children">
+                    Детей: {{ booking.children }} чел. от {{ booking.fromAge }} лет
+                </div>
+                <div>
+                    Пожелания: {{ booking.wishes }}
+                </div>
+                <div>
+                    № заказа: {{ booking._id }}
+                </div>
+
+            </a-card>
 
         </a-col>
         <a-col v-else>
-          <span>У вас нет заказанных туров</span>
+            <span>У вас нет заказанных туров</span>
         </a-col>
-      </a-row>
-   
+    </a-row>
+
 
     <h1>В работе</h1>
     {{ inWork }}
