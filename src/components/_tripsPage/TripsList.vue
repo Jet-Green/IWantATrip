238dc37d358f1d2162fb70dd5a0cfb6ed9d06747
JetGreen <grishadzyin@gmail.com>
2023-06-22 16:13:46 +0400

    add fetch onMounted

--- src/components/_tripsPage/TripsList.vue
+++ src/components/_tripsPage/TripsList.vue
@@ -23,32 +23,35 @@ let handleScroll = async () => {
 }
 
 const wrapper = ref(null)
-onMounted(() => {
+onMounted(async () => {
   wrapper.value.addEventListener("scroll", handleScroll);
+  if (tripStore.trips.length == 0) {
+    await tripStore.fetchTrips()
+  }
 });
 </script>
 <template>
   <div>
     <BackButton />
-  <div class="wrapper" ref="wrapper">
-    <a-row type="flex" justify="center">
-      <a-col :xs="22" :lg="16">
-        <h2>Туры</h2>
-      </a-col>
-    </a-row>
-    <TripFilter :search="route.query.search" />
-    <a-row class="d-flex justify-center">
-      <a-col :xs="22" :lg="16">
-        <a-row :gutter="[16, 16]" class="d-flex justify-center mt-8 pb-24">
-          <a-col :xs="24" :sm="12" :md="8" :lg="6" v-for="trip in tripStore.trips" :key="trip.index">
-            <TripListCard :trip="trip" />
-          </a-col>
+    <div class="wrapper" ref="wrapper">
+      <a-row type="flex" justify="center">
+        <a-col :xs="22" :lg="16">
+          <h2>Туры</h2>
+        </a-col>
+      </a-row>
+      <TripFilter :search="route.query.search" />
+      <a-row class="d-flex justify-center">
+        <a-col :xs="22" :lg="16">
+          <a-row :gutter="[16, 16]" class="d-flex justify-center mt-8 pb-24">
+            <a-col :xs="24" :sm="12" :md="8" :lg="6" v-for="trip in tripStore.trips" :key="trip.index">
+              <TripListCard :trip="trip" />
+            </a-col>
 
-        </a-row>
-      </a-col>
-    </a-row>
+          </a-row>
+        </a-col>
+      </a-row>
+    </div>
   </div>
-</div>
 </template>
 <style lang="scss" scoped>
 .wrapper {
