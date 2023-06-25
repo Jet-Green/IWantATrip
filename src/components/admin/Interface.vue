a7d972292d60a2a541c18106b1f87b5b425815fe
JetGreen <grishadzyin@gmail.com>
2023-06-25 14:25:42 +0400

    delete old version of emails

--- src/components/admin/Interface.vue
+++ src/components/admin/Interface.vue
@@ -1,14 +1,10 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useAppState } from '../../stores/appState';
 
 let appStateStore = useAppState()
 let tripType = ref('')
 
-let sendTo = ref('')
-let selectedEmailTypes = ref([])
-
-let emailsFromDb = ref([])
 
 async function addTripType() {
     let res = await appStateStore.addTripType(tripType.value)
@@ -17,48 +13,10 @@ async function addTripType() {
     }
 }
 
-async function addEmail() {
-    if (selectedEmailTypes.value && sendTo.value.length) {
-        let res = await appStateStore.addEmail(sendTo.value, selectedEmailTypes.value)
-    }
-}
-
-async function changeEmailEvents(email, event, checked) {
-    let updateQuery = { $push: {}, $pull: {} }
-    if (checked) {
-        updateQuery.$push['sendMailsTo.' + event] = email
-    } else {
-        updateQuery.$pull['sendMailsTo.' + event] = email
-    }
-
-    let res = await appStateStore.updateEmails(updateQuery)
-}
-function convertEmailsFromDbToRef() {
-    for (let key of Object.keys(appStateStore.appState[0].sendMailsTo)) {
-        for (let i = 0; i < appStateStore.appState[0].sendMailsTo[key].length; i++) {
-            if (!emailsFromDb.value.find((element) => element.email == appStateStore.appState[0].sendMailsTo[key][i])) {
-                emailsFromDb.value.push({ email: appStateStore.appState[0].sendMailsTo[key][i], checkboxes: [] })
-            }
-            for (let j = 0; j < emailsFromDb.value.length; j++) {
-                if (emailsFromDb.value[j].email == appStateStore.appState[0].sendMailsTo[key][i]) {
-                    emailsFromDb.value[j].checkboxes.push({ value: key, label: key, checked: true })
-                }
-            }
-        }
-    }
-    for (let e of emailsFromDb.value) {
-        for (let key of Object.keys(appStateStore.appState[0].sendMailsTo)) {
-            if (!e.checkboxes.find((el) => el.value == key)) {
-                e.checkboxes.push({ value: key, label: key, checked: false })
-            }
-        }
-    }
-}
 onMounted(async () => {
     if (!appStateStore.appState) {
         await appStateStore.refreshState();
     }
-    convertEmailsFromDbToRef()
 })
 </script>
 <template>
@@ -77,38 +35,6 @@ onMounted(async () => {
                         @confirm="() => { appStateStore.deleteTripType(t) }">
                         {{ t }}
                     </a-popconfirm>
-                </a-col>
-            </a-row>
-            <a-row class="mt-8" :gutter="[16, 8]">
-                <a-col :span="24">
-                    <h3>email-уведомления</h3>
-                </a-col>
-
-                <a-col :span="24" class="d-flex align-center">
-                    <a-input placeholder="email@gmail.com" size="large" v-model:value="sendTo"></a-input>
-                </a-col>
-                <a-col :span="24">
-                    <a-select v-model:value="selectedEmailTypes" placeholder="Выберите когда отправлять"
-                        :options="[{ label: 'Создание тура', value: 'CreateTrip' }, { label: 'Создание попутчика', value: 'CreateCompanion' }, { label: 'Заказ тура', value: 'BookingTrip' }]"
-                        style="width: 100%" size="large" mode="tags">
-                    </a-select>
-                </a-col>
-                <a-col :span="24" class="d-flex justify-center">
-                    <a-button type="primary" class="ml-12 lets_go_btn" @click="addEmail">добавить</a-button>
-                </a-col>
-            </a-row>
-            <a-row class="mt-16 d-flex" :gutter="[16, 16]">
-                <a-col v-for="email of emailsFromDb">
-                    <a-card class="pa-8">
-                        <p>
-                            {{ email.email }}
-                        </p>
-                        <div>
-                            <a-checkbox v-for="ch of email.checkboxes" v-model:checked="ch.checked"
-                                @change="changeEmailEvents(email.email, ch.value, ch.checked)">{{ ch.label
-                                }}</a-checkbox>
-                        </div>
-                    </a-card>
                 </a-col>
             </a-row>
         </a-col>
