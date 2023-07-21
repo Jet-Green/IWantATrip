2249b59d6cf48a8c2661b2ad2996c55a9166abbc
JetGreen <grishadzyin@gmail.com>
2023-07-21 16:36:40 +0400

    add script

---
+++ src/components/admin/Scripts.vue
@@ -0,0 +1,11 @@
+<script setup>
+import axios from 'axios'
+
+async function addFullInfoToBill() {
+    let res = await axios.get(import.meta.env.VITE_API_URL + '/service-functions/add-fullinfo-to-bills')
+    console.log(res.status);
+}
+</script>
+<template>
+    <a-button @click="addFullInfoToBill">добавить fullinfo в каждый объект bill</a-button>
+</template>
