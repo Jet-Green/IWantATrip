ab6591be9e899b720171210c191e6de43d63cb6f
qbclub <17926057+qbclub@users.noreply.github.com>
2023-09-05 12:44:27 +0400

    v4

--- src/App.vue
+++ src/App.vue
@@ -43,18 +43,39 @@ onMounted(async () => {
   //   })
   // }
 })
+
+// @success-color: #BCC662; // success state color
+// @primary-color: #ff6600; // primary color for all components
+// @layout-header-background: rgba(255, 255, 255);
+// @layout-body-background: rgba(0, 0, 0, 0);
+// @card-padding-base: 0px;
+
 </script>
 <template>
-  <a-layout>
-    <Header></Header>
-    <!-- в документации с margin'ом, чтобы предотвратить перекрывание контента хедром -->
-    <a-layout-content style="margin-top: 69px">
-      <router-view v-slot="{ Component }">
-        <transition name="fade" mode="out-in">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </a-layout-content>
-  </a-layout>
+  <a-config-provider :theme="{
+    token: {
+      colorPrimary: '#ff6600',
+      colorSuccess: '#BCC662',
+      borderRadius: 0,
+      fontFamily: 'Montserrat',
+      colorBgLayout: 'white'
+
+
+    },
+  }">
+    <a-layout>
+      <Header></Header>
+      <!-- в документации с margin'ом, чтобы предотвратить перекрывание контента хедром -->
+      <a-layout-content style="margin-top: 69px">
+        <router-view v-slot="{ Component }">
+          <transition name="fade" mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-config-provider>
 </template>
-<style></style>
+<style>
+
+</style>
