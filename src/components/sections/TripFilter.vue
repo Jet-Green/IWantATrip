ef4a93c433ad3094f8dfa1811679e9a976be111e
SaveliyShutov <savelijsutov@gmail.com>
2023-09-19 16:55:29 +0400

    add trip filter rabotaett!!!

--- src/components/sections/TripFilter.vue
+++ src/components/sections/TripFilter.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useTrips } from '../../stores/trips.js';
 import { useAppState } from "../../stores/appState";
@@ -28,9 +28,9 @@ let loading = ref(false)
 let visible = ref(false);
 
 function find() {
-  console.log(type.value);
   query.value = query.value.trim()
   localStorage.setItem("TripQuery", query.value)
+  localStorage.setItem("TripType", type.value)
   tripStore.searchCursor = 1
   tripStore.filteredTrips = []
   tripStore.cursor = 1
@@ -66,9 +66,17 @@ function find() {
   }
 }
 
+function resetForm() {
+  time.value = '';
+  query.value = '';
+  type.value = '';
+  find()
+}
+
 
 onMounted(() => {
   query.value = localStorage.getItem("TripQuery") ?? '';
+  type.value = localStorage.getItem("TripType") ?? '';
   if (localStorage.getItem("TripfilterDate")) {
     let arr = localStorage.getItem("TripfilterDate").split(',').map(function (date) {
       return dayjs(new Date(+date))
@@ -83,30 +91,33 @@ onMounted(() => {
 <template>
   <a-row type="flex" justify="center" align="middle" class="section_bg ">
     <a-col :xs="22" :md="12">
-      <a-row class="mb-8" type="flex" justify="center">
+      <a-row class="mb-4" justify="center">
         <a-col :xs="24" :md="12" class="d-flex">
           <a-input-search v-model:value="query" placeholder="поиск" enter-button style="z-index: 0" @search="find()" />
-          <span class="mdi mdi-24px mdi-filter-outline ml-16" :class="{ active_filter: visible, filter: !visible }"
+          <span class="ml-16 mdi mdi-24px mdi-filter-outline" :class="{ active_filter: visible, filter: !visible }"
             @click="visible = !visible">
           </span>
         </a-col>
       </a-row>
       <Transition name="fade">
         <div v-if="visible">
-          <a-row type="flex" justify="center" class="mt-16">
+          <a-row class="mb-8" type="flex" justify="center" :xs="24" :md="12">
             <a-col :xs="24" :md="12">
               <a-range-picker style="width: 100%" v-model:value="time" :locale="ruLocale" />
             </a-col>
+          </a-row>
+          <a-row type="flex" justify="center">
             <a-col :xs="24" :md="12">
-              <a-select v-model:value="type" style="width: 100%">
+              <a-select placeholder="Tип тура" v-model:value="type" style="width: 100%">
                 <a-select-option v-for="   tripType    in    appStore.appState[0].tripType   " :value="tripType">{{
                   tripType
                 }}</a-select-option>
               </a-select>
             </a-col>
           </a-row>
-          <a-row type="flex" justify="center" class="mt-8">
+          <a-row type="flex" justify="center" class="mt-16 mb-8">
             <a-button @click="find" type="primary" class="lets_go_btn">поиск</a-button>
+            <a-button @click="resetForm" style="border-radius: 15px; margin-left: 10px;">отчистить</a-button>
           </a-row>
         </div>
       </Transition>
@@ -128,4 +139,6 @@ onMounted(() => {
   color: #227597;
   cursor: pointer;
 }
+
+.a-button {}
 </style>
