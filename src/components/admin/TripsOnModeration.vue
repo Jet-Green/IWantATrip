9c7e215dbcd487bd599df5d231ac200a3ba1a52c
SaveliyShutov <savelijsutov@gmail.com>
2023-10-02 21:27:05 +0400

    add storage to moderate trips

--- src/components/admin/TripsOnModeration.vue
+++ src/components/admin/TripsOnModeration.vue
@@ -1,18 +1,22 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
 let tripsStatus = ref('tripsOnModeration')
 let router = useRouter();
 
 watch(tripsStatus, (status) => {
+    localStorage.setItem("tripsStatus", status)
     if (status === 'tripsOnModeration') {
         return router.push('/cabinet/moderation-trips/not-moderated-trips')
     }
     if (status === 'rejected') {
         return router.push('/cabinet/moderation-trips/rejected-trips')
     }
-})
+});
+onMounted(() => {
+    tripsStatus.value = localStorage.getItem("tripsStatus") ? localStorage.getItem("tripsStatus") : 'tripsOnModeration'
+});
 </script>
 <template>
     <a-row>
@@ -22,9 +26,9 @@ watch(tripsStatus, (status) => {
                 <a-radio value="tripsOnModeration">На модерации</a-radio>
                 <a-radio value="rejected">Откaзaнные</a-radio>
             </a-radio-group>
-      
-                <router-view />
-   
+
+            <router-view />
+
         </a-col>
     </a-row>
 </template>
