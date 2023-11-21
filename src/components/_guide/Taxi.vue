45d422c57acafdf34f8af863bdf8b247c7a5a6ce
SaveliyShutov <savelijsutov@gmail.com>
2023-11-21 17:26:37 +0400

    show taxi by location

--- src/components/_guide/Taxi.vue
+++ src/components/_guide/Taxi.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useGuide } from '../../stores/guide';
 import BackButton from "../BackButton.vue";
 
@@ -7,16 +7,17 @@ let localTaxi = ref({})
 
 let guideStore = useGuide()
 
-let rafreshTaxi = async () => {
+
+let refreshTaxi = async () => {
     let location = {}
     if (localStorage.getItem("location")) {
         location = JSON.parse(localStorage.getItem("location"))
     }
     let res = await guideStore.getLocalTaxi(location)
-    localTaxi.value = res.data[0].taxi
+    localTaxi.value = res.data[0]?.taxi
 }
 onMounted(async () => {
-    rafreshTaxi()
+    refreshTaxi()
 })
 </script>
 <template>
@@ -26,7 +27,7 @@ onMounted(async () => {
         <a-row type="flex" justify="center">
             <a-col :xs="22" :lg="16">
                 <h2>Такси</h2>
-                <a-row :gutter="[8, 8]" type="flex" justify="center">
+                <a-row v-if="localTaxi" :gutter="[8, 8]" type="flex" justify="center">
                     <a-col v-for="(t, i) in localTaxi" :xs="24" :md="12" :xl="8">
                         <a :href="`tel:${t.phone}`">
                             <a-card hoverable style="padding:10px 10px; border-radius: 10px; position: relative;"
@@ -38,6 +39,9 @@ onMounted(async () => {
                             </a-card>
                         </a>
                     </a-col>
+                </a-row>
+                <a-row v-else>
+                    В вашем городе нет такси.
                 </a-row>
             </a-col>
         </a-row>
