57d93a8253823a83548043e885f49d325c7e2fbf
qbclub <17926057+qbclub@users.noreply.github.com>
2023-11-26 23:32:10 +0400

    fix

--- src/components/_guide/Poster.vue
+++ src/components/_guide/Poster.vue
@@ -71,17 +71,17 @@ onMounted(async () => {
       <a-col :xs="22" :lg="16">
         <h2 class="d-flex space-between">
           Афиши
-          <!-- <a href="https://plpo.ru" target="_blank">
-            <img src="../../assets/plpo.webp" alt="" srcset="" style="max-height: 50px;"></a> -->
+          <a href="https://plpo.ru" target="_blank">
+            <img src="../../assets/plpo.webp" alt="" srcset="" style="max-height: 50px;"></a>
         </h2>
       </a-col>
     </a-row>
    
-        <iframe title="Афиши" style="width: 100%; height:90dvh; border: none" src="https://plpo.ru/frame/frame-posters">
-        </iframe>
+        <!-- <iframe title="Афиши" style="width: 100%; height:90dvh; border: none" src="https://plpo.ru/frame/frame-posters">
+        </iframe> -->
  
 
-    <!-- <a-row class="d-flex justify-center">
+    <a-row class="d-flex justify-center">
       <a-col :xs="22" :lg="16">
         <a-row type="flex" justify="center">
           <a-col>
@@ -110,6 +110,6 @@ onMounted(async () => {
 
 
       </a-col>
-    </a-row> -->
+    </a-row>
   </div>
 </template>
