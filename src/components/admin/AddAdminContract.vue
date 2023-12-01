503db02880570a1ae92f43ca75a858316be780fb
JetGreen <grishadzyin@gmail.com>
2023-12-01 17:38:45 +0400

    contract form

--- src/components/admin/AddAdminContract.vue
+++ src/components/admin/AddAdminContract.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { reactive, ref, watch } from "vue";
 import { useContract } from '../../stores/contract'
-import { Form, Field, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
+// import { Form, Field, ErrorMessage } from "vee-validate";
+// import * as yup from "yup";
 
 let contractStore = useContract()
 
@@ -29,20 +29,50 @@ function selectStartLocationFact(selected) {
 
 let contractEmail = ref("");
 let contractForm = reactive({
+  billingDescriptor: "",
+  fullName: "",
   name: "",
   inn: "",
   kpp: "",
+  okved: "",
   ogrn: "",
-  yr_address: "",
-  fact_address: "",
-  checking_account: "",
-  checking_account_bank: "",
-  cash_account: "",
-  cash_account_bank: "",
-  bik: "",
-  phone: "",
+  addresses: [
+    {
+      type: "actual",
+      zip: "427627",
+      country: "RUS",
+      city: "Глазов",
+      street: "ул. Калинина, 2А"
+    }
+  ],
   email: "",
-  director: "",
+  founders: {
+    individuals: [
+      {
+        firstName: "Зубенко",
+        lastName: "Михаил",
+        citizenship: "Мафиозник",
+        address: "адрес"
+      }
+    ]
+  },
+  ceo: {
+    firstName: "",
+    lastName: "",
+    middleName: "",
+    birthDate: "",
+    phone: "",
+    country: ""
+  },
+  siteUrl: "",
+  bankAccount: {
+    account: "",
+    bankName: "",
+    bik: "",
+    details: "",
+    tax: "",
+  },
+  fiscalization: { company: "OrangeData" }
 });
 // need modification
 watch(yrLocationSearchRequest, async (newValue, oldValue) => {
@@ -155,26 +185,54 @@ watch(factLocationSearchRequest, async (newValue, oldValue) => {
 })
 
 async function addContract() {
-  contractForm.yr_address = contractForm.yr_address.name
-  contractForm.fact_address = contractForm.fact_address.name
-
   let res = await contractStore.createContract(contractForm, contractEmail.value)
+  console.log(res);
   if (res.status == 200) {
     Object.assign(contractForm, {
+      billingDescriptor: "",
+      fullName: "",
       name: "",
       inn: "",
       kpp: "",
+      okved: "",
       ogrn: "",
-      yr_address: null,
-      fact_address: null,
-      checking_account: "",
-      checking_account_bank: "",
-      cash_account: "",
-      cash_account_bank: "",
-      bik: "",
-      phone: "",
+      addresses: [
+        {
+          type: "actual",
+          zip: "427627",
+          country: "RUS",
+          city: "Глазов",
+          street: "ул. Калинина, 2А"
+        }
+      ],
       email: "",
-      director: "",
+      founders: {
+        individuals: [
+          {
+            firstName: "Зубенко",
+            lastName: "Михаил",
+            citizenship: "Мафиозник",
+            address: "адрес"
+          }
+        ]
+      },
+      ceo: {
+        firstName: "",
+        lastName: "",
+        middleName: "",
+        birthDate: "",
+        phone: "",
+        country: ""
+      },
+      siteUrl: "",
+      bankAccount: {
+        account: "",
+        bankName: "",
+        bik: "",
+        details: "",
+        tax: "",
+      },
+      fiscalization: { company: "OrangeData" }
     })
 
     contractEmail.value = ""
@@ -192,159 +250,111 @@ async function submit() {
     await addContract()
   }
 }
-let formSchema = yup.object({
-  name: yup.string().required("заполните поле"),
-  inn: yup.string().max(10, 'максимум 10 символов').required("заполните поле"),
-  kpp: yup.string().max(9, 'максимум 9 символов').required("заполните поле"),
-  ogrn: yup.string().max(13, 'максимум 13 символов').required("заполните поле"),
-  yr_address: yup.string().required("заполните поле"),
-  fact_address: yup.string().required("заполните поле"),
-  checking_account: yup.string().required("заполните поле"),
-  checking_account_bank: yup.string().required("заполните поле"),
-  cash_account: yup.string().required("заполните поле"),
-  cash_account_bank: yup.string().required("заполните поле"),
-  bik: yup.string().max(9, 'максимум 9 символов').required("заполните поле"),
-  phone: yup.string().max(11, 'максимум 11 символов').required("заполните поле"),
-  email: yup.string().email('неверный формат').required("заполните поле"),
-  director: yup.string().required("заполните поле"),
-});
 </script>
 <template>
   <a-row>
     <a-col :span="24">
       <h3>Договоры</h3>
-      <Form :validation-schema="formSchema" v-slot="{ meta }" @submit="submit" class="ma-16">
+      <form @submit.prevent="submit" class="ma-16">
         <a-row :gutter="[16, 16]">
           <a-col :span="24">
-            <Field name="name" v-slot="{ value, handleChange }" v-model="contractForm.name">
-              Название
-              <a-input placeholder="Название" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="name" class="error-message" />
-            </Transition>
+            billingDescriptor
+            <a-input placeholder="Название" v-model:value="contractForm.billingDescriptor"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="inn" v-slot="{ value, handleChange }" v-model="contractForm.inn">
-              ИНН
-              <a-input placeholder="1010101010" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="inn" class="error-message" />
-            </Transition>
+            fullName
+            <a-input placeholder="Название" v-model:value="contractForm.fullName"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="kpp" v-slot="{ value, handleChange }" v-model="contractForm.kpp">
-              КПП
-              <a-input placeholder="999999999" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="kpp" class="error-message" />
-            </Transition>
+            name
+            <a-input placeholder="Название" v-model:value="contractForm.name"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="ogrn" v-slot="{ value, handleChange }" v-model="contractForm.ogrn">
-              ОГРН
-              <a-input placeholder="СГГККННХХХХХЧ" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="ogrn" class="error-message" />
-            </Transition>
+            ИНН
+            <a-input placeholder="1010101010" v-model:value="contractForm.inn"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="yr_address" v-slot="{ value, handleChange }" v-model="yrLocationSearchRequest">
-              Юридический адрес
-              <a-auto-complete :value="value" @update:value="handleChange" style="width: 100%"
-                :options="possibleLocations" placeholder="Глазов" @select="selectStartLocationYr">
-              </a-auto-complete>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="yr_address" class="error-message" />
-            </Transition>
+            kpp
+            <a-input placeholder="999999999" v-model:value="contractForm.kpp"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="fact_address" v-slot="{ value, handleChange }" v-model="factLocationSearchRequest">
-              Фактический адрес
-              <a-auto-complete :value="value" @update:value="handleChange" style="width: 100%"
-                :options="possibleLocations" placeholder="Глазов" @select="selectStartLocationFact">
-              </a-auto-complete>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="fact_address" class="error-message" />
-            </Transition>
+            okved
+            <a-input placeholder="999999999" v-model:value="contractForm.okved"></a-input>
           </a-col>
           <a-col :span="24">
-            <Field name="checking_account" v-slot="{ value, handleChange }" v-model="contractForm.checking_account">
-              Р/сч
-              <a-input placeholder="20202020202020202020" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="checking_account" class="error-message" />
-            </Transition>
+            ОГРН
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.ogrn"></a-input>
+          </a-col>
+
+          {{ contractForm.addresses }}
+          <!-- <a-col :span="24">
+            Юридический адрес
+            <a-auto-complete :value="value" @update:value="handleChange" style="width: 100%" :options="possibleLocations"
+              placeholder="Глазов" @select="selectStartLocationYr">
+            </a-auto-complete>
           </a-col>
           <a-col :span="24">
-            <Field name="checking_account_bank" v-slot="{ value, handleChange }"
-              v-model="contractForm.checking_account_bank">
-              Отделение банка
-              <a-input placeholder="" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="checking_account_bank" class="error-message" />
-            </Transition>
-          </a-col>
+            Фактический адрес
+            <a-auto-complete :value="value" @update:value="handleChange" style="width: 100%" :options="possibleLocations"
+              placeholder="Глазов" @select="selectStartLocationFact">
+            </a-auto-complete>
+          </a-col> -->
           <a-col :span="24">
-            <Field name="cash_account" v-slot="{ value, handleChange }" v-model="contractForm.cash_account">
-              К/сч
-              <a-input placeholder="20202020202020202020" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="cash_account" class="error-message" />
-            </Transition>
+            email
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.email"></a-input>
           </a-col>
+
+          founders
           <a-col :span="24">
-            <Field name="cash_account_bank" v-slot="{ value, handleChange }" v-model="contractForm.cash_account_bank">
-              Отделение банка
-              <a-input placeholder="und" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="cash_account_bank" class="error-message" />
-            </Transition>
+            firstName
+            <a-input placeholder="" v-model:value="contractForm.founders.individuals[0].firstName"></a-input>
+            lastName
+            <a-input placeholder="" v-model:value="contractForm.founders.individuals[0].lastName"></a-input>
+            citizenship
+            <a-input placeholder="" v-model:value="contractForm.founders.individuals[0].citizenship"></a-input>
+            address
+            <a-input placeholder="" v-model:value="contractForm.founders.individuals[0].address"></a-input>
           </a-col>
+
+          ceo
           <a-col :span="24">
-            <Field name="bik" v-slot="{ value, handleChange }" v-model="contractForm.bik">
-              БИК
-              <a-input placeholder="999999999" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="bik" class="error-message" />
-            </Transition>
+            firstName
+            <a-input placeholder="" v-model:value="contractForm.ceo.firstName"></a-input>
+            lastName
+            <a-input placeholder="" v-model:value="contractForm.ceo.lastName"></a-input>
+            middleName
+            <a-input placeholder="" v-model:value="contractForm.ceo.middleName"></a-input>
+            birthDate
+            <a-input placeholder="" v-model:value="contractForm.ceo.birthDate"></a-input>
+            phone
+            <a-input placeholder="" v-model:value="contractForm.ceo.phone"></a-input>
+            country
+            <a-input placeholder="" v-model:value="contractForm.ceo.country"></a-input>
           </a-col>
+
+          siteUrl
           <a-col :span="24">
-            <Field name="phone" v-slot="{ value, handleChange }" v-model="contractForm.phone">
-              Телефон
-              <a-input placeholder="80000000000" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="phone" class="error-message" />
-            </Transition>
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.siteUrl"></a-input>
           </a-col>
+
+          bankAccount
           <a-col :span="24">
-            <Field name="email" v-slot="{ value, handleChange }" v-model="contractForm.email">
-              Электронная почта
-              <a-input placeholder="ivanov@mail.ru" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="email" class="error-message" />
-            </Transition>
+            account
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.bankAccount.account"></a-input>
+            bankName
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.bankAccount.bankName"></a-input>
+            bik
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.bankAccount.bik"></a-input>
+            details
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.bankAccount.details"></a-input>
+            tax
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.bankAccount.tax"></a-input>
           </a-col>
+
+          fiscalization
           <a-col :span="24">
-            <Field name="director" v-slot="{ value, handleChange }" v-model="contractForm.director">
-              Директор
-              <a-input placeholder="Иванов Иван Иванович" @update:value="handleChange" :value="value"></a-input>
-            </Field>
-            <Transition name="fade">
-              <ErrorMessage name="director" class="error-message" />
-            </Transition>
+            company
+            <a-input placeholder="СГГККННХХХХХЧ" v-model:value="contractForm.fiscalization.company"></a-input>
           </a-col>
           <a-col :span="24" :sm="18" class="mb-16">
             <a-input placeholder="email пользователя" v-model:value="contractEmail"></a-input>
@@ -354,7 +364,7 @@ let formSchema = yup.object({
             </a-button>
           </a-col>
         </a-row>
-      </Form>
+      </form>
     </a-col>
   </a-row>
 </template>
