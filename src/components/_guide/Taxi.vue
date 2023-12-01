5cca785e4c972b4edae54689648e437645bcb7e9
SaveliyShutov <savelijsutov@gmail.com>
2023-12-01 18:01:07 +0400

    watch taxi

--- src/components/_guide/Taxi.vue
+++ src/components/_guide/Taxi.vue
@@ -1,12 +1,16 @@
 <script setup>
+import { storeToRefs } from 'pinia'
 import { onMounted, ref, watch } from 'vue'
 import { useGuide } from '../../stores/guide';
 import BackButton from "../BackButton.vue";
+import { useLocations } from "../../stores/locations";
 
 let localTaxi = ref({})
 
 let guideStore = useGuide()
+let locationStore = useLocations()
 
+const { location } = storeToRefs(locationStore)
 
 let refreshTaxi = async () => {
     let location = {}
@@ -16,8 +20,13 @@ let refreshTaxi = async () => {
     let res = await guideStore.getLocalTaxi(location)
     localTaxi.value = res.data
 }
+watch(location, () => {
+    refreshTaxi()
+})
+
 onMounted(async () => {
     refreshTaxi()
+    locationStore.refreshLocation()
 })
 </script>
 <template>
@@ -27,7 +36,7 @@ onMounted(async () => {
         <a-row type="flex" justify="center">
             <a-col :xs="22" :lg="16">
                 <h2>Такси</h2>
-                <a-row v-if="localTaxi" :gutter="[8, 8]" type="flex" justify="center">
+                <a-row v-if="localTaxi.length != 0" :gutter="[8, 8]" type="flex" justify="center">
                     <a-col v-for="(t, i) in localTaxi" :xs="24" :md="12" :xl="8">
                         <a :href="`tel:${t.phone}`">
                             <a-card hoverable style="padding:10px 10px; border-radius: 10px; position: relative;"
