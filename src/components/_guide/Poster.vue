03990ec637f4c65c2ce69e9c5311e5eff17e3ef8
qbclub <17926057+qbclub@users.noreply.github.com>
2023-12-26 22:10:54 +0400

    add posters frame

--- src/components/_guide/Poster.vue
+++ src/components/_guide/Poster.vue
@@ -1,67 +1,6 @@
 <script setup>
 import BackButton from "../BackButton.vue";
-import { onMounted, ref, reactive, watch } from "vue";
-import axios from "axios";
-import { useLocations } from "../../stores/locations";
 
-
-const locationStore = useLocations();
-
-let posters = ref([])
-let posterTypes = ref([])
-
-
-const backRoute = { name: 'Landing', hash: '#guide' };
-let query = ref({
-  eventLocation: '',
-  eventType: []
-})
-
-let selectType = ref([]);
-
-function goToPlpo(poster_id) {
-  window.open(`https://plpo.ru/post?_id=${poster_id}`)
-}
-
-watch(selectType, () => {
-  let filtered = posterTypes.value.filter((item, index) =>
-    selectType.value[index]
-  )
-  query.value.eventType = filtered.map((item) => item.name)
-  getPosters()
-}, { deep: true }
-
-
-)
-
-watch(() => locationStore.location, () => {
-  query.value.eventLocation = locationStore?.location.name
-  getPosters()
-})
-
-let getPosters = async () => {
-  let plpoPosters = await axios.post('https://api.plpo.ru/api/get-all', { query: query.value }, {
-    headers: {
-      "Authorization": `Bearer ${123}`
-    }
-  })
-  posters.value = plpoPosters.data
-  
-}
-
-onMounted(async () => {
-  let getTypes = await axios.get('https://api.plpo.ru/api/get-posters-types', {
-    headers: {
-      "Authorization": `Bearer ${123}`
-    }
-  })
-  posterTypes.value = getTypes.data.eventTypes
-
-  let setSelected = Array(posterTypes.value.length).fill(false);
-  selectType.value = setSelected
-  query.value.eventLocation = locationStore?.location.name
-  await getPosters()
-})
 
 </script>
 <template>
@@ -77,37 +16,9 @@ onMounted(async () => {
       </a-col>
     </a-row>
    
-        <!-- <iframe title="Афиши" style="width: 100%; height:90dvh; border: none" src="https://plpo.ru/frame/frame-posters">
-        </iframe> -->
+        <iframe title="Афиши" style="width: 100%; height:90dvh; border: none" src="https://plpo.ru/frame/frame-posters">
+        </iframe>
  
 
-    <a-row class="d-flex justify-center">
-      <a-col :xs="22" :lg="16">
-        <a-row type="flex" justify="center">
-          <a-col>
-            <a-space :size="[0, 8]" wrap>
-              <a-checkable-tag v-for="(type, index) in posterTypes" :key="type" v-model:checked="selectType[index]">
-                {{ type.name }}
-              </a-checkable-tag>
-            </a-space>
-          </a-col>
-
-        </a-row>
-
-
-        <a-row type="flex" justify="center">
-          <a-col span="12" :md="6" v-for="poster in posters">
-            <a-card class="ma-8" style="cursor:pointer">
-              <template #cover>
-                <img alt="example" :src="poster.image" @click="goToPlpo(poster._id)"/>
-              </template>
-            </a-card>
-          </a-col>
-        </a-row>
-
-
-
-      </a-col>
-    </a-row>
   </div>
 </template>
