ecbb1211a86aa3cec53b2b52e89e3558b2537033
Yegor Knyazev <egorknyazev10@gmail.com>
2024-03-22 18:05:57 +0400

    added ideas

--- src/views/fallbacks/403.vue
+++ src/views/fallbacks/403.vue
@@ -5,10 +5,12 @@ import { useRouter } from 'vue-router'
 let router = useRouter()
 let title = "403"
 let subtitle = "У вас нет прав для доступа к этой странице"
+let may_send_idea
 try {
     let message = JSON.parse(localStorage.getItem('fallbackMessage'))
     title = message.title
     subtitle = message.subtitle
+    may_send_idea = message.may_send_idea
 } catch (error) {
     console.log(error);
 }
@@ -20,6 +22,7 @@ onUnmounted(() => {
     <a-result status="403" :title="title" :sub-title="subtitle">
         <template #extra>
             <a-button @click="router.back()">Назад</a-button>
+            <a-button v-if="may_send_idea" @click="router.push('/send-idea')">Предложить идею</a-button>
         </template>
     </a-result>
 </template>
