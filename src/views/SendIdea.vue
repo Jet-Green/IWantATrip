393912c8ac1d2e7860283dbc03945c39000d3748
regtyit <titov54275@gmail.com>
2024-03-26 17:14:08 +0400

    idea

--- src/views/SendIdea.vue
+++ src/views/SendIdea.vue
@@ -33,28 +33,35 @@ async function submit() {
             <a-col :xs="22" :lg="12">
                 <h2>Предложить идею</h2>
 
-                <a-row class="mt-4">
-                    <a-col :span="24" :lg="18">
-                        <a-input v-model:value="form.topic" placeholder="Тема" :maxlength="50" show-count></a-input>
+                <a-row class="mt-4" :gutter="[16, 16]">
+                        <a-col :span="24" class="mt-8">
+                            Тема
+                            <a-input v-model:value="form.topic" placeholder="Тур в Абхазию" :maxlength="50" show-count></a-input>
+                        </a-col>
                         
-                        <div class="mt-8">
-                            <a-textarea v-model:value="form.offer" placeholder="Краткое описание"></a-textarea>
-                        </div>
-                        <div class="mt-8">
-                            <a-textarea v-model:value="form.date" placeholder="Примерная дата"></a-textarea>
-                        </div>
-                        <div class="mt-8">
-                            <a-textarea v-model:value="form.tripRoute" placeholder="Маршрут"></a-textarea>
-                        </div>
-                        <div class="mt-8">
-                            <a-textarea v-model:value="form.maxPeople" placeholder="Количество людей"></a-textarea>
-                        </div>                        
-                        <div class="mt-8">
-                            <a-textarea v-model:value="form.description" placeholder="Описание"></a-textarea>
-                        </div>
+                        <a-col :span="24" class="mt-8">
+                            Краткое описание
+                            <a-input v-model:value="form.offer" placeholder="Нужно будет посетить горы и вкусно поесть"></a-input>
+                        </a-col>
+                        <a-col :span="24" class="mt-8">
+                            Примерная дата
+                            <a-input v-model:value="form.date" placeholder="С 10 по 20 июля"></a-input>
+                        </a-col>
+                        <a-col :span="12" class="mt-8">
+                            Маршрут
+                            <a-input v-model:value="form.tripRoute" placeholder="Глазов-Абхазия-Гора1-Гора2-Глазов"></a-input>
+                        </a-col>
+                        <a-col :span="12" class="mt-8">
+                            Количество людей
+                            <a-input v-model:value="form.maxPeople" placeholder="12"></a-input>
+                        </a-col>                        
+                        <a-col :span="24" class="mt-8">
+                            Описание
+                            <a-textarea v-model:value="form.description" placeholder="Посещаем Абхазию на 10 дней. Без алкоголя, можно детей от 16 лет. Посетим много гор, попробуем местную кухню..."></a-textarea>
+                        </a-col>
 
                         <a-button @click="submit" class="lets_go_btn mt-12" type="primary">Отправить</a-button>
-                    </a-col>
+
                 </a-row>                
             </a-col>
         </a-row>
