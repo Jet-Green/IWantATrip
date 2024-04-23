9e6790883edf0842daef8e8e8f232fc55c6e6e6a
Yegor Knyazev <egorknyazev10@gmail.com>
2024-04-23 18:27:01 +0400

    changes

--- src/components/cards/CabinetTrip.vue
+++ src/components/cards/CabinetTrip.vue
@@ -4,13 +4,18 @@ import { useRouter } from "vue-router";
 import TripService from "../../service/TripService";
 import { useTrips } from '../../stores/trips';
 import { useAuth } from '../../stores/auth';
+import { useBus } from '../../stores/bus'
 import { useAppState } from '../../stores/appState';
+import Bus from '../../components/Bus.vue'
 
 import dayjs from 'dayjs'
 
 import locale from "ant-design-vue/es/date-picker/locale/ru_RU";
 const dateFormatList = ["DD.MM.YY", "DD.MM.YY"];
 const ruLocale = locale;
+
+let buses = ref([])
+let selected_bus = ref()
 
 let props = defineProps(['trip', 'actions'])
 let emit = defineEmits(['deleteTrip', 'updateTrip'])
@@ -317,6 +322,7 @@ onMounted(async () => {
     for (let t of appStateStore.appState[0].transport) {
         possibleTransport.value.push({ value: t.name })
     }
+    buses.value = await useBus().get()
     // addTransportForm.value.price = trip.value.transports[0]?.price ?? null
 })
 </script>
@@ -478,6 +484,38 @@ onMounted(async () => {
                     Тип
                     <a-auto-complete style="width: 100%" :options="possibleTransport" placeholder="Минивен"
                         @select="(value) => { addTransportForm.transportType = { name: value } }"></a-auto-complete>
+                </a-col>
+                <a-col :span="24" :md="24">
+                    <a-select
+                        v-model:value="selected_bus"
+                        style="width: 100%"
+                        placeholder="Выбрать автобус"
+                        option-label-prop="label"
+                    >
+                        <a-select-option v-for="bus in buses" :value="bus._id" :label="`${bus.name} (${bus.author}) — ${bus.seats.length} мест`" >
+                            <div style="display: flex; flex-direction: row; gap: 10px">
+                                <Bus :bus="bus" preview style="width: max(4cqw, 90px);" />
+
+                                <a-flex vertical gap="middle">
+                                    <div>Название: {{ bus.name }}</div>
+                                    <div>Автор: {{ bus.author }}</div>
+                                    <div>Мест: {{ bus.seats.length }}</div>
+                                </a-flex>
+                            </div>
+                        </a-select-option>
+
+                        <template #labelRender="{ value, closable, onClose }">
+                            <a-tag :closable="closable" style="margin-right: 3px" @close="onClose">
+                                {{ value }}
+                            </a-tag>
+                        </template>
+                    </a-select>
+                </a-col>
+                
+                <a-col :span="24" :md="24">
+                    <a-button @click="router.push('/create-bus')">
+                        Добавить свой
+                    </a-button>
                 </a-col>
                 <!-- <a-col :span="24" :md="12">
                     Цена для ожидающих
