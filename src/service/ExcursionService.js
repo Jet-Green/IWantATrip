fc56767dce0e9ab0eb0e5a94e99869784f651929
qbclub <17926057+qbclub@users.noreply.github.com>
2024-05-12 15:42:40 +0400

    fix

--- src/service/ExcursionService.js
+++ src/service/ExcursionService.js
@@ -2,7 +2,7 @@ import $api from "../plugins/axios";
 
 export default {
     create(body) {
-        return $api.post('/excursion', body)
+        return $api.post('/excursion/create', body)
     },
     getUserExcursions(userId) {
         return $api.get(`/excursion/get-excursions?user_id=${userId}`)
