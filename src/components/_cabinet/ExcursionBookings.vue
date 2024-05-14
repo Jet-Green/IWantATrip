5d8604db6d64e26e8caca64d4a04358854b38ae5
SaveliyShutov <savelijsutov@gmail.com>
2024-05-14 18:00:10 +0400

    print excursion customers

--- src/components/_cabinet/ExcursionBookings.vue
+++ src/components/_cabinet/ExcursionBookings.vue
@@ -1,17 +1,27 @@
 <script setup>
 import ExcursionBookingCard from './ExcursionBookingCard.vue'
 
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, getCurrentInstance } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { useExcursion } from '../../stores/excursion';
+import PrintExcursionCustomers from '../../components/_cabinet/PrintExcursionCustomers.vue'
 
 const router = useRouter()
 const route = useRoute()
 const excursionStore = useExcursion()
 
+const app = getCurrentInstance();
+const htmlToPaper = app.appContext.config.globalProperties.$htmlToPaper;
+
 let excursion = ref({})
 let bookings = ref([])
 let loading = ref(true)
+
+
+
+const print = async () => {
+  await htmlToPaper('printMe');
+};
 
 onMounted(async () => {
   let response = await excursionStore.getTimeBookings(route.query.excursion_id, route.query.time_id)
@@ -23,7 +33,7 @@ onMounted(async () => {
 })
 </script>
 <template>
-<div v-if="loading" class="d-flex justify-center">
+  <div v-if="loading" class="d-flex justify-center">
     <img src="../../assets/images/founddog.webp" alt="" style="height: 150px; margin-top: 50px;">
   </div>
   <div v-else>
@@ -36,10 +46,22 @@ onMounted(async () => {
         }}</a-breadcrumb-item>
       <a-breadcrumb-item style="cursor: pointer;">{{ route.query.time }}</a-breadcrumb-item>
     </a-breadcrumb>
-    <h3 class="mt-8 mb-8">Заказы <span style="color: #ff6600;">{{ excursion.name }}</span></h3>
+    <a-row class="mb-8">
+      <a-col class="d-flex align-center" :span="12">
+        <h3>Заказы <span style="color: #ff6600;">{{ excursion.name }}</span></h3>
+      </a-col>
+      <a-col :span="12" class="d-flex justify-end">
+        <a-button @click="print()" type="primary" class="lets_go_btn">
+          <span class="mdi mdi-printer-outline mr-4"></span> Печать
+        </a-button>
+      </a-col>
+    </a-row>
+    <div id="printMe"  v-if="bookings.length > 0" style="display: none">
+      <PrintExcursionCustomers :bookings="bookings" :excursion="excursion" />
+    </div>
     <a-row :gutter="[16, 16]" v-if="bookings.length > 0">
       <a-col :span="24" :md="12" :lg="8" v-for="booking of bookings">
-        <ExcursionBookingCard :booking="booking"/>
+        <ExcursionBookingCard :booking="booking" />
       </a-col>
     </a-row>
     <a-row v-else>
--- src/components/_cabinet/PrintExcursionCustomers.vue
+++ src/components/_cabinet/PrintExcursionCustomers.vue
@@ -1,50 +1,19 @@
 <script setup>
-import { computed } from 'vue';
+import { onMounted } from 'vue';
+
 
 let props = defineProps({
-    customers: Object,
-    trip: Object,
-    total: Object
+    excursion: Object,
+    bookings: Array
 })
 
-const clearData = (dateNumber) => {
-    let date = new Date(dateNumber).toLocaleDateString("ru-Ru", {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit",
-    });
-    if (date !== "Invalid Date" && date) {
-        return date;
-    }
-    return "";
-};
-let customersInCart = (index) => {
-    let sum = props.customers[index].cart.reduce((accumulator, currentValue) => accumulator + currentValue.count, 0)
-    return sum
-}
-
-function compare(a, b) {
-    if (a.selectedStartLocation < b.selectedStartLocation) {
-        return -1;
-    }
-    if (a.selectedStartLocation > b.selectedStartLocation) {
-        return 1;
-    }
-    return 0;
-}
-
-let sortCustomersByStartLocation = computed(() => {
-    if (props.customers) {
-        return props.customers.sort(compare)
-    }
+onMounted(() => {
+    console.log(props.bookings)
 })
-
-
 </script>
 <template>
-    <h3 style="text-align:center">{{ trip.name }}</h3>
-    <h4 style="text-align:center">с {{ clearData(trip.start) }} по {{ clearData(trip.end) }} </h4>
-    <h2 style="text-align:center">Список туристов</h2>
+    <h2 style="text-align:center">{{ excursion.name }}</h2>
+    <h3 style="text-align:center">Список туристов</h3>
     <table style="width:100%">
         <tr style="text-align:left; ">
             <th style="border-bottom: 1px solid black;">п/н</th>
@@ -52,51 +21,11 @@ let sortCustomersByStartLocation = computed(() => {
             <th style="border-bottom: 1px solid black;">Место посадки</th>
             <th style="border-bottom: 1px solid black;">Телефон</th>
             <th style="border-bottom: 1px solid black;">Количество</th>
+            <th style="border-bottom: 1px solid black;">Места</th>
         </tr>
-
-        <tr v-for="customer, i in sortCustomersByStartLocation" :key="i">
-            <td style="border-bottom: 1px solid black;">{{ i + 1 }}</td>
-            <td style="border-bottom: 1px solid black;">
-                <div v-for="unit, j in customer.touristsList">
-                    {{ unit.fullname }}
-                </div>
-            </td>
-            <td style="border-bottom: 1px solid black;">
-                {{ customer.selectedStartLocation }}
-            </td>
-            <td style="border-bottom: 1px solid black;">
-                <div v-for="unit, j in customer.touristsList">
-                    {{ unit.phone }}
-                </div>
-            </td>
-            <td style="border-bottom: 1px solid black;">
-                {{ customersInCart(i) }}
-                <!-- <span v-for="item, j in  customer.cart">
-                    {{ item.count }}
-                    <span v-if="customer.cart.length > j + 1">+</span>
-                </span> -->
-
-            </td>
+        <tr v-for="booking, i in bookings" :key="i">
+            <td style="border-bottom: 1px solid black;">{{booking.user.fullinfo.fullname }}</td>
+            <td style="border-bottom: 1px solid black;">{{booking.user.fullinfo.phone }}</td>
         </tr>
-        <!-- <tr v-for="customer, i in customers" :key="i">
-            <td>{{ i + 1 }}<
-                /td>
-            <td>{{ customer.userInfo.fullname }}</td>
-            <td>{{ customer.userInfo.phone }}</td>
-            <td>
-                <span v-for="item, j in  customer.cart">
-                    {{ item.count }}
-                    <span v-if="customer.cart.length > j + 1">+</span>
-                </span>
-            
-            </td>
-        </tr> -->
     </table>
-    <span v-if="total">
-        <h3 style="text-align:end; padding:16px">
-            Итого:
-            {{ total.amount }} чел.
-        </h3>
-    </span>
 </template>
-
