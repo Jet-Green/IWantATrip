b3a3d7c4956ce1c4ae258bfcdcdb2038b7407e45
Yegor Knyazev <egorknyazev10@gmail.com>
2024-06-04 18:49:08 +0400

    fixed roman's buttons

--- src/components/BuyExcursionDialog.vue
+++ src/components/BuyExcursionDialog.vue
@@ -189,12 +189,9 @@ onMounted(() => {
         </div>
       </div>
     </div>
-    <div class="d-flex justify-center">
-      <a-button type="primary" class="lets_go_btn" @click="book" v-if="pricesForm.length == 0">заказать</a-button>
-    </div>
 
     <div class="d-flex justify-center mt-16">
-      <a-button type="primary" class="lets_go_btn" @click="buy" v-if="pricesForm.length > 0">заказать</a-button>
+      <a-button type="primary" class="lets_go_btn" @click="pricesForm.length == 0 ? book() : buy()">заказать</a-button>
     </div>
   </a-modal>
 </template>
