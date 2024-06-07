cdc180debb166f6c19f8511a8794b91d8a6030de
Yegor Knyazev <egorknyazev10@gmail.com>
2024-06-07 18:17:46 +0400

    add suspense

--- src/App.vue
+++ src/App.vue
@@ -75,11 +75,13 @@ onMounted(async () => {
       <Header></Header>
       <!-- в документации с margin'ом, чтобы предотвратить перекрывание контента хедром -->
       <a-layout-content style="margin-top: 69px">
-        <router-view v-slot="{ Component }">
-          <transition name="fade" mode="out-in">
-            <component :is="Component" />
-          </transition>
-        </router-view>
+        <Suspense>
+          <router-view v-slot="{ Component }">
+            <transition name="fade" mode="out-in">
+              <component :is="Component" />
+            </transition>
+          </router-view>
+        </Suspense>
       </a-layout-content>
     </a-layout>
   </a-config-provider>
