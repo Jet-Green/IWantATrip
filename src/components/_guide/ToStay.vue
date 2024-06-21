61a7482ea885529cd052427ac90594eeb887dca4
SaveliyShutov <savelijsutov@gmail.com>
2024-06-21 16:37:30 +0400

    add ostrovok

--- src/components/_guide/ToStay.vue
+++ src/components/_guide/ToStay.vue
@@ -10,10 +10,13 @@ const useGuideStore = useGuide();
 const backRoute = { name: 'Landing', hash: '#guide' };
 
 useGuideStore.fetchElementsByQuery('watch');
+
 onMounted(() => {
-  let recaptchaScript = document.createElement('script')
-  recaptchaScript.setAttribute('src', 'https://hotels.gorodaivesi.ru/static/dist/js/affiliate-search.js')
-  document.head.appendChild(recaptchaScript)
+  let url = 'https://cpa.ostrovok.ru/static/widget/scripts/e9061465c5e7db51e59c37ff9304d6ef.js'
+  let ostrovok = document.createElement('script')
+  ostrovok.setAttribute('src', url)
+  document.body.appendChild(ostrovok)
+
 })
 </script>
 <template>
@@ -28,10 +31,15 @@ onMounted(() => {
     </a-row>
     <a-row class="mt-16" type="flex" justify="center">
       <a-col :xs="22" :lg="16">
-        <link rel="stylesheet" href="//hotels.gorodaivesi.ru/static/dist/css/affiliate-search.css" />
-        <div id="affiliate-search" data-lang="ru" data-domain="//hotels.gorodaivesi.ru" data-color="#ff6413" data-object-id="" data-object-label=""></div>
       </a-col>
     </a-row>
   </div>
 </template>
-<style></style>
+<style>
+body {
+  display: flex;
+  flex-direction: column;
+}
+
+
+</style>
