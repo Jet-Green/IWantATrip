b267a33586231f364de84cf54c089df01dda33c5
qbclub <17926057+qbclub@users.noreply.github.com>
2024-08-22 15:36:25 +0400

    iframe TinkoffPayment

--- src/views/TinkoffPayment.vue
+++ src/views/TinkoffPayment.vue
@@ -1,12 +1,21 @@
 <script setup>
-import { useRoute } from 'vue-router'
+import {onMounted} from "vue"
+import { useRoute, useRouter } from 'vue-router'
 import BackButton from '../components/BackButton.vue';
 
 const route = useRoute()
+const router = useRouter();
+const backRoute = "/"
+
+onMounted(()=>{
+    if(!route.query.url) {
+        router.push("/")
+    }
+})
 </script>
 <template>
     <div style="height: 80vh; width: 100%">
-        <BackButton />
+        <BackButton :backRoute="backRoute" />
         <iframe :src="route.query.url" frameborder="0" width="100%" height="100%"></iframe>
     </div>
 </template>
