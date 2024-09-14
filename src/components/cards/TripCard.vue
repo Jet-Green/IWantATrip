3b306e80043a1975e9a6a024a67a5d21df191cd6
qbclub <17926057+qbclub@users.noreply.github.com>
2024-09-14 12:09:25 +0300

    fix

--- src/components/cards/TripCard.vue
+++ src/components/cards/TripCard.vue
@@ -12,15 +12,17 @@ function goToTripPage() {
   router.push(`/trip?_id=${props.trip._id}`)
 }
 const clearData = (dateNumber) => {
-  let date = new Date(dateNumber).toLocaleDateString("ru-Ru", {
-    year: "2-digit",
-    month: "2-digit",
-    day: "2-digit",
-  })
-  if (date !== 'Invalid Date' && date) {
-    return date
-  }
-  return ''
+  dateNumber = dateNumber - trip.value?.timezoneOffset
+    const date = new Date(dateNumber);
+    if (!isNaN(date.getTime())) {
+        return date.toLocaleDateString("ru-RU", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            timeZone: 'UTC' // Используем UTC для гарантии, что время будет в формате UTC
+        });
+    }
+    return '';
 }
 function getDate() {
   let start = clearData(props.trip.start)
