90237968ab98f05d328e84bffa2215f13041f0a6
JetGreen <grishadzyin@gmail.com>
2024-10-01 11:14:11 +0500

    next version

--- src/components/sections/TripFilter.vue
+++ src/components/sections/TripFilter.vue
@@ -22,7 +22,9 @@ let time = ref([])
 let query = ref("")
 let tripRegion = ref("")
 let type = ref("")
-let locationRadius = ref(100)
+let locationRadius = ref(Number(localStorage.getItem("locationRadius")) ?? 100)
+
+let radiusVisible = ref(false)
 
 let querySuggestionsVisible = ref(false)
 let nearMeSuggestionsVisible = ref(false)
@@ -32,7 +34,7 @@ let router = useRouter()
 let suggestedRegions = computed(() => {
   return (
     appStore.appState[0].tripRegions?.filter((reg) =>
-      reg.toLowerCase().includes(query.value.trim().toLocaleLowerCase())
+      reg.toLowerCase().includes(tripRegion.value.trim().toLocaleLowerCase())
     ) ?? []
   )
 })
@@ -55,15 +57,19 @@ watch(time, (newTime) => {
 
 function hideQuerySuggestions() {
   querySuggestionsVisible.value = false
+  tripRegion.value = ""
 }
+
 function showQuerySuggestions() {
+  tripRegion.value = query.value
   querySuggestionsVisible.value = true
 }
 
+let regionSelected = ref(false)
 function selectRegion(regionName) {
   tripRegion.value = regionName
   query.value = regionName
-  hideQuerySuggestions()
+  regionSelected.value = true
 }
 
 function find() {
@@ -73,7 +79,7 @@ function find() {
   tripStore.searchCursor = 1
   tripStore.cursor = 1
   tripStore.trips = []
-  
+
   tripStore.tripFilter.tripRegion = tripRegion.value
   tripStore.tripFilter.locationRadius = locationRadius.value
   if (time.value[0] ?? time.value[1]) {
@@ -127,6 +133,17 @@ function resetForm() {
   find()
 }
 
+function saveLocationRadius() {
+  localStorage.setItem("locationRadius", locationRadius.value)
+  radiusVisible.value = false
+}
+
+watch(query, () => {
+  if (query.value.length > 2) {
+    showQuerySuggestions()
+  }
+})
+
 onMounted(() => {
   query.value = localStorage.getItem("TripQuery") ?? ""
   type.value = localStorage.getItem("TripType") ?? ""
@@ -152,31 +169,30 @@ onMounted(() => {
   <a-row type="flex" justify="center" class="section_bg">
     <a-col :xs="22" :lg="16">
       <a-row :gutter="[8, 4]" class="d-flex justify-center align-center flex-wrap">
-        <a-col :span="12" :md="6" class="d-flex direction-column query-container">
+        <a-col :span="12" :md="6" class="d-flex direction-column">
           <div for="search" style="font-size: 10px; line-height: 10px">искать</div>
           <a-input
             v-model:value="query"
             placeholder="сочи"
             name="search"
             style="width: 100%"
-            @click="showQuerySuggestions"
             allowClear
             autocomplete="off"
           />
 
-          <Transition name="fade">
+          <!-- <Transition name="fade">
             <div class="query-suggestions" v-if="querySuggestionsVisible">
               <div class="card" v-if="!nearMeSuggestionsVisible">
-                <div class="region-query" @click="hideQuerySuggestions">
-                  <b>{{ query }}</b>
-                </div>
 
                 <span class="mdi mdi-close" @click="hideQuerySuggestions"></span>
 
-                <a-button variant="outlined" class="location-btn" @click="nearMeSuggestionsVisible = true">
+                <a-button variant="outlined" class="location-btn" @click="radiusModal = true">
                   <span class="mdi mdi-map-marker-radius-outline"></span>
                   туры рядом со мной</a-button
                 >
+                <b>
+                  радиус поиска {{ locationRadius }} км.
+                </b>
                 <hr class="mt-16 mb-16" />
                 <div>
                   <div v-if="suggestedRegions.length > 0" v-for="region of suggestedRegions">
@@ -192,7 +208,6 @@ onMounted(() => {
                   <div v-else>Ничего не нашлось</div>
                 </div>
               </div>
-              <!-- near me -->
               <div v-else class="card">
                 <b> Радиус поиска {{ locationRadius }} км. </b>
                 <a-slider
@@ -207,10 +222,71 @@ onMounted(() => {
                 <span class="mdi mdi-close" @click="nearMeSuggestionsVisible = false"></span>
                 <a-button style="border-radius: 18px" @click="hideQuerySuggestions">готово</a-button>
               </div>
-              <!-- near me -->
             </div>
-          </Transition>
+          </Transition> -->
         </a-col>
+
+        <a-modal v-model:open="querySuggestionsVisible" title="Регион">
+          <a-row :gutter="[16, 16]">
+            <a-col :span="24">
+              <a-input
+                v-model:value="tripRegion"
+                placeholder="сочи"
+                name="search"
+                style="width: 100%"
+                allowClear
+                autocomplete="off"
+              />
+            </a-col>
+
+            <a-col :span="24">
+              <a-button v-if="!radiusVisible" variant="outlined" class="location-btn" @click="radiusVisible = true">
+                <span class="mdi mdi-map-marker-radius-outline"></span>
+                туры рядом со мной</a-button
+              >
+              <div v-if="radiusVisible">
+                <a-slider
+                  v-model:value="locationRadius"
+                  :step="100"
+                  :min="100"
+                  :max="1800"
+                  tooltipPlacement="right"
+                  :tipFormatter="(s) => s + ' км'"
+                />
+                <b>Радиус поиска {{ locationRadius }} км.</b>
+              </div>
+            </a-col>
+
+            <a-col :span="24">
+              <hr class="mt-16 mb-16" />
+              <div>
+                <div v-if="suggestedRegions.length > 0" v-for="region of suggestedRegions">
+                  <div class="region-container" @click="selectRegion(region)">
+                    <div>
+                      {{ region }}
+                    </div>
+                    <div>
+                      <span class="mdi mdi-check"></span>
+                    </div>
+                  </div>
+                </div>
+                <div v-else>Ничего не нашлось</div>
+              </div>
+            </a-col>
+          </a-row>
+
+          <template #footer>
+            <a-button key="back" style="border-radius: 18px" @click="hideQuerySuggestions()">не по региону</a-button>
+            <a-button
+              key="submit"
+              :disabled="!regionSelected"
+              style="border-radius: 18px"
+              type="primary"
+              @click="saveLocationRadius(), hideQuerySuggestions(), find()"
+              >применить</a-button
+            >
+          </template>
+        </a-modal>
 
         <a-col :span="12" :md="6" class="d-flex direction-column" v-if="appStore.appState">
           <div style="font-size: 10px; line-height: 10px">вид тура</div>
@@ -258,61 +334,59 @@ onMounted(() => {
 // .section_bg {
 //   background: rgba(67, 65, 79);
 // }
-.query-container {
-  position: relative;
-}
 
-.mdi-close {
-  position: absolute;
-  top: 4px;
-  right: 8px;
-  font-size: 20px;
-  padding: 4px;
+// .mdi-close {
+//   position: absolute;
+//   top: 4px;
+//   right: 8px;
+//   font-size: 20px;
+//   padding: 4px;
+//   cursor: pointer;
+// }
+// .mdi-close:active {
+//   transform: scale(1.2);
+// }
+// .mdi-close:hover {
+//   transform: scale(1.2);
+// }
+// .query-suggestions {
+//   position: absolute;
+//   z-index: 1;
+//   top: 40px;
+//   left: 4px;
+//   width: 140%;
+//   max-height: 400px;
+//   overflow-y: scroll;
+//   padding: 6px;
+
+//   // overflow-x: hidden;
+//   .card {
+//     // background-color: #ff6600;
+//     border-radius: 6px;
+//     // border: 1px solid rgba(95, 95, 95, 0.3);
+//     box-shadow: 0px 0px 6px 0px rgba(95, 95, 95, 0.2);
+//     hr {
+//       color: rgba(95, 95, 95, 0.01);
+//     }
+//   }
+
+// }
+.region-container {
+  display: flex;
+  justify-content: space-between;
   cursor: pointer;
+  margin: 6px 0;
+  user-select: none;
 }
-.mdi-close:active {
-  transform: scale(1.2);
+.region-container:hover {
+  color: #ff6600;
 }
-.mdi-close:hover {
-  transform: scale(1.2);
+.region-query:hover {
+  color: #ff6600;
 }
-.query-suggestions {
-  position: absolute;
-  z-index: 1;
-  top: 40px;
-  left: 4px;
-  width: 140%;
-  max-height: 400px;
-  overflow-y: scroll;
-  padding: 6px;
-
-  // overflow-x: hidden;
-  .card {
-    // background-color: #ff6600;
-    border-radius: 6px;
-    // border: 1px solid rgba(95, 95, 95, 0.3);
-    box-shadow: 0px 0px 6px 0px rgba(95, 95, 95, 0.2);
-    hr {
-      color: rgba(95, 95, 95, 0.01);
-    }
-  }
-  .region-container {
-    display: flex;
-    justify-content: space-between;
-    cursor: pointer;
-    margin: 6px 0;
-    user-select: none;
-  }
-  .region-container:hover {
-    color: #ff6600;
-  }
-  .region-query:hover {
-    color: #ff6600;
-  }
-  .region-query {
-    cursor: pointer;
-    width: fit-content;
-  }
+.region-query {
+  cursor: pointer;
+  width: fit-content;
 }
 @media (max-width: 767px) {
   .query-suggestions {
