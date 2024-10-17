ab385209db424efa3d3b89e60e7a778d03d85f25
qbclub <17926057+qbclub@users.noreply.github.com>
2024-10-17 12:54:27 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -71,7 +71,7 @@ export default defineConfig(
             description: 'Description of your app',
             theme_color: '#ffffff',
             background_color: "#ffffff",
-            version: "1.2.6",
+            version: "1.2.7",
             display: "standalone",
             icons: [
               {
