16331991e7568cb986bd8bee54d2b35216e10279
qbclub <17926057+qbclub@users.noreply.github.com>
2024-11-17 09:00:12 +0400

    small fix

--- src/components/sections/Explanation.vue
+++ src/components/sections/Explanation.vue
@@ -18,7 +18,7 @@ import StartImage from "../_explanation/StartSvgImg.vue";
   text-align: center;
   transform: translateY(120%);
   font-family: 'Caveat', cursive;
-  font-size: clamp(0.875rem, -0.825rem + 6.8vw, 3rem);
+  font-size: clamp(1.25rem, -0.15rem + 5.6vw, 3rem);
   color: white;
   font-weight: 700;
   height: 0;
