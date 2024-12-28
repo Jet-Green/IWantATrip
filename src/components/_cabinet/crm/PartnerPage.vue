73189004aeca11ef682207d9201269e43d83ca66
JetGreen <grishadzyin@gmail.com>
2024-12-28 11:22:49 +0500

    fixed changes

---
+++ src/components/_cabinet/crm/PartnerPage.vue
@@ -0,0 +1,46 @@
+<script setup>
+let partner = ref({})
+</script>
+<template>
+  <div>
+    {{ partner }}
+    <h3>
+      {{ partner.name }}
+    </h3>
+
+    <h3>Контактное лицо</h3>
+    <div class="second-level">
+      <div class="contact-item">
+        <span class="mdi mdi-account-outline"></span>
+        {{ partner.contactPerson }}
+      </div>
+
+      <a :href="`mailto:${partner.email}`" class="contact-item">
+        <span class="mdi mdi-email-fast-outline"></span>
+        {{ partner.email }}
+      </a>
+      <a :href="`tel:${partner.phone}`" class="contact-item">
+        <span class="mdi mdi-phone-outline"></span>
+        {{ partner.phone }}
+      </a>
+    </div>
+
+    <template #footer>
+      <a-button @click="emit('close')">ок</a-button>
+    </template>
+  </div>
+</template>
+<style scoped lang="scss">
+.contact-item {
+  display: flex;
+  align-items: center;
+
+  .mdi {
+    font-size: 20px;
+    margin-right: 10px;
+  }
+}
+.second-level {
+  margin-left: 14px;
+}
+</style>
--- src/components/_cabinet/crm/TaskCard.vue
+++ src/components/_cabinet/crm/TaskCard.vue
@@ -1,55 +1,80 @@
 <script setup>
-import { toRefs, computed, ref, onMounted, reactive } from 'vue';
-import { useRouter } from 'vue-router';
+import { toRefs, computed, ref, onMounted, reactive } from "vue"
+import { useRouter } from "vue-router"
 import datePlugin from "../../../plugins/dates"
 
-let props = defineProps(['task'])
-let emit = defineEmits(['refreshTasks'])
+let props = defineProps(["task"])
+let emit = defineEmits(["refreshTasks"])
 import { useTasks } from "../../../stores/tasks"
 
 const router = useRouter()
 const taskStore = useTasks()
-let color = ref('black')
+let color = ref("black")
 let show = ref(false)
 // toRefs add reactivity
 let { task } = toRefs(props)
 
+let viewPartnerDialog = ref(false)
 
+let deadline = datePlugin.excursions.getLocalDateFromUTC(task.value.deadLine, task.value.timezoneOffset)
+let date = datePlugin.excursions.getPrettyDate(deadline)
 
-async function taskToDelete(_id) {
-  let response = await taskStore.deletePlace(_id);
-  if (response.status == 200) {
-    emit('refreshTasks')
-  }
+async function closePartnerDialog() {
+  viewPartnerDialog.value = false;
 }
 
-
-
+async function taskToDelete(_id) {
+  let response = await taskStore.deletePlace(_id)
+  if (response.status == 200) {
+    emit("refreshTasks")
+  }
+}
 </script>
 <template>
   <a-card class="card" v-if="task._id">
-    <div style="width: 100%; height: 100%; text-align: center; font-weight: 700;">
+    <div style="width: 100%; height: 100%; text-align: center; font-weight: 700">
       {{ task.name }}
     </div>
-    {{ task }}
-    {{ datePlugin.excursions.getLocalDateFromUTC(task.deadLine, task.timezoneOffset) }}
-    <a-divider class="ma-4" style="border-color: #205F79"></a-divider>
-    <div class="actions">
-      <div>
-
-        <a-popconfirm title="Удалить?" ok-text="Да" cancel-text="Нет" @confirm="taskToDelete(task._id)">
-          <span class="mdi mdi-delete" style="color: #ff6600;"></span>
-        </a-popconfirm>
-
-
-        <a-popconfirm title="Редактировать?" ok-text="Да" cancel-text="Нет"
-          @confirm="router.push(`/create-task?_id=${task._id}`)">
-          <span class="mdi mdi-pen"></span>
-        </a-popconfirm>
-
+    <div class="info">
+      <div class="deadline info-item">
+        <span class="mdi mdi-clock-time-five-outline"></span>
+        {{ date.day + " " + date.month }} к
+        {{ deadline.hours + ":" + deadline.minutes }}
       </div>
 
+      <div class="info-item">
+        <b> Тур: </b>
+        <router-link class="link" :to="`/trip?_id=${task.trip._id}`">
+          {{ task.trip.name }}
+        </router-link>
+      </div>
 
+      <div class="partner info-item">
+        <b> Партнёр: </b>
+        <span class="partner-name" @click="viewPartnerDialog = true">
+          {{ task.partner.name }}<span class="mdi mdi-open-in-new"></span>
+        </span>
+      </div>
+    </div>
+    
+    {{ task }}
+
+    <a-divider class="ma-4" style="border-color: #205f79"></a-divider>
+    <div class="actions">
+      <div>
+        <a-popconfirm title="Удалить?" ok-text="Да" cancel-text="Нет" @confirm="taskToDelete(task._id)">
+          <span class="mdi mdi-delete" style="color: #ff6600"></span>
+        </a-popconfirm>
+
+        <a-popconfirm
+          title="Редактировать?"
+          ok-text="Да"
+          cancel-text="Нет"
+          @confirm="router.push(`/create-task?_id=${task._id}`)"
+        >
+          <span class="mdi mdi-pen"></span>
+        </a-popconfirm>
+      </div>
     </div>
 
   </a-card>
@@ -80,5 +105,36 @@ async function taskToDelete(_id) {
 
 .overlay {
   opacity: 0.5;
+}
+
+.info {
+  display: flex;
+  align-items: center;
+}
+
+.deadline {
+  display: flex;
+  font-weight: 600;
+  user-select: none;
+  display: flex;
+  align-items: center;
+  .mdi-clock-time-five-outline {
+    font-size: 22px;
+    margin-right: 6px;
+  }
+}
+.link {
+  text-decoration: underline;
+}
+.info-item {
+  margin: 4px 8px;
+}
+
+.partner {
+  .partner-name {
+    user-select: none;
+    cursor: pointer;
+    font-weight: 500;
+  }
 }
 </style>
