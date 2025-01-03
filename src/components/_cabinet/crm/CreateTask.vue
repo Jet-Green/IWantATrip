edb86f32e316e36416ca8b5f67f50418b80696a9
JetGreen <grishadzyin@gmail.com>
2025-01-03 16:35:15 +0500

    add tripInfo to task

--- src/components/_cabinet/crm/CreateTask.vue
+++ src/components/_cabinet/crm/CreateTask.vue
@@ -1,40 +1,37 @@
 <script setup>
-import { reactive, ref, watch, onMounted, computed } from 'vue';
-import { Form, Field, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
-import dayjs from "dayjs";
-import locale from "ant-design-vue/es/date-picker/locale/ru_RU";
-import 'dayjs/locale/ru';
+import { reactive, ref, watch, onMounted, computed } from "vue"
+import { Form, Field, ErrorMessage } from "vee-validate"
+import * as yup from "yup"
+import dayjs from "dayjs"
+import locale from "ant-design-vue/es/date-picker/locale/ru_RU"
+import "dayjs/locale/ru"
 
-import { message } from "ant-design-vue";
+import { message } from "ant-design-vue"
 
-import { useRouter, useRoute } from 'vue-router';
-import { useAppState } from "../../../stores/appState";
-import BackButton from "../../../components/BackButton.vue";
-import { useAuth } from '../../../stores/auth';
-import { usePartners } from '../../../stores/partner';
-import { useTrips } from '../../../stores/trips';
-import { useTasks } from '../../../stores/tasks';
+import { useRouter, useRoute } from "vue-router"
+import { useAppState } from "../../../stores/appState"
+import BackButton from "../../../components/BackButton.vue"
+import { useAuth } from "../../../stores/auth"
+import { usePartners } from "../../../stores/partner"
+import { useTrips } from "../../../stores/trips"
+import { useTasks } from "../../../stores/tasks"
 
+dayjs.locale("ru")
 
-
-dayjs.locale('ru');
-
-const userStore = useAuth();
-const partnerStore = usePartners();
+const userStore = useAuth()
+const partnerStore = usePartners()
 const tripsStore = useTrips()
 const tasksStore = useTasks()
-const router = useRouter();
+const router = useRouter()
 
-const emailExists = ref([]);
+const emailExists = ref([])
 
 let trips = ref()
 let partners = ref()
 
-
 const form = reactive({
-  author: '',
-  name: '',
+  author: "",
+  name: "",
   trip: "",
   partner: "",
   deadLine: null,
@@ -42,13 +39,13 @@ const form = reactive({
   timezoneOffset: null,
   payAmount: null,
   payments: [],
-  status: '',
+  status: "",
   managers: [],
-  comment: '',
+  comment: "",
   interactions: [],
-});
-
-
+})
+let managersEmails = ref([])
+let tripInfo = ref({})
 
 let formSchema = yup.object({
   name: yup.string().required("заполните поле"),
@@ -57,17 +54,15 @@ let formSchema = yup.object({
   // contactPerson: yup.string().required("заполните поле"),
   // category: yup.string().required("заполните поле"),
   // https://vee-validate.logaretm.com/v4/examples/array-fields/
-});
-
-
+})
 
 function close() {
-  router.push("/cabinet/tasks");
+  router.push("/cabinet/tasks")
 }
 function clearForm() {
   Object.assign(form, {
-    author: '',
-    name: '',
+    author: "",
+    name: "",
     trip: "",
     partner: "",
     deadLine: null,
@@ -75,109 +70,122 @@ function clearForm() {
     timezoneOffset: null,
     payAmount: null,
     payments: [],
-    status: '',
+    status: "",
     managers: [],
-    comment: '',
+    comment: "",
     interactions: [],
-  });
+  })
 }
 
 function handleDateChange(value) {
-  form.deadLine = value;
+  form.deadLine = value
 }
-let isChecking = false;
+let isChecking = false
 
 let checkManagers = async () => {
   if (isChecking) {
     return
-  };
-
-  isChecking = true;
-  emailExists.value = []
-  for (const manager of form.managers) {
-    let result = await userStore.checkUserEmail(manager);
-    emailExists.value.push(result.data);
   }
-  isChecking = false;
+
+  isChecking = true
+  emailExists.value = []
+  for (let i = 0; i < managersEmails.value.length; i++) {
+    let result = await userStore.checkUserEmail(managersEmails.value[i])
+    // приходит либо полностью юзер, либо null
+    if (result.data?._id) {
+      emailExists.value.push(true)
+      form.managers[i] = result.data._id
+    } else {
+      emailExists.value.push(false)
+    }
+  }
+  isChecking = false
 }
 
 async function submit() {
   let toSend = { ...form }
   toSend.author = userStore.user._id
   toSend.createdDate = Date.now()
-  toSend.status = 'open'
-  toSend.timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  toSend.status = "open"
+  toSend.timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000
+  toSend.tripInfo = tripInfo.value
+
   if (toSend.deadLine) {
     toSend.deadLine = new Date(toSend.deadLine).getTime() + toSend.timezoneOffset
   }
-
-
   let response = await tasksStore.create(toSend)
   if (response.status == 200) {
-    message.config({ duration: 1.5, top: "70vh" });
+    message.config({ duration: 1.5, top: "70vh" })
     message.success({
-      content: "Задача добавлена!", onClose: () => {
+      content: "Задача добавлена!",
+      onClose: () => {
         close()
         clearForm()
       },
-    });
+    })
   }
 }
 
 // туры для селектора
 
-let getTips = async (search) => {
+let getTrips = async (search) => {
   if (search.length > 2) {
     let res = await tripsStore.findAuthorTrips(search, userStore.user._id)
     trips.value = res.data
-  }
-  else {
+  } else {
     trips.value = []
   }
 }
 
 const tripSearch = (search) => {
-  getTips(search)
-};
+  getTrips(search)
+}
 const tripChange = (search) => {
-  getTips(search)
-};
+  getTrips(search)
+}
 
 // партнеры для селектора
 let page = 1
 let query = {
   author: userStore.user._id,
-  $or: [
-    { name: { $regex: '', $options: 'i' } },
-  ]
-};
+  $or: [{ name: { $regex: "", $options: "i" } }],
+}
 let getPartners = async (search) => {
   if (search.length > 2) {
     query.$or[0].name.$regex = search
     let data = await partnerStore.getAll(page, query)
     partners.value = data
-  }
-  else {
+  } else {
     partners.value = []
   }
 }
 
 const partnerSearch = (search) => {
   getPartners(search)
-};
+}
 const partnerChange = (search) => {
   getPartners(search)
-};
+}
 let addManager = () => {
-  form.managers.push('')
+  managersEmails.value.push("")
+  form.managers.push("")
 }
 let removeManager = (index) => {
+  managersEmails.value.splice(index, 1)
   form.managers.splice(index, 1)
 }
 
-watch(() => form.managers, () => checkManagers(), { deep: true })
-
-
+watch(managersEmails, () => checkManagers(), { deep: true })
+watch(
+  () => form.trip,
+  (newTripId) => {    
+    for (let i = 0; i < trips.value.length; i++) {
+      if (trips.value[i]._id == newTripId) {
+        tripInfo.value = trips.value[i]        
+      }
+    }
+  }
+)
 </script>
 <template>
   <div>
@@ -190,8 +198,12 @@ watch(() => form.managers, () => checkManagers(), { deep: true })
             <a-col :span="24">
               <Field name="name" v-slot="{ value, handleChange }" v-model="form.name">
                 Наименование<sup>*</sup>
-                <a-input placeholder="Наименование задачи" @update:value="handleChange" :value="value"
-                  allow-clear></a-input>
+                <a-input
+                  placeholder="Наименование задачи"
+                  @update:value="handleChange"
+                  :value="value"
+                  allow-clear
+                ></a-input>
               </Field>
               <Transition name="fade">
                 <ErrorMessage name="name" class="error-message" />
@@ -200,65 +212,98 @@ watch(() => form.managers, () => checkManagers(), { deep: true })
             <a-col :span="24">
               <div>Тур</div>
 
-              <a-select v-model:value="form.trip" show-search placeholder="поиск тура" style="width: 100%;"
-                :default-active-first-option="false" :show-arrow="false" :filter-option="false"
-                :fieldNames="{ label: 'name', value: '_id' }" :not-found-content="null" :options="trips"
-                @search="tripSearch" @change="tripChange"></a-select>
+              <a-select
+                v-model:value="form.trip"
+                show-search
+                placeholder="поиск тура"
+                style="width: 100%"
+                :default-active-first-option="false"
+                :show-arrow="false"
+                :filter-option="false"
+                :fieldNames="{ label: 'name', value: '_id' }"
+                :not-found-content="null"
+                :options="trips"
+                @search="tripSearch"
+                @change="tripChange"
+              ></a-select>
             </a-col>
             <a-col :span="24">
               <div>Партнер</div>
 
-              <a-select v-model:value="form.partner" show-search placeholder="поиск партнера" style="width: 100%;"
-                :default-active-first-option="false" :show-arrow="false" :filter-option="false"
-                :fieldNames="{ label: 'name', value: '_id' }" :not-found-content="null" :options="partners"
-                @search="partnerSearch" @change="partnerChange"></a-select>
+              <a-select
+                v-model:value="form.partner"
+                show-search
+                placeholder="поиск партнера"
+                style="width: 100%"
+                :default-active-first-option="false"
+                :show-arrow="false"
+                :filter-option="false"
+                :fieldNames="{ label: 'name', value: '_id' }"
+                :not-found-content="null"
+                :options="partners"
+                @search="partnerSearch"
+                @change="partnerChange"
+              ></a-select>
             </a-col>
             <a-col :span="24">
-
               <Field name="start" v-slot="{ value, handleChange }" v-model="form.deadLine">
                 Крайний срок
-                <a-date-picker v-model="form.deadLine"
-                  :showTime="{ format: 'HH:mm', defaultValue: dayjs().hour(0).minute(0).second(0) }" style="width: 100%"
-                  placeholder="крайний срок" :locale="locale" :format="(value) => dayjs(value).format('DD.MM.YY HH:mm')"
-                  @change="handleDateChange" />
+                <a-date-picker
+                  v-model="form.deadLine"
+                  :showTime="{ format: 'HH:mm', defaultValue: dayjs().hour(0).minute(0).second(0) }"
+                  style="width: 100%"
+                  placeholder="крайний срок"
+                  :locale="locale"
+                  :format="(value) => dayjs(value).format('DD.MM.YY HH:mm')"
+                  @change="handleDateChange"
+                />
               </Field>
               <Transition name="fade">
                 <ErrorMessage name="start" class="error-message" />
               </Transition>
             </a-col>
             <a-col :span="24">
-
               <Field name="payAmount" v-slot="{ value, handleChange }" v-model="form.payAmount">
                 Стоимость
-                <a-input placeholder="5500" @update:value="handleChange" :value="value" allow-clear
-                  type="number"></a-input>
+                <a-input
+                  placeholder="5500"
+                  @update:value="handleChange"
+                  :value="value"
+                  allow-clear
+                  type="number"
+                ></a-input>
               </Field>
-
             </a-col>
             <a-col :span="24">
-
               <Field name="comment" v-slot="{ value, handleChange }" v-model="form.comment">
                 Комментарий
                 <a-input placeholder="Комментарий" @update:value="handleChange" :value="value" allow-clear></a-input>
               </Field>
             </a-col>
 
-
             <a-col :span="24">
               Менеджеры
 
+              <div
+                v-for="(item, index) in managersEmails"
+                :key="index"
+                style="display: flex"
+                align="center"
+                class="mb-16"
+              >
+                <a-input v-model:value="managersEmails[index]" placeholder="manager@mail.ru" class="mr-8" />
 
-              <div v-for="   item, index in form.managers   " :key="index" style="display: flex" align="center"
-                class="mb-16">
-                <a-input v-model:value="form.managers[index]" placeholder="manager@mail.ru" class="mr-8" />
+                <span
+                  v-if="emailExists[index]"
+                  class="mdi mdi-check"
+                  style="font-size: 24px; font-weight: bold; color: #219fcf"
+                ></span>
 
-                <span v-if='emailExists[index]' class="mdi mdi-check"
-                  style="font-size:24px; font-weight:bold; color:#219FCF"></span>
-
-
-                <span v-else class="mdi mdi-not-equal-variant"
-                  style="font-size:24px; font-weight:bold; color:#ff6600"></span>
-
+                <span
+                  v-else
+                  class="mdi mdi-not-equal-variant"
+                  style="font-size: 24px; font-weight: bold; color: #ff6600"
+                ></span>
 
                 <a-button @click="removeManager(index)" shape="circle" class="ml-4">
                   <span class="mdi mdi-minus" style="cursor: pointer"></span>
@@ -272,7 +317,8 @@ watch(() => form.managers, () => checkManagers(), { deep: true })
             </a-col>
 
             <a-col :span="24" class="d-flex justify-center">
-              <a-button class="lets_go_btn ma-36" type="primary" html-type="submit" :disabled="!meta.valid">Отправить
+              <a-button class="lets_go_btn ma-36" type="primary" html-type="submit" :disabled="!meta.valid"
+                >Отправить
               </a-button>
             </a-col>
           </a-row>
