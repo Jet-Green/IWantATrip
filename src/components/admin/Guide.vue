007b9445a247aabc3a14cca4e1931b7c308758f2
regtyit <titov54275@gmail.com>
2025-03-25 18:42:03 +0400

    sus

--- src/components/admin/Guide.vue
+++ src/components/admin/Guide.vue
@@ -1,14 +1,23 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useGuide } from '../../stores/guide';
-import { name } from 'dayjs/locale/ru';
+import { refresh } from 'less';
 
 let guideStore = useGuide()
 
 let addGuideModal = ref(false)
-let guide = ref({})
+let guide = reactive({
+    name: '',
+    surname: '',
+    email: '',
+    phone: '',
+    image: '',
+    socialMedia: '',
+    offer: '',
+    description: ''
+})
 
-let guides = ref([
+let guides = reactive([
     {
         name: 'Иван',
         surname: 'Иванов',
@@ -21,6 +30,21 @@ let guides = ref([
     }
 ])
 
+async function addGuide() {
+    if (guide.name.length > 1 && guide.surname.length > 1 && guide.phone.length > 2 && guide.email.length > 2 && guide.offer.length > 2) {
+        let location = {}
+        if (localStorage.getItem("location")) {
+            location = JSON.parse(localStorage.getItem("location"))
+            taxi.value.location = location
+        }
+        let res = await guideStore.addGuide(guide.value)
+        addGuideModal.value = false;
+        guide.value = {}
+        if (res.status == 200) {
+            refreshGuides()
+        }
+    }
+}
 
 onMounted(() => {
     // guides.value = guideStore.guides
@@ -53,6 +77,41 @@ onMounted(() => {
                 </a-card>
         </a-col>
     </a-row>
+    <a-modal v-model:open="addGuideModal" :footer="null" title="Добавить такси">
+            <a-row :gutter="[4, 24]">
+                <a-col :span="24" :md="12">
+                    Имя
+                    <a-input style="width: 100%" v-model:value="guide.name" placeholder="Иван" />
+                </a-col>
+                <a-col :span="24" :md="12">
+                    Фамилия
+                    <a-input style="width: 100%" v-model:value="guide.surname" placeholder="Иванов" />
+                </a-col>
+                <a-col :span="24" :md="12">
+                    Почта
+                    <a-input style="width: 100%" v-model:value="guide.email" placeholder="ivanov@ya.ru" />
+                </a-col>
+                <a-col :span="24" :md="12">
+                    Телефон
+                    <a-input style="width: 100%" v-model:value="guide.phone" placeholder="79127528874" />
+                </a-col>
+                <a-col :span="24">
+                    Краткое описание
+                    <a-input style="width: 100%" v-model:value="guide.offer" placeholder="Профессиональный гид" />
+                </a-col>
+                <a-col :span="24">
+                    Социальные сети
+                    <a-input style="width: 100%" v-model:value="guide.socialMedia" placeholder="https://vk.com/qbitclub"/>
+                </a-col>
+                <a-col :span="24">
+                    Описание
+                    <a-textarea style="width: 100%" v-model:value="guide.description" placeholder="Описание" auto-size/>
+                </a-col>
+                <a-col :span="24">
+                    <a-button type="primary" @click="addGuide()"> добавить </a-button>
+                </a-col>
+            </a-row>
+        </a-modal> 
 </template>
 <style>
 </style>
