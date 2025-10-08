d1eda91ed04c74108c1ee60bbd3d98e4602e19ba
qbclub <17926057+qbclub@users.noreply.github.com>
2025-10-08 08:16:26 +0400

    fix

--- src/components/_guide/guides/GuideCard.vue
+++ src/components/_guide/guides/GuideCard.vue
@@ -22,23 +22,26 @@ const router = useRouter()
             <a-col :xs="24" style="text-align: center; margin-bottom: 10px;">
                 {{ guide?.offer }}
             </a-col>
-            <a-col :xs="24" :md="8"> <img :src="guide?.image" class="pa-8" alt="Guide Image" style="width: 90%; text-align: center;" />
+            <a-col :xs="24" :md="8"> <img :src="guide?.image" class="pa-8" alt="Guide Image"
+                    style="width: 90%; text-align: center;" />
             </a-col>
             <a-col :xs="24" :md="16" class="d-flex align-center">
                 <div style="width: 100%;">
                     <div> Локация: <b> {{ guide?.location?.name }}</b></div>
-                    <div v-if="guide?.phone"> Телефон: <b>{{ guide?.phone }}</b></div>
+
+                    <div v-if="guide?.phone"> Телефон: <b><a :href="`tel:${guide.phone}`">{{ guide.phone }}</a></b>
+                    </div>
                     <div v-if="guide?.socialMedia"> Соц. сеть: <b> {{ guide?.socialMedia }}</b></div>
                     <div v-if="guide?.email"> Почта: <b>{{ guide?.email }}</b></div>
-                     <div v-if="guide?.type"> Статус: <b>{{ guide?.type }}</b></div>
-                    <div class="d-flex justify-center" >
-                        <a-button class=" ma-36" 
-                            @click="router.push(`/guide?_id=${guide._id}`)" v-if="withButton">Подробнее</a-button>
+                    <div v-if="guide?.type"> Статус: <b>{{ guide?.type }}</b></div>
+                    <div class="d-flex justify-center">
+                        <a-button class=" ma-36" @click="router.push(`/guide?_id=${guide._id}`)"
+                            v-if="withButton">Подробнее</a-button>
                     </div>
                 </div>
 
             </a-col>
-       
+
 
         </a-row>
 
