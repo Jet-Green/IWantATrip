fc71666a27557d5aa97031fd9ed8495ca4612ac8
regtyit <titov54275@gmail.com>
2026-02-01 17:14:27 +0500

    cringe commit

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { onMounted } from "vue";
+import $api from "../plugins/axios";
+import { onMounted,ref } from "vue";
 // import MainPageMessage from '../components/sections/MainPageMessage.vue'
 import CityGuide from "../components/sections/CityGuide.vue";
 import ButtonsRow from "../components/sections/ButtonsRow.vue"
@@ -12,13 +13,16 @@ import ToTrips from "../components/sections/ToTrips.vue";
 // import Hello from '../components/sections/Hello.vue';
 import Footer from '../components/Footer.vue'
 import { useRoute } from "vue-router";
-
+// import { useFetch } from "@vueuse/core";
+let src=ref('')
 
 const route = useRoute();
 
 
-onMounted (()=>{
-  if(route.hash) {
+onMounted(async () => {
+  src.value =await $api.get('https://suggest.travelpayouts.com/search?service=tutu_trains&term=2000000&term2=2064130&callback=n')//nearest in date trains.
+  console.log(src.value.data)
+  if (route.hash) {
     let id = route.hash.slice(1)
     document.getElementById(id)?.scrollIntoView()
   }
@@ -28,12 +32,20 @@ onMounted (()=>{
   <div>
     <Explanation />
     <ButtonsRow />
+    <a-row justify="center">
+      <a-col :span="6" :md="6">
+        <!-- <div id="tpwl-search"></div> -->
+        <!-- <div id="tpwl-tickets" style="height:500px ;overflow-y:scroll"></div> -->
+        <!-- for aviasales -->
+        <!-- {{ src }} for tutu -->
+      </a-col>
+    </a-row>
     <!-- <ToTrips /> -->
     <FindTrip />
     <!-- <Companions /> -->
-    <CityGuide id="guide"/>
-    <Footer/>
-  
+    <CityGuide id="guide" />
+    <Footer />
+
   </div>
 </template>
 <style lang="scss" scoped></style>
