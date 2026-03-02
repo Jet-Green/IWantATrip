5eca9b9e37b103840b53c21bdb942e32669db32b
regtyit <titov54275@gmail.com>
2026-03-02 19:18:46 +0500

    ExcursionCard

--- src/components/_guide/ExcursionCard.vue
+++ src/components/_guide/ExcursionCard.vue
@@ -10,7 +10,7 @@ const excursion = props.excursion
 let getPrice = computed(() => {
   if (excursion.prices.length) {
     let min = _.minBy(excursion.prices, 'price')
-    return `От ${min.price} руб` 
+    return `От ${min.price} руб`
   } else {
     return 'Бесплатно'
   }
@@ -18,7 +18,7 @@ let getPrice = computed(() => {
 
 }) 
 </script>
-<template>
+<!--- <template>
   <div class="card">
     <div class=" d-flex direction-column space-between">
 
@@ -32,28 +32,72 @@ let getPrice = computed(() => {
     <div class="title"> {{ excursion.name }}</div>
     <div class="price"> {{ getPrice }}</div>
   </div>
+</template> -->
+<template>
+  <div class="excursion-card" :style="{ 'background-image': 'url(' + excursion.images[0] + ')' }">
+    <div class="content">
+      <div class="title"> {{ excursion.name }} </div>
+
+      <div class="information">
+        <span class="about-excursion">
+          {{ excursion.excursionType.type }} |
+          {{ excursion.duration }}
+        </span>
+        <span class="price">
+          От {{ excursion.prices[0].price }}₽
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
-.card {
+.excursion-card {
   overflow: hidden;
   cursor: pointer;
   height: 100%;
+  width: 100%;
+  aspect-ratio: 4/3;
+  border-radius: 10px;
+  object-fit: contain;
+}
 
-  img {
-    object-fit: cover;
-    width: 100%;
-    aspect-ratio: 270 / 175;
-  }
+.content {
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(to bottom, transparent, #484848);
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 16px 16px 16px;
 }
 
 .title {
+  // font-size: clamp(1rem, 0.55rem + 0.8vw, 1.25rem);
+  font-size: 26px;
+  font-weight: 900;
+  color: white;
+  width: 100%;
+  line-height: 1.1;
+}
 
-  font-size: clamp(1rem, 0.55rem + 0.8vw, 1.25rem);
-  font-weight: 500;
+.information {
+  color: white;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.about-excursions {
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.1;
 }
 
 .price {
-  font-weight: 500;
-  font-size: clamp(0.75rem, 0.3rem + 0.8vw, 1rem);
+  font-weight: 600;
+  font-size: 26px;
+  line-height: 1.1;
 }
 </style>
