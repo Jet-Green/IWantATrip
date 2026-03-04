d77e4caa8d3c19721fac1b19f9a3f9ae258f8b30
Jet-Green <grishadzyin@gmail.com>
2026-03-04 19:12:31 +0500

    add excursions module and update any module template

--- src/components/ladingPage/ExcursionsModule.vue
+++ src/components/ladingPage/ExcursionsModule.vue
@@ -1,22 +1,63 @@
-<script setup></script>
+<script setup>
+import ExcursionFilter from '../sections/ExcursionFilter.vue';
+import ExcursionCard from '../_guide/ExcursionCard.vue';
+import { ref } from 'vue';
+import { useExcursion } from '../../stores/excursion';
+
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const excursionStore = useExcursion()
+
+const scrollContainer = ref(null)
+
+function scrollLeft() {
+  if (!scrollContainer.value) return
+  const width = scrollContainer.value.clientWidth
+  scrollContainer.value.scrollBy({
+    left: -width,
+    behavior: 'smooth'
+  })
+}
+
+function scrollRight() {
+  if (!scrollContainer.value) return
+  const width = scrollContainer.value.clientWidth
+  scrollContainer.value.scrollBy({
+    left: +width,
+    behavior: 'smooth'
+  })
+}
+</script>
 <template>
   <div class="container">
     <div class="module-card">
       <div class="cols-22">
-        <h3>Экскурсии</h3>
-        <div style="height: 400px;">
-          тут экскурсии
+        <h3 v-if="excursionStore.excursions.length > 0">Экскурсии</h3>
+        <h3 v-else>Экскурсии не найдены...</h3>
+        <!-- это костыль) чтобы экскурсии загрузились в стор и локация тоже учлась -->
+        <div v-show="false">
+          <ExcursionFilter />
+        </div>
+        <div v-if="excursionStore.excursions.length > 0">
+          <div class="scroll-container" :gutter="[16, 16]" ref="scrollContainer">
+            <div class="scroll-container-col"
+              v-for="ex of [...excursionStore.excursions, ...excursionStore.excursions, ...excursionStore.excursions, ...excursionStore.excursions, ...excursionStore.excursions]"
+              :key="ex._id">
+              <ExcursionCard :excursion="ex" @click="router.push(`/excursion?_id=${ex._id}`)" :id="ex._id" />
+            </div>
+          </div>
         </div>
 
         <div class="actions">
-          <button class="see-all-btn unselectable">Смотреть все</button>
+          <button class="see-all-btn unselectable" @click="router.push('/excursions')">Смотреть все</button>
 
-          <div class="slider-btns-container">
-            <button class="slider-btn">
+          <div class="slider-btns-container" v-if="excursionStore.excursions.length > 0">
+            <button class="slider-btn" @click="scrollLeft">
               <span class="mdi mdi-chevron-left"></span>
             </button>
             <div class="divider"></div>
-            <button class="slider-btn">
+            <button class="slider-btn" @click="scrollRight">
               <span class="mdi mdi-chevron-right"></span>
             </button>
           </div>
@@ -25,7 +66,7 @@
     </div>
   </div>
 </template>
-<style scoped>
+<style scoped lang="scss">
 h3 {
   font-size: clamp(1rem, -0.1932rem + 3.4091vw, 2.5rem);
   font-weight: 900;
@@ -56,6 +97,7 @@ h3 {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-top: 20px;
 }
 
 .see-all-btn {
@@ -67,6 +109,17 @@ h3 {
   font-size: clamp(0.875rem, 0.179rem + 1.9886vw, 1.75rem);
   color: white;
   cursor: pointer;
+
+  transition: all 0.28s ease;
+  transform: scale(1);
+
+  &:hover {
+    transform: scale(1.01);
+  }
+
+  &:active {
+    transform: scale(0.97);
+  }
 }
 
 .slider-btn {
@@ -77,6 +130,16 @@ h3 {
   border: 0;
   font-size: 30px;
   cursor: pointer;
+  transition: all 0.28s ease;
+  transform: scale(1);
+
+  &:hover {
+    transform: scale(1.01);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
 }
 
 .divider {
@@ -91,5 +154,42 @@ h3 {
   display: flex;
   align-items: center;
   height: 50px;
+}
+
+.scroll-container {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  gap: 16px;
+  scroll-behavior: smooth;
+  -webkit-overflow-scrolling: touch;
+  scrollbar-width: none;
+
+  .scroll-container-col {
+    flex: 0 0 100%;
+    min-width: 0;
+  }
+
+  @media (min-width: 520px) {
+    .scroll-container-col {
+      flex: 0 0 calc(50% - 8px);
+    }
+  }
+
+  @media (min-width: 768px) {
+    .scroll-container-col {
+      flex: 0 0 calc((100% - 32px) / 3);
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .scroll-container-col {
+      flex: 0 0 calc((100% - 48px) / 4);
+    }
+  }
+
+  .scroll-container::-webkit-scrollbar {
+    display: none;
+  }
 }
 </style>
