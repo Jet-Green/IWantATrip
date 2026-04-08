1fcb46aa1f93d571030340b5781940707f52324f
Jet-Green <grishadzyin@gmail.com>
2026-04-08 17:05:58 +0500

    nullabe

--- src/views/CreateTripNoHelp.vue
+++ src/views/CreateTripNoHelp.vue
@@ -410,7 +410,7 @@ let formSchema = yup.object({
   // returnConditions: yup.string().required("заполните поле"),
   notNecessarily: yup.string(),
   tripRegion: yup.string().required("заполните поле"),
-  privetMirPaymentLink: yup.string().matches(/^https:\/\/.*/, "ссылка должна начинаться с https://"),
+  privetMirPaymentLink: yup.string().nullable().test("optional-url", "ссылка должна начинаться с https://", value => !value || /^https:\/\/.*/.test(value)),
   // https://vee-validate.logaretm.com/v4/examples/array-fields/
 });
 onMounted(async () => {
